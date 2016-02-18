f014382f19312c60b49276737292a400839c0f89
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-02-18 12:26:37 +0100

    sviluppo AssetFinder e fileCache

---
+++ assets/js/description_translate_edit.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+    var textProductDescription = $('textarea[name^="ProductDescription"]');
+    textProductDescription.each(function () {
+        if (textProductDescription.length) {
+            textProductDescription.summernote({
+                height: 200,
+                onfocus: function (e) {
+                    $('body').addClass('overlay-disabled');
+                },
+                onblur: function (e) {
+                    $('body').removeClass('overlay-disabled');
+                }
+            });
+        }
+    });
+
+});
+
+$(document).on('bs.desc.edit', function (e,element,button) {
+
+    var bsModal = $('#bsModal');
+    var header = $('.modal-header h4');
+    var body = $('.modal-body');
+    var cancelButton = $('.modal-footer .btn-default');
+    var okButton = $('.modal-footer .btn-success');
+
+    header.html('Traduci Descrizione');
+    okButton.html('Fatto').off().on('click', function () {
+        bsModal.modal('hide');
+        okButton.off();
+    });
+    cancelButton.remove();
+
+    $.ajaxForm({
+        type: "PUT",
+        url: "#",
+        formAutofill: true
+    },new FormData()).done(function (content){
+        body.html("Salvataggio riuscito");
+        bsModal.modal();
+        window.location.replace("/blueseal/traduzioni/descrizioni/modifica");
+    }).fail(function (){
+        body.html("Errore grave");
+        bsModal.modal();
+    });
+
+});
