1f08adf0c1a3cc650d3a267183d7b9d1cb01a68d
simon <simona@bambooshoot.agency>
2016-02-18 13:05:00 +0100

    allineamento dal vecchio al nuovo

---
+++ controllers/ajax/CImporterListAjaxController.php
@@ -0,0 +1,129 @@
+<?php
+namespace bamboo\blueseal\controllers\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\intl\CLang;
+
+/**
+ * Class CImporterListAjaxController
+ * @package redpanda\blueseal\controllers\ajax
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>, ${DATE}
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @since ${VERSION}
+ */
+class CImporterListAjaxController extends AAjaxController
+{
+    protected $urls = [];
+    protected $authorizedShops = [];
+    protected $em;
+
+    /**
+     * @param $action
+     * @return mixed
+     */
+    public function createAction($action)
+    {
+        $this->app->setLang(new CLang(1,'it'));
+        $this->urls['base'] = $this->app->baseUrl(false)."/blueseal/";
+        $this->urls['page'] = $this->urls['base']."importatori";
+        $this->urls['dummy'] = $this->app->cfg()->fetch('paths','dummyUrl');
+
+        if ($this->app->getUser()->hasRole('ownerEmployee')) {
+
+        } else if($this->app->getUser()->hasRole('friendEmployee')){
+            $res = $this->app->dbAdapter->select('UserHasShop',['userId'=>$this->app->getUser()->getId()])->fetchAll();
+            foreach($res as $val) {
+                $this->authorizedShops[] = $val['shopId'];
+            }
+        }
+
+        $this->em = new \stdClass();
+        $this->em->shops = $this->app->entityManagerFactory->create('Shop');
+
+        return $this->{$action}();
+    }
+
+    public function get()
+    {
+        $datatable = new CDataTables('vBluesealDirtyProductList',['id'],$_GET);
+
+        if (!empty($this->authorizedShops)) {
+            $datatable->addCondition('shopId',$this->authorizedShops);
+        }
+
+        $shops = $this->app->repoFactory->create('Shop')->em()->findBySql($datatable->getQuery(),$datatable->getParams());
+        $count = $this->em->shops->findCountBySql($datatable->getQuery(true), $datatable->getParams());
+        $totalCount = $this->em->shops->findCountBySql($datatable->getQuery('full'), $datatable->getParams());
+
+        $response = [];
+        $response ['draw'] = $this->app->router->request()->getRequestData('draw');
+        $response ['recordsTotal'] = $totalCount;
+        $response ['recordsFiltered'] = $count;
+        $response ['data'] = [];
+
+
+        $i = 0;
+        foreach($shops as $shop) {
+            $trans = 0;
+            $state = '';
+
+            $resB = $this->app->dbAdapter->query("SELECT count(0) AS mancanti
+                                                FROM DictionaryBrand d1
+                                                WHERE (d1.shopId = ? AND isnull(d1.productBrandId))", [$shop->id])->fetchAll()[0];
+            $resC = $this->app->dbAdapter->query("SELECT count(0) AS mancanti
+                                                FROM DictionaryCategory d1
+                                                WHERE (d1.shopId = ? AND isnull(d1.productCategoryId))", [$shop->id])->fetchAll()[0];
+            $resCG = $this->app->dbAdapter->query("SELECT count(0) AS mancanti
+                                                FROM DictionaryColorGroup d1
+                                                WHERE (d1.shopId = ? AND isnull(d1.productColorGroupId))", [$shop->id])->fetchAll()[0];
+            $resS = $this->app->dbAdapter->query("SELECT count(0) AS mancanti
+                                                FROM DictionarySeason d1
+                                                WHERE (d1.shopId = ? AND isnull(d1.productSeasonId))", [$shop->id])->fetchAll()[0];
+            $resZ = $this->app->dbAdapter->query("SELECT count(0) AS mancanti
+                                                FROM DictionarySize d1
+                                                WHERE (d1.shopId = ? AND isnull(d1.productSizeId))", [$shop->id])->fetchAll()[0];
+            $resT = $this->app->dbAdapter->query("SELECT count(0) AS mancanti
+                                                FROM DictionaryTag d1
+                                                WHERE (d1.shopId = ? AND isnull(d1.tagId))", [$shop->id])->fetchAll()[0];
+            $conn = $this->app->dbAdapter->query("SELECT count(0) AS conn
+                                                FROM ImporterConnector d1
+                                                WHERE (d1.shopId = ?)", [$shop->id])->fetchAll()[0];
+            $errorExe = $this->app->dbAdapter->query("SELECT count(0) AS error
+                                                FROM JobExecution je, Job j
+                                                WHERE (j.defaultArgs = ?) AND je.jobId=j.id
+                                                AND je.error = 1 ", [$shop->id])->fetchAll()[0];
+            $errorLog = $this->app->dbAdapter->query("SELECT count(0) AS error
+                                                FROM Job j, JobLog jl
+                                                WHERE (j.defaultArgs = ?) AND jl.jobId=j.id
+                                                AND jl.severity = 'ERROR'", [$shop->id])->fetchAll()[0];
+
+            $trans = $resB['mancanti']+$resC['mancanti']+$resCG['mancanti']+$resS['mancanti']+$resZ['mancanti']+$resT['mancanti'];
+            $error = $errorLog['error']+$errorExe['error'];
+
+            if ($shop->job->isRunning != 0) {
+                $state = 'Working';
+            } elseif ($shop->job->isActive == 0) {
+                $state = 'Offline';
+            } else {
+                $state = 'Online';
+            }
+
+            $response['data'][$i]["DT_RowId"] = 'row__'.$shop->id;
+            $response['data'][$i]["DT_RowClass"] = 'colore';
+            $response['data'][$i]['name'] = $shop->title;
+            $response['data'][$i]['trans'] = $trans;
+            $response['data'][$i]['state'] = $state;
+            $response['data'][$i]['error'] = $error;
+            $response['data'][$i]['connector'] = $conn['conn'];
+
+            $i++;
+        }
+
+        echo json_encode($response);
+    }
+}
---
+++ controllers/ajax/CTagListAjaxController.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrea Tesei
+ * Modified File: CUserListAjaxController.php
+ * Date: 11/02/2016
+ * Time: 10:46
+ */
+
+namespace bamboo\blueseal\controllers\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\intl\CLang;
+
+class CTagListAjaxController extends AAjaxController              /** Modifica qui */
+{
+    protected $urls = [];
+    protected $authorizedShops = [];
+    protected $em;
+
+    /**
+     * @param $action
+     * @return mixed
+     */
+    public function createAction($action)
+    {
+        $this->app->setLang(new CLang(1, 'it'));
+        $this->urls['base'] = $this->app->baseUrl(false) . "/blueseal/";
+        $this->urls['page'] = $this->urls['base'] . "tag";                            /** Modifica qui */
+        $this->urls['dummy'] = $this->app->cfg()->fetch('paths', 'dummyUrl');
+
+        if ($this->app->getUser()->hasRole('ownerEmployee')) {
+
+        } else if ($this->app->getUser()->hasRole('friendEmployee')) {
+            $res = $this->app->dbAdapter->select('UserHasShop', ['userId' => $this->app->getUser()->getId()])->fetchAll();
+            foreach ($res as $val) {
+                $this->authorizedShops[] = $val['shopId'];
+            }
+        }
+
+        $this->em = new \stdClass();
+        $this->em->tags = $this->app->entityManagerFactory->create('Tag');
+
+        return $this->{$action}();
+    }
+
+    public function get()
+    {
+        $datatable = new CDataTables('vBluesealTagList', ['id'], $_GET);
+
+        $tags = $this->em->tags->findBySql($datatable->getQuery(), $datatable->getParams());
+        $count = $this->em->tags->findCountBySql($datatable->getQuery(true), $datatable->getParams());
+        $totalCount = $this->em->tags->findCountBySql($datatable->getQuery('full'), $datatable->getParams());
+
+        $modifica = $this->urls['base'] . "tag";
+
+        $okManage = $this->app->getUser()->hasPermission('/admin/product/edit');
+
+        $response = [];
+        $response ['draw'] = $_GET['draw'];
+        $response ['recordsTotal'] = $totalCount;
+        $response ['recordsFiltered'] = $count;
+        $response ['data'] = [];
+
+        $i = 0;
+
+        \BlueSeal::dump($tags);
+
+        foreach ($tags as $val) {
+
+            $response['data'][$i]["DT_RowId"] = 'row__' . $val->id . '__' . $val->id;
+            $response['data'][$i]["DT_RowClass"] = 'colore';
+            $response['data'][$i]['slug'] = $okManage ? '<a data-toggle="tooltip" title="modifica" data-placement="right" href="' . $modifica . '?id=' . $val->id . '">' . $val->slug . '</a>' : $val->slug;
+            $response['data'][$i]['priority'] = $val->sortingPriority->priority;             /** ??? */
+
+            $i++;
+        }
+
+        echo json_encode($response);
+    }
+}
