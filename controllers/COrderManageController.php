77e9c4f1480ffcfb92d7a637a1f1fca03659c176
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-02-19 10:13:01 +0100

    allineamento tra vecchiop e nuovo, fix scrittura file

--- controllers/COrderManageController.php
+++ controllers/COrderManageController.php
@@ -24,9 +24,9 @@ class COrderManageController extends ARestrictedAccessRootController
 
         $repoStatus = $this->app->repoFactory->create('OrderStatus');
         $statuses = $repoStatus->findAll();
-        $orderId =  $this->app->router->request()->getRequestData();
+        $orderId =  $this->app->router->request()->getRequestData('order');
 		\BlueSeal::dump($orderId);
-	    $order = $this->app->repoFactory->create('Order')->findOne($orderId);
+	    $order = $this->app->repoFactory->create('Order')->findOne([$orderId]);
 		\BlueSeal::dump($order);
 
         $em = $this->app->entityManagerFactory->create('Country');
