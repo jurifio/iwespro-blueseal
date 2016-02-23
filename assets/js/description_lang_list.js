6213def9e3e50bcd362cf497d0a0d50bda7a6867
simon <simona@bambooshoot.agency>
2016-02-23 17:59:00 +0100

    allineamento dal vecchio al nuovo

---
+++ assets/js/description_lang_list.js
@@ -0,0 +1,24 @@
+$(document).on('focusout', $('name[descId]').val(), function(event) {
+    var changed =  $(event.target);
+    $.ajax({
+        type: "PUT",
+        url: changed.data('action'),
+        data: {
+            nameId: changed.val(),
+            id: changed.data('pid'),
+            lang: changed.data('lang')
+        }
+    });
+});
+
+$(document).on('focusin', $('name[descId]').val(), function(event) {
+    var textProductDescription = $('textarea[name^="descId"]');
+    textProductDescription.each(function () {
+        if (textProductDescription.length) {
+            textProductDescription.summernote({
+                height: 200
+            });
+        }
+    });
+
+});
