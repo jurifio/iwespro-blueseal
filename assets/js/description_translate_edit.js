a196649c8f56d2896efde7ed2583e68f7dc6939a
simon <simona@bambooshoot.agency>
2016-02-23 12:14:18 +0100

    allineamento dal vecchio al nuovo

--- assets/js/description_translate_edit.js
+++ assets/js/description_translate_edit.js
@@ -4,10 +4,7 @@ $(document).ready(function() {
     textProductDescription.each(function () {
         if (textProductDescription.length) {
             textProductDescription.summernote({
-                height: 200,
-                onfocus: function (e) {
-                    $('body').addClass('overlay-disabled');
-                }
+                height: 200
             });
         }
     });
