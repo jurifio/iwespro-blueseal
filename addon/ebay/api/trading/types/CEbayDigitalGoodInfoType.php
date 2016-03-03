ad620558e26f434e3cf2486d2973318d227d93b5
simon <simona@bambooshoot.agency>
2016-03-03 16:40:54 +0100

    allineamento dal vecchio al nuovo

---
+++ addon/ebay/api/trading/types/CEbayDigitalGoodInfoType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace bamboo\addon\ebay\api\trading\calls\types;
+
+
+/**
+ * Class CEbayDigitalGoodInfoType
+ * @package bamboo\addon\ebay\api\trading\calls\types
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 03/03/2016
+ * @since 1.0
+ */
+class CEbayDigitalGoodInfoType
+{
+    /** @var bool */
+    protected $digitalDelivery;
+
+}
