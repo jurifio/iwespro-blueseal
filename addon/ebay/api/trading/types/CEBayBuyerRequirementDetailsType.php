044cfe48d56df2a71e7c1a167f6f84a35218efe7
emanuele <emanuele@bambooshoot.agency>
2016-03-03 12:57:35 +0100

    eBay SDK 0.1 - aggiunti enum e types

---
+++ addon/ebay/api/trading/types/CEBayBuyerRequirementDetailsType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace redpanda\blueseal\ebay\calls\trading\types;
+
+/**
+ * Class CEbayBuyerRequirementDetailsType
+ * @package redpanda\blueseal\ebay\calls\trading\types
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 03/03/2016
+ * @since 1.0
+ */
+class CEbayBuyerRequirementDetailsType
+{
+    /** @var bool */
+   protected $linkedPayPalAccount;
+}
