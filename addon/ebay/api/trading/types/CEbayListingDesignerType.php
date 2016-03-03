f220b302ab51d6099372fbdf23edb24a5f97eead
simon <simona@bambooshoot.agency>
2016-03-03 17:20:14 +0100

    allineamento dal vecchio al nuovo

---
+++ addon/ebay/api/trading/types/CEbayListingDesignerType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace bamboo\addon\ebay\api\trading\calls\types;
+/**
+ * Class CEbayListingDesignerType
+ * @package bamboo\addon\ebay\api\trading\calls\types
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 03/03/2016
+ * @since 1.0
+ */
+
+class CEbayListingDesignerType
+{
+   /** @var int */
+   protected $layoutID;
+   /** @var bool */
+   protected $optimalPictureSize;
+   /** @var int */
+   protected $themeID;
+
+}
