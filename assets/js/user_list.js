e586d3bd0bd5ad3d932f7fd2e08c24aaa114d650
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-04-22 13:10:45 +0200

    permessi

--- assets/js/user_list.js
+++ assets/js/user_list.js
@@ -22,55 +22,45 @@ $(document).on('bs.roles.show', function (e,element,button) {
 		var selectedRowsCount = selectedRows.length;
 
 		if (selectedRowsCount ==  1) {
-			$.ajax({
-				url: "/blueseal/xhr/GetRolesForUser",
-				async: false,
-				data: { id: selectedRows[0].DT_RowId.split('__')[1] }
-			}).done(function (res) {
-
-				userRoles = JSON.parse(res);
-
-				var radioTree = $("#rolesTree");
-				if (radioTree.length) {
-					radioTree.dynatree({
-						debugLevel: 0,
-						initAjax: {
-							url: "/blueseal/xhr/GetRolesTree"
-						},
-						autoexpand: true,
-						checkbox: true,
-						imagePath: "/assets/img/skin/icons_better.gif",
-						selectMode: 2,
-						onPostInit: function () {
-							for (var i = 0; i < userRoles.length; i++) {
-								if (this.getNodeByKey(userRoles[i].id) != null) {
-									this.getNodeByKey(userRoles[i].id).select();
-								}
-							}
-							$.map(this.getSelectedNodes(), function (node) {
-								node.makeVisible();
-							});
-							$('#rolesTree').scrollbar({
-								axis: "y"
-							});
-						},
-						onSelect: function (select, node) {
-							//seleziona
+			var radioTree = $("#rolesTree");
+			if (radioTree.length) {
+				radioTree.fancytree({
+					selectMode: 2,
+					checkbox: true,
+					source: {
+						url: "/blueseal/xhr/GetRolesTree",
+						cache:false,
+						complete: function() {
+							$(document).trigger('bs.rolesTree.loaded');
 						}
-					});
-				}
-			});
+					}
+				});
+			}
+			bsModal.modal('show');
 		} else {
 			new Alert({
 				type: "warning",
 				message: "Devi selezionare un solo utente per visualizzare i ruoli"
 			}).open();
 		}
-
-
-		bsModal.modal('show');
 	});
 });
+
+$(document).on('bs.rolesTree.loaded',function() {
+	Pace.ignore(function() {
+		var selectedRows = $('.table').DataTable().rows('.selected').data();
+		$.ajax({
+			url: "/blueseal/xhr/GetRolesForUser",
+			data: {id: selectedRows[0].DT_RowId.split('__')[1]}
+		}).done(function (res) {
+			var userRoles = JSON.parse(res);
+			for (var i = 0; i < userRoles.length; i++) {
+				$("#rolesTree").fancytree("getTree").getNodeByKey(userRoles[i].id).setSelected();
+			}
+		});
+	});
+});
+
 $(document).on('bs.permission.show', function (e,element,button) {
 	var bsModal = $('#bsModal');
 	var header = $('#bsModal .modal-header h4');
@@ -78,58 +68,36 @@ $(document).on('bs.permission.show', function (e,element,button) {
 	var cancelButton = $('#bsModal .modal-footer .btn-default');
 	var okButton = $('#bsModal .modal-footer .btn-success');
 
-	header.html('Seleziona Ruolo');
+	header.html('Visualizza Permessi');
 	okButton.html('Fatto').off().on('click', function () {
 		bsModal.modal('hide');
 		okButton.off();
 	});
 	cancelButton.remove();
 	body.css("text-align", 'left');
-	body.html('<div id="permissionTree"></div>');
+	body.html('<div id="permissionsTree"></div>');
 
 	Pace.ignore(function() {
 
 		var selectedRows = $('.table').DataTable().rows('.selected').data();
 		var selectedRowsCount = selectedRows.length;
 
-		if (selectedRowsCount == 1) {
-			$.ajax({
-				url: "/blueseal/xhr/GetPermissionsForUser",
-				async: false,
-				data: { id: selectedRows[0].DT_RowId.split('__')[1] }
-			}).done(function (res) {
-				userPermissions = JSON.parse(res);
-
-				var radioTree = $("#permissionTree");
-				if (radioTree.length) {
-					radioTree.dynatree({
-						debugLevel: 0,
-						initAjax: {
-							url: "/blueseal/xhr/GetPermissionsTree"
-						},
-						autoexpand: true,
-						checkbox: true,
-						imagePath: "/assets/img/skin/icons_better.gif",
-						selectMode: 2,
-						onPostInit: function () {
-							for (var i = 0; i < userPermissions.length; i++) {
-								if (this.getNodeByKey(userPermissions[i]) != null) {
-									this.getNodeByKey(userPermissions[i]).select();
-								}
-							}
-							$.map(this.getSelectedNodes(), function (node) {
-								node.makeVisible();
-							});
-							$('#permissionTree').scrollbar({
-								axis: "y"
-							});
-						},
-						onSelect: function (select, node) {
-							//seleziona
+		if (selectedRowsCount ==  1) {
+			var radioTree = $("#permissionsTree");
+			if (radioTree.length) {
+				radioTree.fancytree({
+					selectMode: 2,
+					checkbox: true,
+					source: {
+						url: "/blueseal/xhr/GetPermissionsTree",
+						cache:false,
+						complete: function() {
+							$(document).trigger('bs.permissionsTree.loaded');
 						}
-					});
-				}
-			});
+					}
+				});
+			}
+			bsModal.modal('show');
 		} else {
 			new Alert({
 				type: "warning",
@@ -137,7 +105,20 @@ $(document).on('bs.permission.show', function (e,element,button) {
 			}).open();
 			return;
 		}
+	});
+});
 
-		bsModal.modal('show');
+$(document).on('bs.permissionsTree.loaded',function() {
+	Pace.ignore(function() {
+		var selectedRows = $('.table').DataTable().rows('.selected').data();
+		$.ajax({
+			url: "/blueseal/xhr/GetPermissionsForUser",
+			data: {id: selectedRows[0].DT_RowId.split('__')[1]}
+		}).done(function (res) {
+			var userPermissions = JSON.parse(res);
+			for (var i = 0; i < userPermissions.length; i++) {
+				$("#permissionsTree").fancytree("getTree").getNodeByKey(userPermissions[i]).setSelected();
+			}
+		});
 	});
 });
