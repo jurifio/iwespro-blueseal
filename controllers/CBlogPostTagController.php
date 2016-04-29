691e28856187d1deb6723f4bfc9fd790cdfe5b3c
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-04-29 11:58:50 +0200

    Inserita gestione tag Blog

--- controllers/CBlogPostTagController.php
+++ controllers/CBlogPostTagController.php
@@ -2,6 +2,7 @@
 namespace bamboo\blueseal\controllers;
 
 use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\core\utils\slugify\CSlugify;
 use bamboo\ecommerce\views\VBase;
 
 /**
@@ -10,18 +11,62 @@ use bamboo\ecommerce\views\VBase;
  */
 class CBlogPostTagController extends ARestrictedAccessRootController
 {
-    protected $fallBack = "blueseal";
-    protected $pageSlug = "blog_tag";
+	protected $fallBack = "blueseal";
+	protected $pageSlug = "blog_tag";
 
-    public function get()
-    {
-        $view = new VBase(array());
-        $view->setTemplatePath($this->app->rootPath().$this->app->cfg()->fetch('paths','blueseal').'/template/blog_tag.php');
+	public function get()
+	{
+		$view = new VBase([]);
+		$view->setTemplatePath($this->app->rootPath() . $this->app->cfg()->fetch('paths', 'blueseal') . '/template/blog_tag.php');
 
-        return $view->render([
-            'app' => new CRestrictedAccessWidgetHelper($this->app),
-            'page'=>$this->page,
-            'sidebar' => $this->sidebar->build()
-        ]);
-    }
+		return $view->render([
+			'app'     => new CRestrictedAccessWidgetHelper($this->app),
+			'page'    => $this->page,
+			'sidebar' => $this->sidebar->build(),
+			'tags'    => $this->app->repoFactory->create('PostTag')->findAll()
+		]);
+	}
+
+	public function post()
+	{
+		$data = $this->app->router->request()->getRequestData();
+		$tag = $this->app->repoFactory->create('PostTag')->getEmptyEntity();
+		$s = new CSlugify();
+		$tag->slug = $s->slugify($data['PostTagTranslation.name']);
+		$tag->id = $tag->insert();
+
+		$tagTranslation = $this->app->repoFactory->create('PostTagTranslation')->getEmptyEntity();
+		$tagTranslation->postTagId = $tag->id;
+		$tagTranslation->langId = $this->app->getLang()->getId();
+		$tagTranslation->name = $data['PostTagTranslation.name'];
+		$tagTranslation->insert();
+
+		return true;
+	}
+
+	/**
+	 * @return bool
+	 * @throws \Exception
+	 */
+	public function delete()
+	{
+		$data = $this->app->router->request()->getRequestData('ids');
+		$ids = explode(',', $data);
+
+		foreach ($ids as $id) {
+			try {
+				$postTag = $this->app->repoFactory->create('PostTag')->findOneBy(['id' => $id]);;
+				foreach ($postTag->postTagTranslation as $item) {
+					$item->delete();
+				}
+				$postTag->delete();
+				$this->app->dbAdapter->commit();
+			} catch (\Exception $e) {
+				$this->app->dbAdapter->rollBack();
+				throw $e;
+			}
+		}
+
+		return true;
+	}
 }
