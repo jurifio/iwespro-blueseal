3e81d15332fa7dd434ff61859187c67d7a419344
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-05-05 10:26:42 +0200

    sistemato logo

--- assets/js/blog_edit.js
+++ assets/js/blog_edit.js
@@ -1,6 +1,11 @@
 $('textarea.summer').summernote({
     lang: "it-IT",
-    height: 300
+    height: 300,
+	fontNames: [
+		'Serif', 'Sans', 'Arial', 'Arial Black', 'Courier',
+		'Courier New', 'Comic Sans MS', 'Helvetica', 'Impact', 'Lucida Grande',
+		'Sacramento'
+	],
 });
 
 $('[data-toggle="popover"]').popover();
