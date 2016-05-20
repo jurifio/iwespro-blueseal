d4b8ba986caeac9c1320a73a3ac7e133a2bd81cd
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-05-20 17:49:50 +0200

    fix blog

--- assets/js/blog_edit.js
+++ assets/js/blog_edit.js
@@ -1,45 +1,45 @@
-$(document).on('ready', function() {
-	var summer = $('textarea.summer');
-	summer.summernote({
-		lang: "it-IT",
-		height: 300,
-		fontNames: [
-			'Arial',
-			'Arial Black',
-			'Comic Sans MS',
-			'Courier',
-			'Courier New',
-			'Helvetica',
-			'Impact',
-			'Lucida Grande',
-			'Raleway',
-			'Serif',
-			'Sans',
-			'Sacramento',
-			'Tahoma',
-			'Times New Roman',
-			'Verdana'
-		],
-		fontNamesIgnoreCheck: ['Raleway']
-	});
+var summer = $('textarea.summer');
+summer.summernote({
+	lang: "it-IT",
+	height: 300,
+	fontNames: [
+		'Arial',
+		'Arial Black',
+		'Comic Sans MS',
+		'Courier',
+		'Courier New',
+		'Helvetica',
+		'Impact',
+		'Lucida Grande',
+		'Raleway',
+		'Serif',
+		'Sans',
+		'Sacramento',
+		'Tahoma',
+		'Times New Roman',
+		'Verdana'
+	],
+	onImageUpload: function() {},
+	fontNamesIgnoreCheck: ['Raleway']
+});
 
-	summer.on('summernote.image.upload', function(we, files) {
-		// upload image to server and create imgNode...
-		var data = new FormData();
-		data.append("file", files[0]);
-		$.ajaxForm({
-			url: '/blueseal/xhr/BlogPostPhotoUploadAjaxController',
-			type: 'POST',
-			formAutofill: false
-		},data).done(function (result) {
-			var i = new Image;
-			i.src = result;
-			summer.summernote('insertNode', i);
-		}).fail(function (result) {
-			console.log('fail');
-		});
+summer.on('summernote.image.upload', function(we, files) {
+	// upload image to server and create imgNode...
+	var data = new FormData();
+	data.append("file", files[0]);
+	$.ajaxForm({
+		url: '/blueseal/xhr/BlogPostPhotoUploadAjaxController',
+		type: 'POST',
+		formAutofill: false
+	},data).done(function (result) {
+		var i = new Image;
+		i.src = result;
+		summer.summernote('insertNode', i);
+	}).fail(function (result) {
+		console.log('fail');
 	});
 });
+
 
 $('[data-toggle="popover"]').popover();
 
