0cc2adadcc88ffd1787b428fa48994d7e61ec595
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-05-25 10:28:32 +0200

    fix Datatables, da finire di fixare

--- business/CDataTables.php
+++ business/CDataTables.php
@@ -208,31 +208,50 @@ class CDataTables
         }
 
         if($count != 'full'){
-            $columns = [];
+            $columnsFilter = [];
             foreach ($this->columns as $idx => $column) {
                 if ($column['searchable'] == true) {
                     if($this->search){
-                        $search[] = $column['name']." RLIKE ?";
-                        $this->params[] = $this->search;
+                        $search['cols'][] = $column['name']." RLIKE ?";
+                        $search['params'][] = $this->search;
                     }
-                    if(!empty($column['search']) && ("extId" !== $column['name']) && ( "extSkuId" !== $column['name'])){ // @TODO: l'esclusione dei campi extId e extSkuId (che vengono sempre passati, anche se non ci sono le colonne corrispondenti nelle tabelle), crea sicuramente problemi da qualche parte.
-                        $columns[] = $column['name']." RLIKE ?";
-                        $this->params[] = $this->likeSearch($column['search']);
+                    if(!empty($column['search'])){
+	                    $search['cols'][] = $column['name']." RLIKE ?";
+	                    $search['params'][] = $this->search;
                     }
+	                if(!empty($column['filter'])) {
+		                $columnsFilter['cols'][] = $column['name']." RLIKE ?";
+		                $columnsFilter['params'][] = $this->likeSearch($column['filter']);
+	                }
                 }
             }
             if($this->search){
                 foreach($this->keys as $key){
-                    $search[] = $key." RLIKE ?";
-                    $this->params[] = $this->search;
+	                $search['cols'][] = $key." RLIKE ?";
+	                $search['params'][] = $this->search;
                 }
             }
         }
 
-        $conditions = empty($conditions) ? " 1=1 " : implode(' AND ' , $conditions );
-        $search = empty($search) ? " 1=1 " : ' ( '.implode(' OR ',$search).' ) ';
-        $columns = empty($columns) ? " 1=1 " : ' ( ' . implode(' AND ' , $columns) . ' ) ';
-        $this->where = " WHERE ".$conditions." AND ".$search . ' AND ' . $columns;
+        $conditionsWhere = empty($conditions) ? " 1=1 " : implode(' AND ' , $conditions );
+
+	    if(!empty($search['cols'])) {
+		    $searchWhere = ' ( '.implode(' OR ',($search['cols'])).' ) ';
+		    //MISTERO DELLA FEDE
+		    array_push($this->params,...$search['params']);
+	    } else {
+		    $searchWhere = " 1=1 ";
+	    }
+
+	    if(!empty($columnsFilter['cols'])) {
+		    $columnsFilterWhere = ' ( ' . implode(' AND ' , $columnsFilter['cols']) . ' ) ';
+		    //MISTERO DELLA FEDE
+		    array_push($this->params,...$columnsFilter['params']);
+	    } else {
+		    $columnsFilterWhere = " 1=1 ";
+	    }
+        $this->where = " WHERE ".$conditionsWhere." AND ".$searchWhere . ' AND ' . $columnsFilterWhere;
+
         return $this->where;
     }
 
@@ -282,7 +301,7 @@ class CDataTables
     {
         foreach($columns as $column){
             $key = (isset($column['name']) && $column['name'] != '') ? $column['name'] : $column['data'];
-            $this->createColumn($key,$column['orderable'],$column['searchable'],$column['search']['value'],null);
+            $this->createColumn($key,$column['orderable'],$column['searchable'],null,null,$column['search']['value']);
         }
     }
 
@@ -316,16 +335,17 @@ class CDataTables
         $this->offset= isset($dtData['start']) ? $dtData['start'] : 0;
     }
 
-    /**
-     * @param $column
-     * @param bool $sortable
-     * @param bool $searchable
-     * @param null $search
-     * @param null $permission
-     */
-    protected function createColumn($column, $sortable = true, $searchable = true, $search = null, $permission = null)
+	/**
+	 * @param $column
+	 * @param bool $sortable
+	 * @param bool $searchable
+	 * @param null $search
+	 * @param null $permission
+	 * @param null $filter
+	 */
+    protected function createColumn($column, $sortable = true, $searchable = true, $search = null, $permission = null, $filter = null)
     {
-        $this->columns[] = ["name"=>$column,"sortable"=>filter_var($sortable, FILTER_VALIDATE_BOOLEAN),"searchable"=>filter_var($searchable, FILTER_VALIDATE_BOOLEAN),"search"=>$search,"permission"=>$permission,];
+        $this->columns[] = ["name"=>$column,"sortable"=>filter_var($sortable, FILTER_VALIDATE_BOOLEAN),"searchable"=>filter_var($searchable, FILTER_VALIDATE_BOOLEAN),"search"=>$search,"permission"=>$permission,"filter"=>$filter];
     }
 
     /**
