115ce9737aed7c915f5fe493be144fea582f0997
Enrico Pascucci <enrico@iwes.it>
2016-05-30 12:27:42 +0200

    modificato il numero di dettagli visualizzati di default in "dettagli prodotto"

--- assets/js/datatables.init.js
+++ assets/js/datatables.init.js
@@ -255,7 +255,7 @@
 			}
 		],
 		lengthMenu: [100, 200, 500, 1000, 3000, 5000],
-		displayLength: 5000
+		displayLength: 3000
 	});
 	tableSetup.detail_translate_list = $.extend({}, tableSetup.common, {
 		order: [[0, "asc"]],
