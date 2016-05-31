1943aafc4540885c09910dc54df24d76b583b175
Enrico Pascucci <enrico@iwes.it>
2016-05-31 15:56:08 +0200

    pagina creata (clonando i dettagli)

---
+++ assets/js/product_size_list.js
@@ -0,0 +1,218 @@
+$(document).on('bs.manage.detail', function () {
+
+    var bsModal = $('#bsModal');
+    var dataTable = $('.dataTable').DataTable();
+    var header = $('.modal-header h4');
+    var body = $('.modal-body');
+    var loader = body.html();
+    var cancelButton = $('.modal-footer .btn-default');
+    var okButton = $('.modal-footer .btn-success');
+
+    var getVarsArray = [];
+    var selectedRows = $('.table').DataTable().rows('.selected').data();
+    var selectedRowsCount = selectedRows.length;
+
+    if (selectedRowsCount < 1) {
+	    new Alert({
+		    type: "warning",
+		    message: "Devi selezionare almeno un dettaglio da unire"
+	    }).open();
+	    return false;
+    }
+
+    var i = 0;
+    $.each(selectedRows, function (k, v) {
+        var rowId = v.DT_RowId.split('__');
+        getVarsArray[i] = rowId[0] + i + '=' + rowId[1];
+        i++;
+    });
+
+    var getVars = getVarsArray.join('&');
+
+    var result = {
+        status: "ko",
+        bodyMessage: "Errore di caricamento, controlla la rete",
+        okButtonLabel: "Ok",
+        cancelButtonLabel: null
+    };
+
+    header.html('Unione dettagli');
+
+    $.ajax({
+        url: "/blueseal/xhr/DetailManager",
+        type: "GET",
+        data: getVars
+    }).done(function (response) {
+        result = JSON.parse(response);
+        body.html(result.bodyMessage);
+        $(bsModal).find('table').addClass('table');
+        $('#productDetailId').change(function(){
+            var detName = $('#productDetailId option:selected').text();
+            $('#productDetailName').val(detName.substring(0, detName.indexOf('(')));
+        });
+        if (result.cancelButtonLabel == null) {
+            cancelButton.hide();
+        } else {
+            cancelButton.html(result.cancelButtonLabel);
+        }
+        bsModal.modal('show');
+        if (result.status == 'ok') {
+            okButton.html(result.okButtonLabel).off().on('click', function (e) {
+                var selected = $("#productDetailId").val();
+                var name = $("#productDetailName").val();
+                body.html(loader);
+	            Pace.ignore(function () {
+		            $.ajax({
+			            url: "/blueseal/xhr/DetailManager",
+			            type: "PUT",
+			            data: getVars + "&productDetailId=" + selected + "&productDetailName=" + name
+		            }).done(function (content) {
+			            body.html("Modifica eseguita con successo");
+		            }).fail(function () {
+			            body.html("Modifica non eseguita");
+		            }).always(function () {
+			            okButton.html('Ok');
+			            okButton.off().on('click', function () {
+				            bsModal.modal('hide');
+				            dataTable.ajax.reload();
+			            });
+		            });
+	            });
+            });
+            bsModal.modal();
+        }
+    });
+});
+
+$(document).on('bs.manage.detailproducts', function () {
+    var bsModal = $('#bsModal');
+    var dataTable = $('.dataTable').DataTable();
+    var header = $('.modal-header h4');
+    var body = $('.modal-body');
+    var loader = body.html();
+    var cancelButton = $('.modal-footer .btn-default');
+    var okButton = $('.modal-footer .btn-success');
+
+    var getVarsArray = [];
+    var selectedRows = $('.table').DataTable().rows('.selected').data();
+    var selectedRowsCount = selectedRows.length;
+
+    if (selectedRowsCount < 1 || selectedRowsCount > 1) {
+        header.html('Prodotti che usano il dettaglio');
+        okButton.html('Fatto').off().on('click', function () {
+            bsModal.modal('hide');
+            okButton.off();
+        });
+        cancelButton.remove();
+
+        $.ajaxForm({
+            type: "GET",
+            url: "#",
+            formAutofill: true
+        }, new FormData()).done(function (content) {
+            body.html("Deve essere selezionato un dettaglio alla volta");
+            bsModal.modal();
+        })
+    }
+
+    var i = 0;
+    $.each(selectedRows, function (k, v) {
+        var rowId = v.DT_RowId.split('__');
+        getVarsArray[i] = rowId[0] + i + '=' + rowId[1];
+        i++;
+    });
+
+    var getVars = getVarsArray.join('&');
+
+    header.html('Prodotti che usano il dettaglio');
+
+    $.ajax({
+        url: "/blueseal/xhr/ProductListAjaxDetail",
+        type: "GET",
+        data: getVars
+    }).done(function (response) {
+        body.html(response);
+        $(bsModal).modal("show");
+        okButton.html('Fatto').on('click', function () {
+            bsModal.modal('hide');
+            okButton.off();
+        });
+    });
+
+});
+
+$(document).on('bs.manage.deletedetails', function () {
+    var bsModal = $('#bsModal');
+    var dataTable = $('.dataTable').DataTable();
+    var header = $('.modal-header h4');
+    var body = $('.modal-body');
+    var loader = body.html();
+    var cancelButton = $('.modal-footer .btn-default');
+    var okButton = $('.modal-footer .btn-success');
+
+
+
+    var getVarsArray = [];
+    var selectedRows = $('.table').DataTable().rows('.selected').data();
+    var selectedRowsCount = selectedRows.length;
+
+    var i = 0;
+    $.each(selectedRows, function (k, v) {
+        var rowId = v.DT_RowId.split('__');
+        getVarsArray[i] = rowId[0] + i + '=' + rowId[1];
+        i++;
+    });
+
+    var getVars = getVarsArray.join('&');
+
+    console.log(getVars);
+    header.html('Cancellazione dei dettagli');
+
+
+    if (!selectedRowsCount) {
+        body.html("<p>Nessun prodotto selezionato.</p><p>Saranno eliminati tutti i dettagli non associati a prodotti o associati a prodotti senza disponibilità</p>" +
+            "L'azione non è reversibile.<br />" +
+            "Continuare?</p>");
+        $(bsModal).modal("show");
+    } else {
+            $.ajax({
+                url: "/blueseal/xhr/ProductListAjaxDetail",
+                type: "GET",
+                data: getVars
+            }).done(function (response) {
+                body.html("<p>Numero prodotti selezionati: " + selectedRowsCount + "</p><p>I dettagli selezionati sono associati ai seguenti prodotti:</p><p>" +
+                    response +
+                    "<p>L'azione non è reversibile.<br />" +
+                    "Continuare?</p>");
+                bsModal.modal();
+                okButton.html('Cancella').off().on('click', function() {
+                    okButton.off();
+                    $.ajax({
+                        url: "/blueseal/xhr/ProductListAjaxDetail",
+                        type: "DELETE",
+                        data: getVars
+                    }).done(function (response) {
+                        header.html('Cancellazione Dettagli');
+                        body.html(response);
+                        okButton.html('Fatto').off().on('click', function () {
+                            okButton.off();
+                            bsModal.modal("hide");
+                        });
+                        cancelButton.hide();
+                        if (0 > response.search("OOPS")) {
+                            $('table[data-datatable-name="product_detail_list"]').DataTable().draw();
+                        }
+                    });
+                });
+            });
+
+
+    }
+    cancelButton.html("Non voglio farlo").off().on('click', function () {
+        bsModal.modal('hide');
+        cancelButton.off();
+    });
+
+
+
+});
