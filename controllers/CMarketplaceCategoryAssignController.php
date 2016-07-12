4129fb8e4ad8a517814d8e3708562e34a18bd4d7
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-07-12 19:00:55 +0200

    creazione pannello per associazione categorie

---
+++ controllers/CMarketplaceCategoryAssignController.php
@@ -0,0 +1,37 @@
+<?php
+namespace bamboo\blueseal\controllers;
+
+use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\ecommerce\views\VBase;
+
+
+/**
+ * Class CMarketplaceCategoryAssignController
+ * @package bamboo\blueseal\controllers
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 12/07/2016
+ * @since 1.0
+ */
+class CMarketplaceCategoryAssignController extends ARestrictedAccessRootController
+{
+    protected $fallBack = "blueseal";
+    protected $pageSlug = "marketplace_category_assign";
+
+    public function get()
+    {
+        $view = new VBase(array());
+        $view->setTemplatePath($this->app->rootPath().$this->app->cfg()->fetch('paths','blueseal').'/template/marketplace_category_assign.php');
+
+        return $view->render([
+            'app' => new CRestrictedAccessWidgetHelper($this->app),
+            'page'=>$this->page,
+            'sidebar' => $this->sidebar->build()
+        ]);
+    }
+}
---
+++ controllers/ajax/CMarketplaceCategoryAssignAjaxController.php
@@ -0,0 +1,57 @@
+<?php
+namespace bamboo\blueseal\controllers\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\intl\CLang;
+
+/**
+ * Class CMarketplaceCategoryAssignAjaxController
+ * @package bamboo\blueseal\controllers\ajax
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 12/07/2016
+ * @since 1.0
+ */
+class CMarketplaceCategoryAssignAjaxController extends AAjaxController
+{
+    protected $urls = [];
+    protected $authorizedShops = [];
+    protected $em;
+
+    public function get()
+    {
+        $datatable = new CDataTables('vBluesealMarketplaceCategory',['marketplaceId','marketplaceCategoryId'],$_GET);
+
+        $marketplaceCategories = $this->app->repoFactory->create('MarketplaceCategoryLookup')->em()->findBySql($datatable->getQuery(),$datatable->getParams());
+        $count = $this->app->repoFactory->create('MarketplaceCategoryLookup')->em()->findCountBySql($datatable->getQuery(true), $datatable->getParams());
+        $totalCount = $this->app->repoFactory->create('MarketplaceCategoryLookup')->em()->findCountBySql($datatable->getQuery('full'), $datatable->getParams());
+
+        $okManage = $this->app->getUser()->hasPermission('/admin/product/edit');
+
+        $response = [];
+        $response ['draw'] = $_GET['draw'];
+        $response ['recordsTotal'] = $totalCount;
+        $response ['recordsFiltered'] = $count;
+        $response ['data'] = [];
+
+        $i = 0;
+
+        foreach($marketplaceCategories as $val){
+
+            $response['data'][$i]["DT_RowId"] = 'row__'.$val->marketplaceId.'-'.$val->marketplaceCategoryId;
+            $response['data'][$i]['marketplace'] = $val->marketplace->name;
+            $response['data'][$i]['marketplaceCategory'] = $val->name;
+            $response['data'][$i]['marketplaceCategoryDescription'] = $val->path;
+            $response['data'][$i]['internalCategory'] = $okManage ? $val->productCategoryId : 'ciccia';//'<a data-toggle="tooltip" title="modifica" data-placement="right" href="'.$modifica.'?id='.$val->id.'">'.$val->name.'</a>' : $val->name;
+
+            $i++;
+        }
+
+        return json_encode($response);
+    }
+}
