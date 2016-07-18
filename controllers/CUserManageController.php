9e17d0d5729979dcda45c96e8c3ff13d55d80f8c
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-07-18 15:02:59 +0200

    inserimento utenti

--- controllers/CUserManageController.php
+++ controllers/CUserManageController.php
@@ -1,6 +1,7 @@
 <?php
 namespace bamboo\blueseal\controllers;
 
+use bamboo\core\base\CToken;
 use bamboo\core\theming\CRestrictedAccessWidgetHelper;
 use bamboo\ecommerce\views\VBase;
 
@@ -33,6 +34,32 @@ class CUserManageController extends ARestrictedAccessRootController
 
     public function post()
     {
-		var_dump($this->app->router->request()->getRequestData());
+    	$user = $this->app->repoFactory->create('User')->getEmptyEntity();
+	    $user->email = $this->app->router->request()->getRequestData('user_email');
+	    $user->password = password_hash($this->app->router->request()->getRequestData('user_password'), PASSWORD_BCRYPT);
+	    $user->registrationEntryPoint = 'backoffice';
+	    $user->isActive = 1;
+	    $user->isDeleted = 0;
+	    $user->id = $user->insert();
+
+	    $userD = $this->app->repoFactory->create('UserDetails')->getEmptyEntity();
+	    $userD->userId = $user->id;
+	    $userD->name = $this->app->router->request()->getRequestData('user_name');
+	    $userD->surname = $this->app->router->request()->getRequestData('user_surname');
+	    $userD->birthDate = $this->app->router->request()->getRequestData('user_birthdate');
+	    $userD->gender = $this->app->router->request()->getRequestData('user_gender');
+	    $userD->phone = $this->app->router->request()->getRequestData('user_phone');
+	    $userD->fiscalCode = $this->app->router->request()->getRequestData('user_fiscalCode');
+		$userD->insert();
+
+	    $userEmail = $this->app->repoFactory->create('UserEmail')->getEmptyEntity();
+	    $userEmail->userId = $user->id;
+	    $userEmail->address = $user->email;
+	    $userEmail->isPrimary = true;
+	    $userEmail->insert();
+
+	    $this->app->repoFactory->create('User')->persistRegistrationToken($user->id,(new CToken(64))->getToken(),time() + $this->app->cfg()->fetch('miscellaneous', 'confirmExpiration'));
+
+	    return $user->id;
     }
 }
