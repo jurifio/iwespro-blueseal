f8736abc67b334fc6444c9a57354066de81e28ed
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-07-18 11:34:46 +0200

    inserimento utenti

---
+++ controllers/CUserManageController.php
@@ -0,0 +1,38 @@
+<?php
+namespace bamboo\blueseal\controllers;
+
+use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\ecommerce\views\VBase;
+
+/**
+ * Class CUserListController
+ * @package bamboo\blueseal\controllers
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>, ${DATE}
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @since ${VERSION}
+ */
+class CUserManageController extends ARestrictedAccessRootController
+{
+    protected $fallBack = "blueseal";
+    protected $pageSlug = "user_add";
+
+    public function get()
+    {
+        $view = new VBase(array());
+        $view->setTemplatePath($this->app->rootPath().$this->app->cfg()->fetch('paths','blueseal').'/template/user_add.php');
+
+        return $view->render([
+            'app' => new CRestrictedAccessWidgetHelper($this->app),
+            'page' => $this->page,
+            'sidebar' => $this->sidebar->build()
+        ]);
+    }
+
+    public function post()
+    {
+		var_dump('ciccia');
+    }
+}
---
+++ template/user_add.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include "parts/head.php" ?>
+    <?php echo $app->getAssets(['ui','forms'], $page); ?>
+    <title>BlueSeal - <?php echo $page->getTitle(); ?></title>
+</head>
+<body class="fixed-header">
+<?php include "parts/sidebar.php"; ?>
+<div class="page-container">
+    <?php include "parts/header.php" ?>
+    <?php include "parts/operations.php" ?>
+
+    <div class="page-content-wrapper">
+        <div class="content sm-gutter">
+
+            <div class="container-fluid container-fixed-lg bg-white">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 alert-container closed"></div>
+                </div>
+            </div>
+
+            <div class="container-fluid">
+                <form id="form-project" enctype="multipart/form-data" role="form" action="" method="post" autocomplete="off">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-default clearfix">
+                                <div class="panel-heading clearfix">
+                                    <h5 class="m-t-10">Informazioni di base</h5>
+                                </div>
+                                <div class="panel-body clearfix">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group form-group-default required">
+                                                        <label for="user_name">Nome</label>
+                                                        <input autocomplete="off" type="text" id="user_name" class="form-control" name="user_name" value="" required="required">
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group form-group-default required">
+                                                        <label for="user_surname">Cognome</label>
+                                                        <input id="user_surname" autocomplete="off" type="text" class="form-control" name="user_surname" value="" required="required" />
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group form-group-default required">
+                                                        <label for="user_email">Email</label>
+                                                        <input id="user_email" autocomplete="off" type="text" class="form-control" name="user_email" value="" required="required" />
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group form-group-default required">
+                                                        <label for="user_password">Password</label>
+                                                        <input id="user_password" autocomplete="off" type="text" class="form-control" name="user_password" value="" required="required" />
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group form-group-default required">
+                                                        <label for="user_birthdate">Data di Nascita</label>
+                                                        <input id="user_birthdate" autocomplete="off" type="date" class="form-control" name="user_birthdate" value="" />
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group form-group-default required">
+                                                        <label for="user_gender">Sesso</label>
+                                                        <select id="user_gender" autocomplete="off" class="form-control" name="user_gender" required="required" >
+                                                            <option value=""></option>
+                                                            <option value="M">Maschile</option>
+                                                            <option value="F">Femminile</option>
+                                                        </select>
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php include "parts/footer.php" ?>
+</div>
+<?php include "parts/bsmodal.php"; ?>
+<?php include "parts/alert.php"; ?>
+<bs-toolbar class="toolbar-definition">
+    <bs-toolbar-group data-group-label="Gestione prodotti">
+        <bs-toolbar-button
+            data-tag="a"
+            data-icon="fa-floppy-o"
+            data-permission="/admin/product/add"
+            data-event="bs.product.add"
+            data-class="btn btn-default"
+            data-rel="tooltip"
+            data-title="Salva"
+            data-placement="bottom"
+            ></bs-toolbar-button>
+        <bs-toolbar-button
+            data-tag="a"
+            data-icon="fa-image"
+            data-permission="/admin/product/add"
+            data-event="bs.dummy.add"
+            data-class="btn btn-default"
+            data-rel="tooltip"
+            data-title="Dummy picture"
+            data-placement="bottom"
+            ></bs-toolbar-button>
+        <bs-toolbar-button
+            data-tag="a"
+            data-icon="fa-sitemap"
+            data-permission="/admin/product/add"
+            data-event="bs.category.add"
+            data-class="btn btn-default"
+            data-rel="tooltip"
+            data-title="Categorie"
+            data-placement="bottom"
+            ></bs-toolbar-button>
+        <bs-toolbar-button
+            data-tag="a"
+            data-icon="fa-tag"
+            data-permission="/admin/product/add"
+            data-event="bs.tag.add"
+            data-class="btn btn-default"
+            data-rel="tooltip"
+            data-title="Tag"
+            data-placement="bottom"
+            ></bs-toolbar-button>
+    </bs-toolbar-group>
+    <bs-toolbar-group data-group-label="Stato del prodotto">
+        <bs-toolbar-select
+            data-tag="select"
+            data-icon="fa-random"
+            data-permission="/admin/product/add"
+            data-rel="tooltip"
+            data-button="false"
+            data-placement="bottom"
+            data-class="btn btn-default"
+            data-name="Product_status"
+            data-title="Modifica stato"
+            data-event="bs.product.changestatus"
+            data-options='<?php echo json_encode($statuses); ?>'
+            ></bs-toolbar-select>
+    </bs-toolbar-group>
+</bs-toolbar>
+</body>
+</html>
