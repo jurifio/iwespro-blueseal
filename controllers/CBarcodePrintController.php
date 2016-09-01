edfd5cf8d848f13a2b94c253d75c906161f94129
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-09-01 11:45:01 +0200

    printing barcdes

---
+++ controllers/CBarcodePrintController.php
@@ -0,0 +1,51 @@
+<?php
+namespace bamboo\blueseal\controllers;
+
+use bamboo\core\base\CObjectCollection;
+use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\ecommerce\views\VBase;
+use bamboo\core\db\pandaorm\entities\CEntityManager;
+
+/**
+ * Class CCategoryListController
+ * @package bamboo\blueseal\controllers
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>, ${DATE}
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @since ${VERSION}
+ */
+class CBarcodePrintController extends ARestrictedAccessRootController
+{
+    protected $fallBack = "blueseal";
+    protected $pageSlug = "product_category_list";
+
+    public function get()
+    {
+        $view = new VBase(array());
+        $view->setTemplatePath($this->app->rootPath().$this->app->cfg()->fetch('paths','blueseal').'/template/barcode_print.php');
+
+        $this->app->vendorLibraries->load('barcode');
+        $generatorSVG = new \Picqer\Barcode\BarcodeGeneratorSVG();
+
+        $productSkus = new CObjectCollection();
+        $seen = [];
+        foreach ($this->app->router->request()->getRequestData('moviments') as $storageOperationId) {
+            $storageOperation = $this->app->repoFactory->create('StorageOperation')->findOneByStringId($storageOperationId);
+            foreach ($storageOperation->storageOperationLine as $storageOperationLine) {
+                if(array_search($storageOperationLine->productSku->printId(),$seen) === false) {
+                    $productSkus->add($storageOperationLine->productSku);
+                    $seen[] = $storageOperationLine->productSku->printId();
+                }
+            }
+        }
+
+        return $view->render([
+            'barcodeGenerator' => $generatorSVG,
+            'productSkus' => $productSkus
+        ]);
+    }
+}
--- template/barcode_print.php
+++ template/barcode_print.php
@@ -245,8 +245,7 @@
 <?php endif; ?>
         <div class="col-xs-6">
             <div class="col-xs-4" style="margin-top:10px">
-                <?php $generatorSVG = new Picqer\Barcode\BarcodeGeneratorSVG();
-                echo $generatorSVG->getBarcode($productSku->barcode,Picqer\Barcode\BarcodeGenerator::TYPE_CODE_128,2); ?>
+                <?php echo $barcodeGenerator->getBarcode($productSku->barcode,Picqer\Barcode\BarcodeGenerator::TYPE_CODE_128,2); ?>
             </div>
             <div class="col-xs-8 pull-left" style="border-right: 1px dotted #c0c0c0;">
                 <ul>
