a3709dffefd81185807423a50b0790b420212dce
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-09-08 13:14:13 +0200

    inzio scarico rapido

---
+++ controllers/CStorehouseFastOutgoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace bamboo\blueseal\controllers;
+
+use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\ecommerce\views\VBase;
+
+/**
+ * Class CProductSlimListController
+ * @package bamboo\blueseal\controllers
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 05/09/2016
+ * @since 1.0
+ */
+class CStorehouseFastOutgoController extends ARestrictedAccessRootController
+{
+    protected $fallBack = "blueseal";
+    protected $pageSlug = "storehouse_fast_outgo";
+
+    public function get()
+    {
+
+    }
+}
