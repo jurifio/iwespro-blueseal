9a105daff75b00d3eacc74eabad5a1264943be31
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-09-09 13:45:17 +0200

    fix cambio gruppo taglie

--- assets/js/button/bs.product.sizeGroup.change.js
+++ assets/js/button/bs.product.sizeGroup.change.js
@@ -1,7 +1,7 @@
 window.buttonSetup = {
     tag:"a",
     icon:"fa-magic",
-    permission:"/admin/product/edit",
+    permission:"/admin/product/edit&&allShops",
     event:"bs.manage.sizeGroups",
     class:"btn btn-default",
     rel:"tooltip",
