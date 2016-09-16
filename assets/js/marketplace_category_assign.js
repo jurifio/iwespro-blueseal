a92bfc310e6ea593e19a21fc906bc0afb19b850a
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-09-16 15:44:02 +0200

    fix pagina valore prodotti

--- assets/js/marketplace_category_assign.js
+++ assets/js/marketplace_category_assign.js
@@ -28,7 +28,7 @@ $('table[data-datatable-name]').on('draw.dt', function () {
 				var sel = $(v).selectize({
 					valueField: 'id',
 					labelField: 'value',
-					maxItems: 100,
+					maxItems: 250,
 					searchField: ['value'],
 					options: window.categories
 				});
--- template/brand_value_list.php
+++ template/brand_value_list.php
@@ -87,9 +87,7 @@
 <?php include "parts/bsmodal.php"; ?>
 <?php include "parts/alert.php"; ?>
 <bs-toolbar class="toolbar-definition">
-    <bs-toolbar-group data-group-label="Gestione prodotti">
 
-    </bs-toolbar-group>
 </bs-toolbar>
 </body>
 </html>
