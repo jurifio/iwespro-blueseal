4cb46511dc03944d553d1c82306d0dea87c872ba
Enrico Pascucci <enrico@iwes.it>
2016-10-20 18:41:47 +0200

    [blueseal] [modelli] inserimento di massa dalle liste dei prodotti

--- template/product_slim_list.php
+++ template/product_slim_list.php
@@ -142,6 +142,9 @@
         <bs-toolbar-button
             data-remote="bs.product.PriceEditForAllShop"
         ></bs-toolbar-button>
+        <bs-toolbar-button
+            data-remote="bs.product.model.insertIntoProducts"
+        ></bs-toolbar-button>
     </bs-toolbar-group>
     <bs-toolbar-group data-group-label="Gestione prezzi">
         <bs-toolbar-button
