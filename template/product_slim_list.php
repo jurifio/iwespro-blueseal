e8cd3df431fcc18257c1d42c33baecab785339e3
Enrico Pascucci <enrico@iwes.it>
2016-11-02 16:05:44 +0100

    [blueseal] nuovi nomi prodotto inseribili pure dalle liste prodotto

--- template/product_slim_list.php
+++ template/product_slim_list.php
@@ -145,6 +145,9 @@
         <bs-toolbar-button
             data-remote="bs.product.model.insertIntoProducts"
         ></bs-toolbar-button>
+        <bs-toolbar-button
+            data-remote="bs.product.name.insert"
+        ></bs-toolbar-button>
     </bs-toolbar-group>
     <bs-toolbar-group data-group-label="Gestione prezzi">
         <bs-toolbar-button
