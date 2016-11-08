17b1146c538ae25caf88f35ea626dc4e9d1ef493
Enrico Pascucci <enrico@iwes.it>
2016-11-08 18:51:49 +0100

    [core] nuovi eventi creati

---
+++ events/listeners/CSetReleaseDate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace bamboo\events\listeners;
+
+use bamboo\core\events\AEventListener;
+use bamboo\core\exceptions\BambooException;
+
+/**
+ * Class COtherTest
+ * @package bamboo\app\evtlisteners
+ */
+class CSetReleaseDate extends AEventListener
+{
+    public function run($eventName)
+    {
+        throw new BambooException('triggerato in ' . $eventName);
+    }
+}
