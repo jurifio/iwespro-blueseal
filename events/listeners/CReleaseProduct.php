955bcea6ae4b5c1bb36d2cc7341840f69820f145
Enrico Pascucci <enrico@iwes.it>
2016-11-15 12:15:39 +0100

    logging per il releaseDate dei prodotti unificate le classi che loggano il rilascio del prodotto

--- events/listeners/CReleaseProduct.php
+++ events/listeners/CReleaseProduct.php
@@ -13,20 +13,31 @@ class CReleaseProduct extends CLogging
 {
     public function run($eventName)
     {
-        $shp = $this->getParam('ShopHasProductE');
-        if (!$shp)  {
-            $shpR = \Monkey::app()->repoFactory->create('ShopHasProduct');
-            $sku = $this->getParam('ProductSku');
-            $shp = $shpR->findOneBy(['productId' => $sku->productId, 'productVariantId' => $sku->productVariantId, 'shopId']);
-        }
-
-        if (!$shp) {
-            $product = $this->getParam('ProductSku');
-            $shp = $product->shopHasProduct;
-        }
-
         $releaseDate = $this->getParam('releaseDate');
         if (!$releaseDate) $releaseDate = date('Y-m-d H:i:s');
-        $this->insertLogRow($eventName, null, $shp->getEntityName(), $shp->printId(), $releaseDate);
+        $shp = $this->getParam('ShopHasProductE');
+
+        if ($shp) {
+            if (!$shp->releaseDate) {
+                $shp->releaseDate = $releaseDate;
+                $shp->update();
+                $this->insertLogRow($eventName, null, $shp->getEntityName(), $shp->printId(), $releaseDate);
+            }
+        } else {
+            $product = $this->getParam('Product');
+            if (($product) && ($product->productPhoto->count())) {
+                foreach ($product->shopHasProduct as $shp) {
+                    $qty = 0;
+                    foreach($shp->productSku as $sku) {
+                        $qty += $sku->stockQty;
+                    }
+                    if (($qty) && (!$shp->releaseDate)) {
+                        $shp->releaseDate = $releaseDate;
+                        $shp->update();
+                        $this->insertLogRow($eventName, null, $shp->getEntityName(), $shp->printId(), $releaseDate);
+                    }
+                }
+            }
+        }
     }
 }
