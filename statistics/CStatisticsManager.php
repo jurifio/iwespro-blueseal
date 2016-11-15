a234521f4c2df7432f1fd8aef71cf9697f8d42a3
Enrico Pascucci <enrico@iwes.it>
2016-11-15 19:20:54 +0100

    ho da fare

--- statistics/CStatisticsManager.php
+++ statistics/CStatisticsManager.php
@@ -9,18 +9,93 @@
 namespace bamboo\statistics;
 
 
-class AStatisticsManager
+use bamboo\domain\entities\CStatisticsHeader;
+
+class CStatisticsManager
 {
     /**
      * @var array
+     * Ogni elemento dell'array ha come chiave il campo da usare come condizione e come valore un array avente
+     * elementi con chiave contenente l'etichetta da visualizzare e come valore, il valore da usare nelle condizioni
+     * della query
      */
-    private $collectionType = ['sum', 'medium', 'sumPrevious'];
+    private $filters = [];
 
     /**
-     * @var
+     * @var array in ingresso alle ricerche nelle entity
      */
-    private $queryResult;
+    private $selectedFilters = [];
+    /**
+     * @var null
+     */
+    private $outputField = null;
+    /**
+     * @var string DAY|HOUR|WEEK|MONTH|YEAR
+     */
+    private $timeUnit = null;
+    /**
+     * @var array
+     */
+    private $timeRange = ['start' => null, 'stop' => null];
+    /**
+     * @var string 3|6
+     */
+    protected $dataInputType = 0;
+    /**
+     * @var array
+     */
+    private $groupDataType = ['SUM', 'MEDIUM', 'SUM-PREVIOUS'];
 
-    public function retrieveData() {}
+    /**
+     * @var CStatisticsHeader
+     */
+    private $computedStatistics;
+
+    protected function getComputedStatistics($tableName, $groupDataType, $dateGroup, $keyField) {
+        $shR = \Monkey::app()->repoFactory->create('StatisticsHeader');
+        $conditions = [
+            'tableName' => $tableName,
+            'groupDataType' => $groupDataType,
+            'dateGroup' => $dateGroup,
+            //'dateStart' => $dateStart,
+            //'dateEnd' => $dateEnd,
+            'keyField' => $keyField,
+            'graphUnitName'
+        ];
+        //if ($graphUnitName) $conditions['graphUnitName'] = $graphUnitName;
+        //if ($graphValueName) $conditions['graphValueName'] = $graphValueName;
+        //if ($fullScaleValue) $conditions['fullScaleValue'] = $fullScaleValue;
+        $sh = $shR->findOneBy($conditions);
+        return $sh;
+    }
+
+    protected function createNewStatisticsData($tableName, $keyField, $selectedFilters, $groupDataType, $dateGroup, $fromDate = null, $toDate = null) {
+        $fromDate = (false !== date_parse($fromDate)) ? $fromDate : $this->firstDate();
+        $toDate = (false !== date_parse($fromDate) or ())
+        if ($this->isDbTable($tableName)) {
+            foreach()
+            $repo = \Monkey::app()->repoFactory->create($tableName);
+            $oc = $repo->findBy($selectedFilters);
+
+
+            } else {
+
+        }
+    }
+
+    protected function firstDate() {
+        return \Monkey::app()->dbAdapter->query('SELECT min(time) FROM _Dates WHERE 1')->fetch();
+    }
+
+    /**
+     * @param $tableName
+     * @return bool
+     */
+    public function isDbTable($tableName) {
+        $query = "SHOW TABLES LIKE '" . htmlentities($tableName) . "'";
+        $res = \Monkey::app()->dbAdapter->query($query, [])->fetchAll();
+        if (count($res)) return true;
+        return false;
+    }
 }
 ?>
