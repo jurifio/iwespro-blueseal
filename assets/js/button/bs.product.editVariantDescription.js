630ba85bbfe297446e96378d0b885ea689ed7c23
Enrico Pascucci <enrico@iwes.it>
2016-11-21 16:48:49 +0100

    nuova colonna nella lista prodotti e funzionalità per modificare i nomi colore del produttore

---
+++ assets/js/button/bs.product.editVariantDescription.js
@@ -0,0 +1,67 @@
+window.buttonSetup = {
+    tag:"a",
+    icon:"fa-paint-brush",
+    permission:"/admin/product/edit",
+    event:"bs.product.editVariantDescription",
+    class:"btn btn-default",
+    rel:"tooltip",
+    title:"Aggiorna nome colore produttore",
+    placement:"bottom",
+    toggle:"modal"
+};
+
+$(document).on('bs.product.editVariantDescription', function (e) {
+    e.preventDefault();
+
+    var selectedRows = $('.table').DataTable().rows('.selected').data();
+    var selectedRowsCount = selectedRows.length;
+
+    if (0 == selectedRowsCount) {
+        new Alert({
+            type: "warning",
+            message: "Devi selezionare almeno un prodotto"
+        }).open();
+        return false;
+    }
+
+    var i = 0;
+    var row = [];
+    $.each(selectedRows, function (k, v) {
+        row[i] = v.DT_RowId;
+        i++;
+    });
+
+    modal = new $.bsModal('Cambia nome produttore',
+        {
+            body: '<form id="detailAdd"><div class="form-group">' +
+            '<label>Inserisci il nome variante da assegnare ai prodotti selezionati</label>' +
+            '<input type="text" class="form-control new-dett-ita" name="newColorPrice" />' +
+            '</div></form>',
+            isCancelButton: true,
+            okButtonEvent: function() {
+                $.ajax({
+                    url: '/blueseal/xhr/EditVariantDescription',
+                    method: 'post',
+                    data: {
+                        codes: row,
+                        colorNameManufacturer: $('[name="newColorPrice"]').val()
+                    }
+                }).done(
+                    function(res){
+                        modal.writeBody(res);
+                        modal.hideCancelBtn();
+                        modal.setOkEvent(function(){
+                            modal.hide();
+                        });
+                    }).fail(function(res){
+                        console.log(res);
+                        modal.writeBody('OOPS! ' + res.responseText);
+                        modal.hideCancelBtn();
+                        modal.setOkEvent(function(){
+                            modal.hide();
+                        });
+                    });
+            }
+        }
+    );
+});
--- template/product_list.php
+++ template/product_list.php
@@ -183,6 +183,9 @@
         <bs-toolbar-button
             data-remote="bs.product.marketplace.publish"
         ></bs-toolbar-button>
+        <bs-toolbar-button
+            data-remote="bs.product.editVariantDescription"
+        ></bs-toolbar-button>
     </bs-toolbar-group>
     <bs-toolbar-group data-group-label="Gestione prezzi">
         <bs-toolbar-button
