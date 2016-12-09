04795dbc6702f9e4b7c4790b8809abc1d11f90fa
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-12-09 11:19:28 +0100

    cambio password utenti

---
+++ assets/js/button/bs.user.password.change.js
@@ -0,0 +1,70 @@
+window.buttonSetup = {
+    tag: "a",
+    icon: "fa-unlock-alt",
+    permission: "/admin/product/edit&&allShops",
+    event: "bs.user.password.change",
+    class: "btn btn-default",
+    rel: "tooltip",
+    title: "Tagga prodotti",
+    placement: "bottom"
+};
+
+$(document).on('bs.user.password.change', function () {
+
+    var bsModal = $('#bsModal');
+    var header = $('.modal-header h4');
+    var body = $('.modal-body');
+    var cancelButton = $('.modal-footer .btn-default');
+    var okButton = $('.modal-footer .btn-success');
+
+    header.html('Cambia Password');
+
+    var selectedRows = $('.table').DataTable().rows('.selected').data();
+    var selectedRowsCount = selectedRows.length;
+
+    if (selectedRowsCount != 1) {
+        new Alert({
+            type: "warning",
+            message: "Devi selezionare un Utente per poter cambiare la password"
+        }).open();
+        return false;
+    }
+
+    var userId;
+    $.each(selectedRows, function (k, v) {
+        userId = v.DT_RowId;
+    });
+
+    body.html('<img src="/assets/img/ajax-loader.gif" />');
+
+    Pace.ignore(function () {
+        $.ajax({
+            url: '/blueseal/xhr/ChangeUserPassword',
+            type: "get"
+        }).done(function (response) {
+            body.html('<input id="changePwdUser" value="'+response+'" label="Nuova Password" aria-label="Nuova Password">');
+
+            okButton.html('Ok').off().on('click', function () {
+                okButton.on('click', function () {
+                    bsModal.modal('hide')
+                });
+
+                body.html('<img src="/assets/img/ajax-loader.gif" />');
+                $.ajax({
+                    url: '/blueseal/xhr/ChangeUserPassword',
+                    type: 'put',
+                    data: {
+                        userId: userId,
+                        password: $('#changePwdUser').val()
+                    }
+                }).done(function (response) {
+                    body.html('<p>Password Cambiata: '+$('#changePwdUser').val()+'</p>');
+                }).fail(function (response) {
+                    body.html('<p>Errore</p>');
+                });
+            });
+        });
+    });
+
+    bsModal.modal();
+});
