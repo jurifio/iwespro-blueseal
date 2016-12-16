5cc643f2f4b8a6ccc1a3ef0f648a80322bedc37f
Enrico Pascucci <enrico@iwes.it>
2016-12-16 16:55:52 +0100

    pagina per sistemare i prodotti con i gruppi taglie scassati

---
+++ controllers/CFixProductGroupSizeController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace bamboo\blueseal\controllers;
+
+use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\ecommerce\views\VBase;
+
+/**
+ * Class CProductListController
+ * @package bamboo\blueseal\controllers
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>, ${DATE}
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @since ${VERSION}
+ */
+class CFixProductGroupSizeController extends ARestrictedAccessRootController
+{
+    protected $fallBack = "blueseal";
+    protected $pageSlug = "product_incomplete_import";
+
+    public function get()
+    {
+        $view = new VBase(array());
+        $view->setTemplatePath($this->app->rootPath().$this->app->cfg()->fetch('paths','blueseal').'/template/product_incomplete_import.php');
+
+        /** LOGICA */
+        $bluesealBase = $this->app->baseUrl(false).'/blueseal/';
+        $pageURL = $bluesealBase."prodotti";
+        $modifica = $bluesealBase."prodotti/aggiungi";
+        $carica = $bluesealBase."skus";
+        $foto = $bluesealBase."carica_foto.php";
+        $importa_foto_cartechini = $bluesealBase."import/cartechiniphoto/total_import_cartechini.php";
+        $importa_foto_newage = $bluesealBase."import/cartechiniphoto/total_import_newage.php";
+        $dummyUrl = $this->app->cfg()->fetch('paths','dummyUrl');
+
+        if ($this->app->getUser()->hasPermission('allShops')) {
+
+        } else {
+            $res = $this->app->dbAdapter->select('UserHasShop',['userId'=>$this->app->getUser()->getId()])->fetchAll();
+            $shops = [];
+            foreach($res as $val){
+                $shops[] = $val['shopId'];
+            }
+        }
+
+        $prodotti = null;
+
+        $em = $this->app->entityManagerFactory->create('ProductStatus');
+        $productStatuses = $em->findAll('limit 99','');
+
+        $statuses = [];
+        foreach($productStatuses as $status){
+            $statuses[$status->code] = $status->name;
+        }
+
+        //img count
+        $imgs = 0;
+        foreach($this->app->theme->getImageSizes() as $v){
+            $imgs++;
+        }
+
+        return $view->render([
+            'app' => new CRestrictedAccessWidgetHelper($this->app),
+            'dummyUrl' =>$dummyUrl,
+            'statuses' =>$statuses,
+            'modifica' =>$modifica,
+            'carica' =>$carica,
+            'foto' =>$foto,
+            'imgs'=>$imgs,
+            'bluesealBase' =>$bluesealBase,
+            'cm'=>$this->app->categoryManager,
+            'pageURL'=>$pageURL,
+            'prodotti'=>$prodotti,
+            'importa_foto_cartechini' => $importa_foto_cartechini,
+            'importa_foto_newage' => $importa_foto_newage,
+            'page' => $this->page,
+            'sidebar' => $this->sidebar->build()
+        ]);
+    }
+}
---
+++ controllers/ajax/CFixProductGroupSizeAjaxController.php
@@ -0,0 +1,149 @@
+<?php
+namespace bamboo\blueseal\controllers\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\intl\CLang;
+use bamboo\domain\entities\CProduct;
+
+/**
+ * Class CProductListAjaxController
+ * @package bamboo\blueseal\controllers\ajax
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>, ${DATE}
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @since ${VERSION}
+ */
+class CFixProductGroupSizeAjaxController extends AAjaxController
+{protected $urls = [];
+    protected $authorizedShops = [];
+    protected $em;
+
+    /**
+     * @param $action
+     * @return mixed
+     */
+    public function createAction($action)
+    {
+        $this->app->setLang(new CLang(1,'it'));
+        $this->urls['base'] = $this->app->baseUrl(false)."/blueseal/";
+        $this->urls['page'] = $this->urls['base']."prodotti";
+        $this->urls['dummy'] = $this->app->cfg()->fetch('paths','dummyUrl');
+
+        if ($this->app->getUser()->hasPermission('allShops')) {
+
+        } else{
+            $res = $this->app->dbAdapter->select('UserHasShop',['userId'=>$this->app->getUser()->getId()])->fetchAll();
+            foreach($res as $val) {
+                $this->authorizedShops[] = $val['shopId'];
+            }
+        }
+
+        $this->em = new \stdClass();
+        $this->em->products = $this->app->entityManagerFactory->create('Product');
+
+        return $this->{$action}();
+    }
+
+    public function get()
+    {
+
+        /** @var $mysql CMySQLAdapter **/
+        /** @var $em CEntityManager **/
+
+        $bluesealBase = $this->app->baseUrl(false)."/blueseal/";
+        $dummyUrl = $this->app->cfg()->fetch('paths','dummyUrl');
+        $shops = [];
+
+
+        $query =
+            "SELECT DISTINCT p.id as id, p.productVariantId as productVariantId FROM Product as p
+JOIN ProductSku as ps On p.id = ps.productId AND p.productVariantId = ps.productVariantId
+  join Shop ON ps.shopId = Shop.id
+where p.productSizeGroupId NOT IN (SELECT productSizeGroupId FROM ProductSizeGroupHasProductSize WHERE ProductSizeGroupHasProductSize.productSizeId = ps.productSizeId)
+AND p.productStatusId in (5,6,11) group by p.productVariantId";
+
+        $datatable = new CDataTables($query,['id','productVariantId'],$_GET, true);
+        if(!empty($this->authorizedShops)){
+            $datatable->addCondition('shopId',$this->authorizedShops);
+        }
+
+        $prodotti = $this->app->repoFactory->create('Product')->em()->findBySql($datatable->getQuery(),$datatable->getParams());
+        $count = $this->em->products->findCountBySql($datatable->getQuery(true), $datatable->getParams());
+        $totlalCount = $this->em->products->findCountBySql($datatable->getQuery('full'), $datatable->getParams());
+
+        $modifica = $bluesealBase."prodotti/modifica";
+
+        $response = [];
+        $response ['draw'] = $_GET['draw'];
+        $response ['recordsTotal'] = $totlalCount;
+        $response ['recordsFiltered'] = $count;
+        $response ['data'] = [];
+        $i = 0;
+        foreach($prodotti as $val){
+
+            $cats = [];
+            foreach($val->productCategoryTranslation as $cat){
+                $path = $this->app->categoryManager->categories()->getPath($cat->productCategoryId);
+                unset($path[0]);
+                $cats[] = '<span>'.implode('/',array_column($path, 'slug')).'</span>';
+            }
+            $shops = [];
+            foreach($val->shop as $shop){
+                $shops[] = $shop->name;
+            }
+
+            $tools = "";
+            $tools .= $this->app->getUser()->hasPermission("/admin/product/list") ? '<span class="tools-spaced"><a href="'.$bluesealBase.'printAztecCode.php?src='.base64_encode($val->id.'-'.$val->productVariantId.'__'.$val->productBrand->name.' - '.$val->itemno.' - '.$val->productVariant->name).'" target="_blank"><i class="fa fa-barcode"></i></a></span>' : '<span class="tools-spaced"><i class="fa fa-barcode"></i></span>';
+            $tools .= $this->app->getUser()->hasPermission('/admin/product/edit') ? '<span class="tools-spaced"><a href="'.$modifica.'?id='.$val->id.'&productVariantId='.$val->productVariantId.'"><i class="fa fa-pencil-square-o"></i></a></span>' : '<span class="tools-spaced"><i class="fa fa-pencil-square-o"></i></span>';
+
+            /** @var CProduct $val */
+
+            $creationDate = new \DateTime($val->creationDate);
+
+            $response['aaData'][$i]["DT_RowId"] = $val->printId();
+            $response['aaData'][$i]["DT_RowClass"] = 'colore';
+            $response['aaData'][$i]["productCode"] = $this->app->getUser()->hasPermission('/admin/product/edit') ? '<span class="tools-spaced"><a href="'.$modifica.'?id='.$val->id.'&productVariantId='.$val->productVariantId.'">'.$val->id.'-'.$val->productVariantId.'</a></span>' : $val->id.'-'.$val->productVariantId;
+            $response['aaData'][$i]["shop"] = implode(',',$shops);
+            $response['aaData'][$i]["code"] = $val->itemno.' # '.$val->productVariant->name;
+            $macroname = explode("_", explode("-", $val->productSizeGroup->macroName)[0])[0];
+            $response['aaData'][$i]["sizeGroup"] = '<span class="small">' . $val->productSizeGroup->locale . '-' . $macroname . '</span>';
+            $response['aaData'][$i]["dummyPicture"] = '<img width="80" src="'.$val->getDummyPictureUrl().'">';
+            $response['aaData'][$i]["brand"] = isset($val->productBrand) ? $val->productBrand->name : "";
+            //$response['aaData'][$i][$k++] = implode(',<br>',$cats);
+            $response['aaData'][$i]["status"] = $val->productStatus->name;
+            $response['aaData'][$i]["creationDate"] = $creationDate->format('d-m-Y H:i');
+            $response['aaData'][$i]["problems"] = $this->parseProblem($val);
+
+            $i++;
+        }
+        return json_encode($response);
+    }
+
+    /**
+     * @param CProduct $product
+     * @return string
+     */
+    private function parseProblem(CProduct $product)
+    {
+        $message = "[500] Size Mismatch";
+        $sizes = $this->app->dbAdapter->query('SELECT size FROM DirtyProduct dp, DirtySku ds where dp.id = ds.dirtyProductId and dp.productId = ? and dp.productVariantId = ?',[$product->id,$product->productVariantId])->fetchAll();
+        $newSize = [];
+        foreach($sizes as $size){
+            $newSize[] = $size['size'];
+        }
+        $message .= " ".implode('-',$newSize);
+        return '<span>'.$message.'</span>';
+    }
+
+    public function post(){
+        throw new \Exception();
+    }
+
+    public function delete(){
+        throw new \Exception();
+    }
+}
