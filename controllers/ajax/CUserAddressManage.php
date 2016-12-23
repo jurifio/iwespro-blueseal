cb32ed4fdb1cfda04c952cc9b24afbfcc67dff22
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2016-12-23 12:53:07 +0100

    inserimento indirizzo

--- controllers/ajax/CUserAddressManage.php
+++ controllers/ajax/CUserAddressManage.php
@@ -16,6 +16,9 @@ namespace bamboo\blueseal\controllers\ajax;
  */
 class CUserAddressManage extends AAjaxController
 {
+    /**
+     * @return string
+     */
 	public function get()
 	{
 		$list = [];
@@ -28,9 +31,24 @@ class CUserAddressManage extends AAjaxController
 		return json_encode($list);
 	}
 
+    /**
+     * @return mixed
+     */
 	public function post()
     {
         $data = $this->app->router->request()->getRequestData();
-
+        $userAddress = $this->app->repoFactory->create('UserAddress')->getEmptyEntity();
+        $userAddress->userId = $data['user_id'];
+        $userAddress->name = $data['user_address_name'];
+        $userAddress->surname = $data['user_address_surname'];
+        $userAddress->phone = $data['user_address_phone'];
+        $userAddress->countryId = $data['user_address_country'];
+        $userAddress->address = $data['user_address_address'];
+        $userAddress->extra = $data['user_address_address2'];
+        $userAddress->postcode = $data['user_address_postcode'];
+        $userAddress->province = $data['user_address_province'];
+        $userAddress->city = $data['user_address_city'];
+        $userAddress->isBilling = 1;
+        return $userAddress->insert();
     }
 }
