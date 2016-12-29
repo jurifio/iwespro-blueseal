b7aad49e90c02740e9d3a98d85c9aaa888329a76
Enrico Pascucci <enrico@iwes.it>
2016-12-29 12:53:21 +0100

    minifix

--- template/friend_order_list.php
+++ template/friend_order_list.php
@@ -30,8 +30,8 @@
                                data-controller="FriendOrderListAjaxController"
                                data-url="<?php echo $app->urlForBluesealXhr() ?>"
                                data-inner-setup="true"
-                               data-
-                               data-length-menu="50, 100, 200"
+                               data-length-menu-setup="50, 100, 200, 500"
+                               data-display-length="100">
                                id="orderTable">
                             <thead>
                             <tr>
