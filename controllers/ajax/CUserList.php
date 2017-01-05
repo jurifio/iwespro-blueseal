012fe457b242670b82ed3c5ac9e5dd0caa505360
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-01-05 19:03:11 +0100

    messo l'elenco utenti desc

--- controllers/ajax/CUserList.php
+++ controllers/ajax/CUserList.php
@@ -19,7 +19,7 @@ class CUserList extends AAjaxController
 	public function get()
 	{
 		$list = [];
-		foreach ($this->app->repoFactory->create('User')->findBy(['isDeleted'=>0]) as $user) {
+		foreach ($this->app->repoFactory->create('User')->findBy(['isDeleted'=>0],"","ORDER BY id desc") as $user) {
 			$list[] = ['id'=>$user->id,
 			           'email'=>$user->email,
 			           'name'=> is_null($user->userDetails) ? "" : $user->userDetails->name,
