c2d5a7ce1c1e895a7c5c735964d33cc3bfef3a0e
Enrico Pascucci <enrico@iwes.it>
2017-01-11 16:19:38 +0100

    devo andare a cercare i trigger da qualche parte

---
+++ domain/repositories/CInvoiceNewRepo.php
@@ -0,0 +1,28 @@
+<?php
+namespace bamboo\domain\repositories;
+
+use bamboo\core\db\pandaorm\repositories\ARepo;
+
+/**
+ * Class CProductRepo
+ * @package bamboo\domain\repositories
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 01/11/2014
+ * @since 1.0
+ */
+class CInvoiceNewRepo extends ARepo {
+
+    protected function createInvoice($invoiceType, $userId) {
+
+    }
+
+    public function recordFriendInvoice($number, $date, $paymentDate, $shopId, array $orderLine) {
+
+    }
+}
