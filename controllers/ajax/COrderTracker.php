719cae9286e05c9f612296a1a40f276db657049a
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-01-17 14:48:48 +0100

    inserimento emails

--- controllers/ajax/COrderTracker.php
+++ controllers/ajax/COrderTracker.php
@@ -24,35 +24,19 @@ class COrderTracker extends AAjaxController
     {
 	    $orderId = $this->app->router->request()->getRequestData('orderId');
 	    $langId = $this->app->router->request()->getRequestData('langId');
-	    $carrierId = $this->app->router->request()->getRequestData('carrierId');
-	    $trackingNumber = $this->app->router->request()->getRequestData('tracking');
+	    $reasons = $this->app->router->request()->getRequestData('reasons');
+
         $order = $this->app->repoFactory->create('Order')->findOneByStringId($orderId);
         $lang = $this->app->repoFactory->create('Lang')->findOneByStringId($langId);
 
-        /** @var CShipment $shipment */
-        $shipment = $this->app->repoFactory->create('Shipment')->getEmptyEntity();
-        $shipment->carrierId = $carrierId;
-        $shipment->scope = $shipment::SCOPE_US_TO_USER;
-        $shipment->trackingNumber = $trackingNumber;
-        $shipment->shipmentDate = $this->time();
-        $shipment->declaredValue = $order->grossTotal;
-        $shipment->id = $shipment->insert();
-
-        foreach ($order->orderLine as $orderLine) {
-            $olhs = $this->app->repoFactory->create('OrderLineHasShipment')->getEmptyEntity();
-            $olhs->orderId = $orderLine->orderId;
-            $olhs->orderLineId = $orderLine->id;
-            $olhs->shipmentId = $shipment->id;
-            $olhs->insert();
-        }
-
-        $order->note = $order->note." Tracking: ".$shipment->trackingNumber. ' Spedito: '.date('Y-m-d');
+        $order->note = $order->note." Cancellato: ".date('Y-m-d');
         $order->update();
-        $this->app->orderManager->changeStatus($order,'ORD_SHIPPED');
+        $this->app->orderManager->changeStatus($order,'ORD_FR_CANCEL');
 
         $to = [$order->user->email];
         $this->app->mailer->prepare('shipmentclient','no-reply', $to,[],[],['order'=>$order,'shipment'=>$shipment,'lang'=>$lang->lang]);
         $res = $this->app->mailer->send();
         if($res) return 'ok';
+
     }
 }
