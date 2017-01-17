20f82b884b80806e1fa65de2df834f857c8022b3
Enrico Pascucci <enrico@iwes.it>
2017-01-17 16:51:02 +0100

    aggiornamento dell'aggiornamento stato riga, nella scheda degli ordini.

---
+++ controllers/ajax/CChangeLineStatus.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace bamboo\blueseal\controllers\ajax;
+use bamboo\core\exceptions\BambooException;
+use bamboo\domain\repositories\COrderLineRepo;
+
+/**
+ * Class CChangeLineStatus
+ * @package bamboo\blueseal\controllers\ajax
+ */
+class CChangeLineStatus extends AAjaxController
+{
+    /**
+     * @return bool
+     */
+    public function put()
+    {
+        try {
+            $ids = explode('-', $this->data['value']);
+            /** @var COrderLineRepo $repo */
+            $repo = $this->app->repoFactory->create('OrderLine');
+            $line = $repo->findOne(['id' => $ids[0], 'orderId' => $ids[1]]);
+            $repo->updateStatus($line, $ids[2]);
+            return true;
+        } catch (BambooException $e) {
+            \Monkey::app()->router->response()->raiseProcessingError();
+            return false;
+        }
+    }
+
+}
