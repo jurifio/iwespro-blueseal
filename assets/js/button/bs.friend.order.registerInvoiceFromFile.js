13fbc6ea611e1d6e131f8d8b601422ea5adc3c89
Enrico Pascucci <enrico@iwes.it>
2017-01-27 12:31:28 +0100

    fix permessi tasto fatture

--- assets/js/button/bs.friend.order.registerInvoiceFromFile.js
+++ assets/js/button/bs.friend.order.registerInvoiceFromFile.js
@@ -1,7 +1,7 @@
 window.buttonSetup = {
     tag:"a",
     icon:"fa-money",
-    permission:"/admin/product/edit&&allShops",
+    permission:"/admin/product/edit",
     event:"bs.friend.order.registerInvoiceFromFile",
     class:"btn btn-default",
     rel:"tooltip",
