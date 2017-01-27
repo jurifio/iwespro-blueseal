57b93e6de692096303336f19753883278202c2eb
Enrico Pascucci <enrico@iwes.it>
2017-01-27 16:15:53 +0100

    fix permessi tasto fatture

--- controllers/ajax/COrderListAjaxController.php
+++ controllers/ajax/COrderListAjaxController.php
@@ -63,7 +63,7 @@ class COrderListAjaxController extends AAjaxController
         $critical = \Monkey::app()->router->request()->getRequestData('critical');
         if ($critical) {
             $sql .= " AND ((`o`.`status` LIKE 'ORD_PENDING' AND `ol`.`status` NOT LIKE 'ORD_FRND_CANC' AND `ol`.`status` NOT LIKE 'ORD_CANCEL' AND (`o`.`orderPaymentMethodId` <> 5 OR `o`.`paymentDate` is NULL) ) " .
-                " OR (`ol`.`status` LIKE 'ORD_FRND_OK' AND `os`.`id ` NOT IN (8,9,10) AND `ols`.`id` < 10))";
+                " OR (`ol`.`status` LIKE 'ORD_FRND_OK' AND `os`.`id` NOT IN (8,9,10) AND `ols`.`id` < 10))";
         }
         $datatable = new CDataTables($sql, ['id'], $_GET,true);
         //$datatable->addCondition('statusCode', ['ORD_CANCEL'], true);
