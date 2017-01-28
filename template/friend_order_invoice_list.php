0b3c6a620bf454702fa2922f3aac18fd378e7eac
Enrico Pascucci <enrico@iwes.it>
2017-01-28 20:19:03 +0100

    csv lista fatture

--- template/friend_order_invoice_list.php
+++ template/friend_order_invoice_list.php
@@ -107,6 +107,12 @@
             data-placement="bottom"
             data-event="bs.orderline.editPaymentBillRemoveInvoices"
         ></bs-toolbar-button>
+        <!--<bs-toolbar-button
+            data-remote="bs.friend.order.invoice.download"
+        ></bs-toolbar-button>-->
+        <bs-toolbar-button
+            data-remote="bs.lists.generate.csv"
+        ></bs-toolbar-button>
     </bs-toolbar-group>
     <?php endif; ?>
 </bs-toolbar>
