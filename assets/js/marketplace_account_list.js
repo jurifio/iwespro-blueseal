7b4398ac727f50f9b43c91fef96cb15ecf85dfb3
Enrico Pascucci <enrico@iwes.it>
2017-01-28 10:19:14 +0100

    provo ad aggiustarlo

--- assets/js/marketplace_account_list.js
+++ assets/js/marketplace_account_list.js
@@ -3,5 +3,20 @@ $(document).on('bs.marketplace.category.href', function(a,b,c){
 
     var table = $('.table');
     var dt = table.DataTable();
-    window.location.href = "/blueseal/marketplace/account/aggregato/"+dt.row('.selected').DT_RowId;
+    var selectedRows = dt.rows('.selected').data();
+
+    if (1 > selectedRows.length) {
+        new Alert({
+            type: "warning",
+            message: "Devi selezionare almeno un prodotto"
+        }).open();
+        return false;
+    }
+
+    var i = 0;
+    var row = '';
+    $.each(selectedRows, function (k, v) {
+        rows(v.DT_RowId);
+    });
+    window.location.href = "/blueseal/marketplace/account/aggregato/" + row;
 });
