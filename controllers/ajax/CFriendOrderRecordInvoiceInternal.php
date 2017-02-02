0b9dff208e9ccf6d889b410d2aeb2d20458d795a
Enrico Pascucci <enrico@iwes.it>
2017-02-02 10:34:29 +0100

    tracciamento errori fatturazione da parte dei friend

--- controllers/ajax/CFriendOrderRecordInvoiceInternal.php
+++ controllers/ajax/CFriendOrderRecordInvoiceInternal.php
@@ -46,6 +46,7 @@ class CFriendOrderRecordInvoiceInternal extends AAjaxController
             $res['responseText'] = $e->getMessage();
             return json_encode($res);
         } catch (BambooException $e) {
+            \Monkey::app()->applicationError('FriendOrderRecordInvoiceInternal', 'errore grave inserimento fattura con fatturazione interna', $e->getMessage());
             return $e->getMessage();
         }
     }
