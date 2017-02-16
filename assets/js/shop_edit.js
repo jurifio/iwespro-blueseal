15440dbe308a52fbb6490763f33f018f8b7c1775
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-02-16 15:43:10 +0100

    pannello friends in lavorazione

--- assets/js/shop_edit.js
+++ assets/js/shop_edit.js
@@ -34,7 +34,6 @@ $(document).on('bs.shop.save',function() {
 			dataType: "json"
 		}).done(function (res) {
 			$('#shop_id').val(res.id);
-			$('#shop_name').val(res.name);
 			$('#shop_title').val(res.title);
 			$('#shop_owner').val(res.owner);
 			$('#shop_referrerEmails').val(res.referrerEmails);
@@ -43,8 +42,58 @@ $(document).on('bs.shop.save',function() {
 			$('#shop_pastSeasonMultiplier').val(res.pastSeasonMultiplier);
 			$('#shop_saleMultiplier').val(res.saleMultiplier);
             $('#shop_referrerEmails').selectize({
-                delimiter: ';'
+                delimiter: ';',
+                create: function(input) {
+                    return {
+                        value: input,
+                        text: input
+                    }
+                }
+            });
+            fillShipment(res.billingAddressBook,'#billingAddress');
+            res.shippingAddressBook.forEach(function(addressData) {
+                $('#shippingAddresses').append('<div class="shippingAddress" id="shippingAddress_'+addressData.id+'"></div>');
+                fillShipment(addressData,'#shippingAddress_'+addressData.id);
             });
 		});
 	}
 })(jQuery);
+
+function fillShipment(data,containerSelector) {
+    let element = $(containerSelector);
+    $.getTemplate('addressBookFormMock').done(function (res) {
+        element.html($(res));
+        Pace.ignore(function() {
+            $.get({
+                url: '/blueseal/xhr/GetTableContent',
+                data: {
+                    table: 'Country'
+                },
+                dataType: 'json'
+            }).done(function (res2) {
+                let select = element.find('#country');
+                if(typeof (select[0].selectize) != 'undefined') select[0].selectize.destroy();
+                select.selectize({
+                    valueField: 'id',
+                    labelField: 'name',
+                    searchField: ['name'],
+                    options: res2,
+                });
+                if(Object.keys(data).length  > 0) {
+                    element.find('#name').val(data.name);
+                    element.find('#subject').val(data.subject);
+                    element.find('#address').val(data.address);
+                    element.find('#extra').val(data.extra);
+                    element.find('#city').val(data.city);
+                    //select.selectize.setValue(data.countryId);
+                    element.find('#postcode').val(data.postcode);
+                    element.find('#phone').val(data.phone);
+                    element.find('#cellphone').val(data.cellphone);
+                    element.find('#province').val(data.province);
+                }
+            });
+        });
+    });
+    element.find()
+}
+
--- template/shop_edit.php
+++ template/shop_edit.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <?php include "parts/head.php" ?>
-    <?php echo $app->getAssets(['ui','forms'], $page); ?>
+    <?php echo $app->getAssets(['ui', 'forms'], $page); ?>
     <title>BlueSeal - <?php echo $page->getTitle(); ?></title>
 </head>
 <body class="fixed-header">
@@ -21,7 +21,8 @@
             </div>
 
             <div class="container-fluid">
-                <form id="form-project" enctype="multipart/form-data" role="form" action="" method="post" autocomplete="off">
+                <form id="form-project" enctype="multipart/form-data" role="form" action="" method="post"
+                      autocomplete="off">
                     <div class="row">
                         <div class="col-md-12">
                             <div class="panel panel-default clearfix">
@@ -32,24 +33,49 @@
                                     <div class="row">
                                         <div class="col-md-12">
                                             <div class="row">
-                                                <div class="col-md-4">
-                                                    <div class="form-group form-group-default required">
-                                                        <label for="shop_name">Nome</label>
-                                                        <input autocomplete="off" type="text" id="shop_name" class="form-control" name="shop_name" value="" required="required">
-                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
+                                                <div class="col-md-3">
                                                     <div class="form-group form-group-default required">
                                                         <label for="shop_title">Titolo</label>
-                                                        <input id="shop_title" autocomplete="off" type="text" class="form-control" name="shop_title" value="" required="required" />
+                                                        <input id="shop_title" autocomplete="off" type="text"
+                                                               class="form-control" name="shop_title" value=""
+                                                               required="required"/>
                                                         <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
                                                     </div>
                                                 </div>
-                                                <div class="col-md-4">
+                                                <div class="col-md-3">
                                                     <div class="form-group form-group-default">
                                                         <label for="shop_owner">Proprietario</label>
-                                                        <input id="shop_owner" autocomplete="off" type="text" class="form-control" name="shop_owner" value="" />
+                                                        <input id="shop_owner" autocomplete="off" type="text"
+                                                               class="form-control" name="shop_owner" value=""/>
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="form-group form-group-default">
+                                                        <label for="shop_currentSeasonMultiplier">Moltiplicatore
+                                                            Stagione Corrente</label>
+                                                        <input id="shop_currentSeasonMultiplier" autocomplete="off"
+                                                               type="number" class="form-control"
+                                                               name="shop_currentSeasonMultiplier" value=""/>
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="form-group form-group-default">
+                                                        <label for="shop_pastSeasonMultiplier">Moltiplicatore Stagioni
+                                                            Passate</label>
+                                                        <input id="shop_pastSeasonMultiplier" autocomplete="off"
+                                                               type="number" class="form-control"
+                                                               name="shop_pastSeasonMultiplier" value=""/>
+                                                        <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-2">
+                                                    <div class="form-group form-group-default">
+                                                        <label for="shop_saleMultiplier">Moltiplicatore Saldi</label>
+                                                        <input id="shop_saleMultiplier" autocomplete="off" type="number"
+                                                               class="form-control" name="shop_saleMultiplier"
+                                                               value=""/>
                                                         <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
                                                     </div>
                                                 </div>
@@ -58,39 +84,116 @@
                                                 <div class="col-md-6">
                                                     <div class="form-group form-group-default required">
                                                         <label for="shop_referrerEmails">Email referenti</label>
-                                                        <input id="shop_referrerEmails" type="text" class="form-control" name="shop_referrerEmails" value="" required="required" />
+                                                        <input id="shop_referrerEmails" type="text" class="form-control"
+                                                               name="shop_referrerEmails" value="" required="required"/>
                                                         <span class="bs red corner label"><i class="fa fa-asterisk"></i></span>
                                                     </div>
                                                 </div>
                                                 <div class="col-md-6">
                                                     <div class="form-group form-group-default">
                                                         <label for="shop_iban">IBAN</label>
-                                                        <input id="shop_iban" autocomplete="off" type="text" class="form-control" name="shop_iban" value="" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-md-4">
-                                                    <div class="form-group form-group-default">
-                                                        <label for="shop_currentSeasonMultiplier">Moltiplicatore Stagione Corrente</label>
-                                                        <input id="shop_currentSeasonMultiplier" autocomplete="off" type="number" class="form-control" name="shop_currentSeasonMultiplier" value="" />
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-group form-group-default">
-                                                        <label for="shop_pastSeasonMultiplier">Moltiplicatore Stagioni Passate</label>
-                                                        <input id="shop_pastSeasonMultiplier" autocomplete="off" type="number" class="form-control" name="shop_pastSeasonMultiplier" value="" />
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-4">
-                                                    <div class="form-group form-group-default">
-                                                        <label for="shop_saleMultiplier">Moltiplicatore Saldi</label>
-                                                        <input id="shop_saleMultiplier" autocomplete="off" type="number" class="form-control" name="shop_saleMultiplier" value="" />
+                                                        <input id="shop_iban" autocomplete="off" type="text"
+                                                               class="form-control" name="shop_iban" value=""/>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="panel panel-default clearfix">
+                                <div class="panel-heading clearfix">
+                                    <h5 class="m-t-10">Informazioni di Fatturazione</h5>
+                                </div>
+                                <div class="panel-body clearfix" id="billingAddress">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_name">Denominazione Inidirzzo</label>
+                                                <input id="shop_billing_name" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_name" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_subject">Destinatario</label>
+                                                <input id="shop_billing_subject" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_subject" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_address">Indirizzo</label>
+                                                <input id="shop_billing_address" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_address" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_extra">Indirizzo 2</label>
+                                                <input id="shop_billing_extra" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_extra" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_city">City</label>
+                                                <input id="shop_billing_city" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_city" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_postcode">Postcode</label>
+                                                <input id="shop_billing_postcode" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_postcode" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_province">Provincia</label>
+                                                <input id="shop_billing_province" autocomplete="off" type="text"
+                                                       class="form-control" maxlength="2"
+                                                       name="shop_billing_province" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_phone">Telefono</label>
+                                                <input id="shop_billing_phone" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_phone" value=""/>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <div class="form-group form-group-default">
+                                                <label for="shop_billing_cellphone">Cellulare</label>
+                                                <input id="shop_billing_cellphone" autocomplete="off" type="text"
+                                                       class="form-control"
+                                                       name="shop_billing_cellphone" value=""/>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="panel panel-default clearfix">
+                                <div class="panel-heading clearfix">
+                                    <h5 class="m-t-10">Indirizzi di Ritiro</h5>
+                                </div>
+                                <div class="panel-body clearfix" id="shippingAddresses">
+
                                 </div>
                             </div>
                         </div>
