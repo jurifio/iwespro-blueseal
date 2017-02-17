bf29f86ccc65c5d8d1e2e206aaff41321684ce33
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-02-17 18:55:47 +0100

    inserite pagine shops

--- assets/js/shop_edit.js
+++ assets/js/shop_edit.js
@@ -1,5 +1,5 @@
-$(document).on('bs.shop.save',function() {
-	let method;
+$(document).on('bs.shop.save', function () {
+    let method;
     let data = {};
     data.id = $('#shop_id').val();
     data.title = $('#shop_title').val();
@@ -22,122 +22,93 @@ $(document).on('bs.shop.save',function() {
 
     data.billingAddressBook = readShipment('#billingAddress');
     data.shippingAddresses = [];
-    $.each($('#shippingAddresses .shippingAddress'),function (k,v) {
+    $.each($('#shippingAddresses .shippingAddress'), function (k, v) {
         data.shippingAddresses.push(readShipment(v));
     });
 
-	if(data.id.length) {
-		method = "PUT";
-	} else {
-		method = "POST";
-	}
+    if (data.id.length) {
+        method = "PUT";
+    } else {
+        method = "POST";
+    }
 
-	$.ajax({
-		method: method,
-		url: "/blueseal/xhr/ShopManage",
+    $.ajax({
+        method: method,
+        url: "/blueseal/xhr/ShopManage",
         data: {
-		    shop: data
+            shop: data
         }
-	}).done(function() {
-		new Alert({
-			type: "success",
-			message: "Modifiche Salvate"
-		}).open();
-	}).fail(function(e) {
+    }).done(function () {
+        new Alert({
+            type: "success",
+            message: "Modifiche Salvate"
+        }).open();
+    }).fail(function (e) {
         console.log(e);
-		new Alert({
-			type: "danger",
-			message: "Impossibile Salvare"
-		}).open();
-	});
+        new Alert({
+            type: "danger",
+            message: "Impossibile Salvare"
+        }).open();
+    });
 });
 
 (function ($) {
-	let params = $.decodeGetStringFromUrl(window.location.href);
-	if(typeof params.id != 'undefined') {
-		$.ajax({
-			url: "/blueseal/xhr/ShopManage",
-			data: {
-				id: params.id
-			},
-			dataType: "json"
-		}).done(function (res) {
-			$('#shop_id').val(res.id);
-			$('#shop_title').val(res.title);
-			$('#shop_owner').val(res.owner);
-			$('#shop_referrerEmails').val(res.referrerEmails);
-			$('#shop_iban').val(res.iban);
-			$('#shop_currentSeasonMultiplier').val(res.currentSeasonMultiplier);
-			$('#shop_pastSeasonMultiplier').val(res.pastSeasonMultiplier);
-			$('#shop_saleMultiplier').val(res.saleMultiplier);
-			$('#shop_config_refusalRate').val(res.config.refusalRate);
-			$('#shop_config_refusalRate_lastMonth').val(res.config.refusalRateLastMonth);
-			$('#shop_config_reactionRate').val(res.config.reactionRate);
-			$('#shop_config_reactionRate_lastMonth').val(res.config.reactionRateLastMonth);
-			$('#shop_accountStatus').val(res.config.accountStatus);
-			$('#shop_accountType').val(res.config.accountType);
-			$('#shop_photoCost').val(res.config.photoCost);
-			$('#shop_shootingTransportCost').val(res.config.shootingTransportCost);
-			$('#shop_orderTransportCost').val(res.config.orderTransportCost);
+    let params = $.decodeGetStringFromUrl(window.location.href);
+    if (typeof params.id != 'undefined') {
+        $.ajax({
+            url: "/blueseal/xhr/ShopManage",
+            data: {
+                id: params.id
+            },
+            dataType: "json"
+        }).done(function (res) {
+            $('#shop_id').val(res.id);
+            $('#shop_title').val(res.title);
+            $('#shop_owner').val(res.owner);
+            $('#shop_referrerEmails').val(res.referrerEmails);
+            $('#shop_iban').val(res.iban);
+            $('#shop_currentSeasonMultiplier').val(res.currentSeasonMultiplier);
+            $('#shop_pastSeasonMultiplier').val(res.pastSeasonMultiplier);
+            $('#shop_saleMultiplier').val(res.saleMultiplier);
+            $('#shop_config_refusalRate').val(res.config.refusalRate);
+            $('#shop_config_refusalRate_lastMonth').val(res.config.refusalRateLastMonth);
+            $('#shop_config_reactionRate').val(res.config.reactionRate);
+            $('#shop_config_reactionRate_lastMonth').val(res.config.reactionRateLastMonth);
+            $('#shop_accountStatus').val(res.config.accountStatus);
+            $('#shop_accountType').val(res.config.accountType);
+            $('#shop_photoCost').val(res.config.photoCost);
+            $('#shop_shootingTransportCost').val(res.config.shootingTransportCost);
+            $('#shop_orderTransportCost').val(res.config.orderTransportCost);
 
-			checkPermission('allShops').done(function() {
-			    "use strict";
-                $('#shop_currentSeasonMultiplier')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-                $('#shop_pastSeasonMultiplier')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-                $('#shop_saleMultiplier')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-                $('#shop_config_refusalRate')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-                $('#shop_config_refusalRate_lastMonth')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-                $('#shop_config_reactionRate')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-                $('#shop_config_reactionRate_lastMonth')
-                    .prop("disabled", false)
-                    .closest('div')
-                    .removeClass('disabled')
-                    .prop("disabled", false);
-            }).fail(function() {
+            checkPermission('allShops')
+                .done(function () {
+                    $.each('input[disabled]', function (k, v) {
+                        $(v).prop("disabled", false)
+                            .closest('div')
+                            .removeClass('disabled')
+                            .prop("disabled", false);
+                    });
+                }).fail(function () {
                 "use strict";
 
             });
 
             $('#shop_referrerEmails').selectize({
                 delimiter: ';',
-                create: function(input) {
+                create: function (input) {
                     return {
                         value: input,
                         text: input
                     }
                 }
             });
-            appendShipment(res.billingAddressBook,'#billingAddress');
-            res.shippingAddressBook.forEach(function(addressData) {
-                appendShipment(addressData,'#shippingAddresses');
+            appendShipment(res.billingAddressBook, '#billingAddress');
+            res.shippingAddressBook.forEach(function (addressData) {
+                appendShipment(addressData, '#shippingAddresses');
             });
-            appendShipment({},'#shippingAddresses');
-		});
-	}
+            appendShipment({}, '#shippingAddresses');
+        });
+    }
 })(jQuery);
 
 function readShipment(containerSelector) {
@@ -158,11 +129,11 @@ function readShipment(containerSelector) {
     return data;
 }
 
-function appendShipment(data,containerSelector) {
+function appendShipment(data, containerSelector) {
     let container = $(containerSelector);
     $.getTemplate('addressBookFormMock').done(function (res) {
         let element = $(res);
-        Pace.ignore(function() {
+        Pace.ignore(function () {
             $.get({
                 url: '/blueseal/xhr/GetTableContent',
                 data: {
@@ -171,14 +142,14 @@ function appendShipment(data,containerSelector) {
                 dataType: 'json'
             }).done(function (res2) {
                 let select = element.find('#country');
-                if(typeof (select[0].selectize) != 'undefined') select[0].selectize.destroy();
+                if (typeof (select[0].selectize) != 'undefined') select[0].selectize.destroy();
                 select.selectize({
                     valueField: 'id',
                     labelField: 'name',
                     searchField: ['name'],
                     options: res2,
                 });
-                if(Object.keys(data).length  > 0) {
+                if (Object.keys(data).length > 0) {
                     element.find('#id').val(data.id);
                     element.find('#name').val(data.name);
                     element.find('#subject').val(data.subject);
