dfb19a5aeb2f325a2a420caa4f066b91d2796ffa
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-02-24 10:59:34 +0100

    job per segnalazione email

---
+++ jobs/CRemindShipmentToFriend.php
@@ -0,0 +1,70 @@
+<?php
+namespace bamboo\blueseal\jobs;
+
+use bamboo\core\base\CObjectCollection;
+use bamboo\core\jobs\ACronJob;
+use bamboo\domain\repositories\COrderLineRepo;
+use bamboo\export\order\COrderExport;
+use bamboo\core\db\pandaorm\repositories\CRepo;
+
+/**
+ * Class CRemindShipmentToFriend
+ * @package bamboo\blueseal\jobs
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date $date
+ * @since 1.0
+ */
+class CRemindShipmentToFriend extends ACronJob
+{
+    /**
+     * @param null $args
+     */
+    public function run($args = null)
+    {
+        $query = "SELECT distinct Shop.id
+                  from 
+                    Shipment s 
+                    join AddressBook ab on s.fromAddressBookId = ab.id
+                    JOIN ShopHasShippingAddressBook shsab on shsab.addressBookId = ab.id
+                    JOIN Shop on Shop.id = shsab.shopId
+                  where s.predictedShipmentDate = CURRENT_DATE
+                  and s.shipmentDate is null and  s.cancellationDate is null";
+        $shops = $this->app->repoFactory->create('Shop')->findBySql($query,[]);
+
+        foreach($shops as $shop){
+            try {
+                $to = explode(';',$shop->referrerEmails);
+                $this->app->mailer->prepare('friendshipmentreminder','no-reply', $to,[],[],['shop'=>$shop]);
+                //$this->app->mailer->send();
+                $this->report('Working Shop ' . $shop->name . ' End', 'Reminder Sent ended');
+            } catch(\Throwable $e){
+                $this->error( 'Working Shop ' . $shop->name . ' End', 'ERROR Sending Lines',$e);
+            }
+        }
+
+        $query = "SELECT distinct Shop.id
+                  from 
+                    Shipment s 
+                    join AddressBook ab on s.fromAddressBookId = ab.id
+                    JOIN ShopHasShippingAddressBook shsab on shsab.addressBookId = ab.id
+                    JOIN Shop on Shop.id = shsab.shopId
+                  where s.predictedShipmentDate = CURRENT_DATE - 1
+                  and s.cancellationDate is null";
+        $shops = $this->app->repoFactory->create('Shop')->findBySql($query,[]);
+
+        $names = [];
+        foreach($shops as $shop){
+            try {
+                $names[] = $shop->title;
+            } catch(\Throwable $e){
+            }
+        }
+        mail('friends@iwes.it','Mancate Spedizioni','Attenzione, si segnala che alcune spedizioni previste per ieri non sono arrivate: '.implode(', ',$names));
+    }
+}
