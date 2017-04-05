eaa7b9e9e3f2f9ca605132a572982fbceeec4367
Enrico Pascucci <enrico@iwes.it>
2017-04-05 12:28:31 +0200

    fusione dei prodotti aggiornata

--- controllers/ajax/CProductMerge.php
+++ controllers/ajax/CProductMerge.php
@@ -1,6 +1,10 @@
 <?php
 namespace bamboo\blueseal\controllers\ajax;
 use bamboo\core\exceptions\BambooException;
+use bamboo\domain\entities\CProduct;
+use bamboo\domain\entities\CStorehouseOperation;
+use bamboo\domain\repositories\CProductRepo;
+use bamboo\domain\repositories\CStorehouseOperationRepo;
 
 /**
  * Class CProductListAjaxController
@@ -58,6 +62,8 @@ class CProductMerge extends AAjaxController
 
     private function mergeProducts($rows, $choosen)
     {
+        /** @var CProductRepo $pR */
+        $pR = \Monkey::app()->repoFactory->create('Product');
         //controllo che i prodotti non scelti per la fusione non abbiano ordini
 
        /* foreach ($rows as $k => $v) {
@@ -72,12 +78,12 @@ class CProductMerge extends AAjaxController
         //controllo di nuovo i gruppi taglie e i
         $sizeGroup = 0;
         foreach ($rows as $k => $v) {
-            $prod = $this->app->repoFactory->create('Product')->findOneBy(['Id' => $v['id'], 'productVariantId' => $v['productVariantId']]);
+            $prod = $pR->findOneBy(['Id' => $v['id'], 'productVariantId' => $v['productVariantId']]);
             if (0 == $sizeGroup) $sizeGroup = $prod->productSizeGroupId;
             elseif ($sizeGroup != $prod->productSizeGroupId) return "ERRORE: Il prodotto da fondere non può contenere ordini!";
         }
         //controllo che i prodotti da fondere non siano dello stesso shop
-        $shopControl = [];
+        /*$shopControl = [];
         foreach ($rows as $k => $v) {
             $sku = $this->app->repoFactory->create('ProductSku')->findBy(['productId' => $v['id'], 'productVariantId' => $v['productVariantId']]);
             $shopSku = [];
@@ -91,7 +97,7 @@ class CProductMerge extends AAjaxController
             } else {
             $shopControl = array_merge($shopControl, $shopSku);
             }
-        }
+        }*/
 
         //controllo che nessuno dei prodotti sia già fuso.
         foreach ($rows as $k => $v) {
@@ -109,12 +115,26 @@ class CProductMerge extends AAjaxController
 
                 //assegno lo shop assegnato al prodotto da fondere al prodotto scelto
                 try {
-                    $sop = $this->app->repoFactory->create('ShopHasProduct')->findBy(['productId' => $v['id'], 'productVariantId' => $v['productVariantId']]);
-                    foreach($sop as $kshop => $vshop) {
+                    $sop = $this->app->repoFactory->create('ShopHasProduct')
+                        ->findBy(['productId' => $v['id'], 'productVariantId' => $v['productVariantId']]);
+                    $choosenShopId = null;
+                    foreach($sop as $vshop) {
+                        if ($v['id'] == $vshop->productId && $v['productVariantId'] == $vshop->productVariantId) {
+                            $choosenShopId = $vshop->shopId;
+                            break;
+                        }
+                    }
+                    if (null == $choosenShopId) throw new BambooException(
+                        'Shop del prodotto scelo per la fusione non trovato'
+                    );
+
+                    foreach($sop as $vshop) {
+                        if ($choosenShopId == $vshop->shopId) continue;
                         $vshop->productId = $rows[$choosen]['id'];
                         $vshop->productVariantId = $rows[$choosen]['productVariantId'];
                         $vshop->insert();
                     }
+
                 } catch (\Throwable $e) {
                     throw new BambooException(
                         $this->buildErrorMsg(
@@ -127,10 +147,11 @@ class CProductMerge extends AAjaxController
                 }
             }
 
-            //aggiorno la relazione tra product e dirtyProduct del prodotto da fondere
+            //aggiorno la relazione tra product e dirtyProduct del prodotto da fondere se c'è l'importatore
+            //se è utilizzato il sistema dei movimenti interno creo i movimenti di scarico e carico
             try {
-
-                foreach($sop as $kshop => $vshop) {
+                $movedProducts = [];
+                foreach ($sop as $vshop) {
                     $dp = $this->app->repoFactory->create('DirtyProduct')->findOneBy([
                         'productId' => $v['id'],
                         'productVariantId' => $v['productVariantId'],
@@ -140,6 +161,32 @@ class CProductMerge extends AAjaxController
                         $dp->productId = $rows[$choosen]['id'];
                         $dp->productVariantId = $rows[$choosen]['productVariantId'];
                         $dp->update();
+                    } else {
+                        $solR = \Monkey::app()->repoFactory->create('StorehouseOperationLine');
+                        $solOC = $solR->findBy(
+                            [
+                                'shopId' => $vshop->shopId,
+                                'productId' => $v['id'],
+                                'productVariantId' => $v['productVariantId']
+                            ]
+                        );
+                        if ($solOC->count()) {
+                            /** @var CStorehouseOperationRepo $soR */
+                            $soR = \Monkey::app()->repoFactory->create('StorehouseOperation');
+                            $productSource = $pR->findOne([$v['id'], $v['productVariantId']]);
+                            $productDestination = $pR->findOne([
+                                $rows[$choosen]['id'], $rows[$choosen]['productVariantId']
+                            ]);
+
+                            $soR->moveStocksOnADifferentProduct(
+                                $productSource, $productDestination, $vshop->shopId
+                            );
+
+                            $moved = [];
+                            $moved['id'] = $productDestination->id;
+                            $moved['productVariantId'] = $productDestination->productVariantId;
+                            $movedProducts[] = $moved;
+                        }
                     }
                 }
             } catch(\Throwable $e) {
@@ -154,14 +201,35 @@ class CProductMerge extends AAjaxController
             }
             // sposto gli sku dei prodotti da fondere, facendoli puntare al prodotto scelto
             try {
-                $psarr = $this->app->repoFactory->create('ProductSku')->findBy([
+                $pskR = $this->app->repoFactory->create('ProductSku');
+                $psarr = $pskR->findBy([
                     'productId' => $v['id'],
                     'productVariantId' => $v['productVariantId']
                     ]);
                 foreach($psarr as $k => $ps) {
-                    $ps->productId = $rows[$choosen]['id'];
-                    $ps->productVariantId = $rows[$choosen]['productVariantId'];
-                    $ps->insert();
+                    $isMoved = false;
+                    foreach($movedProducts as $mk => $mv) {
+                        if ($mv['id'] == $ps->productId && $mv['productVariantId'] == $ps->productVariantId) {
+                            $isMoved = true;
+                            break;
+                        }
+                    }
+                    if (false == $isMoved){
+                        $pskComparison = $pskR->findOneBy([
+                            'productId' => $rows[$choosen]['id'],
+                            'productVariantId' => $rows[$choosen]['productVariantId'],
+                            'productSizeId' => $ps->productSizeId,
+                            'shopId' => $ps->shopId,
+                        ]);
+                        if (null == $pskComparison) {
+                            $ps->productId = $rows[$choosen]['id'];
+                            $ps->productVariantId = $rows[$choosen]['productVariantId'];
+                            $ps->insert();
+                        } else {
+                            $pskComparison->stockQty+= $ps->stockQty;
+                            $pskComparison->update();
+                        }
+                    }
                 }
             } catch(\Throwable $e) {
                 throw new BambooException(
