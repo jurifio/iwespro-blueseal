01a990a00be275f5933fc9ffb9cb396de9f205db
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-04-07 15:35:14 +0200

    fix pannello traduzioni

--- assets/js/content_translation.js
+++ assets/js/content_translation.js
@@ -1,4 +1,4 @@
-$(document).on('blur','.translation-element', function(a,b,c) {
+$(document).on('blur', '.translation-element', function (a, b, c) {
 
     let input = $(this);
     $.ajax({
@@ -9,16 +9,26 @@ $(document).on('blur','.translation-element', function(a,b,c) {
             hash: input.data('hash'),
             langId: input.data('langId')
         }
-    }).done(function (content){
+    }).done(function (content) {
         new Alert({
             type: "success",
             message: "Traduzione Inserita"
         }).open();
 
-    }).fail(function (content){
+    }).fail(function (content) {
         new Alert({
             type: "warning",
             message: "Errore di inserimento"
         }).open();
     });
+});
+
+$(document).on('column-visibility.dt draw.dt', function (e, settings, column, state) {
+    if (typeof state == 'undefined' || state) {
+        $("textarea:empty").each(function() {
+            "use strict";
+            let value = $(this).data('encodedValue');
+            $(this).html(b64DecodeUnicode(value));
+        })
+    }
 });
