e9392c2f13cf4e68593e5ea04c9688c64b1f6012
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-07-04 15:37:54 +0200

    aggiornato il backoffice per fare chiamate solo quando il tab è attivo

--- assets/js/blueseal.common.js
+++ assets/js/blueseal.common.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
     var sessionMonitorContainer = $('#sessionMonitor');
     if(sessionMonitorContainer.length === 1) {
-        setInterval(function () {
-            if(isVisible()) {
-                Pace.ignore(function () {
-                    $.ajax({
-                        url: "/blueseal/xhr/SessionMonitor",
-                        dataType: "JSON"
-                    }).done(function (res) {
-                        sessionMonitorContainer.find('#s1').html(res.traffic.sessions);
-                        sessionMonitorContainer.find('#u1').html(res.traffic.users);
-                        sessionMonitorContainer.find('#l1').html(res.load.m1 + '%');
-                        sessionMonitorContainer.find('#ja').html(res.job);
-                    });
-                });
-            }
-        }, 10000);
+        updateMonitoring();
+        setInterval(updateMonitoring, 10000);
 
     }
 });
+
+const updateMonitoring = function () {
+    if(isVisible()) {
+        Pace.ignore(function () {
+            $.ajax({
+                url: "/blueseal/xhr/SessionMonitor",
+                dataType: "JSON"
+            }).done(function (res) {
+                sessionMonitorContainer.find('#s1').html(res.traffic.sessions);
+                sessionMonitorContainer.find('#u1').html(res.traffic.users);
+                sessionMonitorContainer.find('#l1').html(res.load.m1 + '%');
+                sessionMonitorContainer.find('#ja').html(res.job);
+            });
+        });
+    }
+}
