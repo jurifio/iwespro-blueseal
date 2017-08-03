dbb71635b6834e1965930184c863561bf30710cd
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-08-03 12:35:57 +0200

    finito sistema per la depubblicazione automatica dai marketplace

--- jobs/CDepublishMarketplaceProducts.php
+++ jobs/CDepublishMarketplaceProducts.php
@@ -37,24 +37,29 @@ class CDepublishMarketplaceProducts extends ACronJob
                         cvhp.productVariantId,
                         round(sum(cv.cost),2) AS cost,
                         count(DISTINCT cv.id) AS visits,
-                        count(DISTINCT cvho.orderId) AS orderCount
+                        count(DISTINCT cvho.orderId) AS orderCount,
+                        ifnull(sum(DISTINCT ol.netPrice),0) AS orderValue
                 FROM Campaign c
                   JOIN CampaignVisit cv ON c.id = cv.campaignId
                   JOIN CampaignVisitHasProduct cvhp ON (cv.id, cv.campaignId) = (cvhp.campaignVisitId, cvhp.campaignId)
-                  LEFT JOIN CampaignVisitHasOrder cvho ON (cv.id, cv.campaignId) = (cvho.campaignVisitId, cvho.campaignId)
-                WHERE cv.timestamp > (NOW() - INTERVAL 1 WEEK)
+                  LEFT JOIN (
+                    CampaignVisitHasOrder cvho JOIN OrderLine ol ON cvho.orderId = ol.orderId
+                  ) ON (cv.id, cv.campaignId) = (cvho.campaignVisitId, cvho.campaignId) AND 
+                    (cvhp.productId,cvhp.productVariantId) = (ol.productId,ol.productVariantId)
+                WHERE cv.timestamp > (NOW() - INTERVAL 2 WEEK)
                 GROUP BY c.id,cvhp.productId,cvhp.productVariantId
                 HAVING cost > 0
                 ORDER BY c.id ASC";
 
-        $res = \Monkey::app()->dbAdapter->query($sql, []);
+        $res = \Monkey::app()->dbAdapter->query($sql, [])->fetchAll();
         /** @var CProductRepo $productRepo */
         $productRepo = \Monkey::app()->repoFactory->create('Product');
         /** @var CCampaignRepo $campaingRepo */
         $campaingRepo = \Monkey::app()->repoFactory->create('Campaign');
         /** @var CMarketplaceAccountHasProductRepo $marketplaceAccountHasProduct */
-        $marketplaceAccountHasProduct = \Monkey::app()->repoFactory->create('MarketplaceAccountHasProduct');
+        $marketplaceAccountHasProductRepo = \Monkey::app()->repoFactory->create('MarketplaceAccountHasProduct');
         $reportArray = [];
+        $this->report('run', 'Starting Cycle for ' . count($res));
         foreach ($res as $one) {
             /** @var CProduct $product */
             $product = $productRepo->findOneBy(["id" => $one['productId'], "productVariantId" => $one['productVariantId']]);
@@ -63,36 +68,77 @@ class CDepublishMarketplaceProducts extends ACronJob
             if (!$campaign->marketplaceAccount) {
                 $this->warning('Cycle', 'Campaign not linked with marketplace', $one);
             }
-            $cos = $one['cost'] / (($one['orderCount'] == 0 ? 1 : $one['orderCount']) * $product->getDisplayActivePrice()) * 100;
+            $iniSizes = $product->productSku->count();
+            $actualSizes = 0;
+            foreach ($product->productSku as $sku) if ($sku->stockQty > 0) $actualSizes++;
 
-            if ($cos > $campaign->marketplaceAccount->getConfig()['maxCos'] ?? 15) {
-                $this->report('Cycle', "Deleting product from Marketplace, cos: $cos, over maxCos: " . $config['maxCos'], ['product' => $product, 'campaing' => $campaign]);
+            $this->debug('Cycle Res', 'Doing Math',
+                [
+                    'iniSizes' => $iniSizes,
+                    'actualSizes' => $actualSizes,
+                    'cost' => $one['cost'],
+                    'orderCount' => $one['orderCount'],
+                    'productPrice' => $product->getDisplayActivePrice()
+                ]);
+
+            $sizeFill = $actualSizes / $iniSizes;
+
+            if ($sizeFill === 0) $nCos = 'NaN';
+            else $nCos = (
+                    $one['cost'] /
+                    (
+                        ($one['orderCount'] == 0 ? 0.1 : $one['orderCount']) * $product->getDisplayActivePrice()
+                    )
+                    * $sizeFill
+                ) * 100;
+
+            if ($one['orderCount'] == 0) $cos = 'NaN';
+            else $cos = $one['cost'] / $one['orderValue'];
+            $this->debug('Cycle Res', 'Math Done', [
+                'cos' => $cos,
+                'nCos' => $nCos
+            ]);
+            $maxCos = $campaign->marketplaceAccount->getConfig()['maxCos'] ?? 15;
+            if ($nCos === 'NaN' || $nCos > $maxCos) {
+                $this->report('Cycle', "Deleting product from Marketplace, cos: $nCos, over maxCos: " . $maxCos, ['product' => $product, 'campaing' => $campaign]);
                 /** @var CMarketplaceAccountHasProduct $marketplaceAccountHasProduct */
-                $marketplaceAccountHasProduct->getEmptyEntity();
+                $marketplaceAccountHasProduct = $marketplaceAccountHasProductRepo->getEmptyEntity();
                 $marketplaceAccountHasProduct->productId = $product->id;
                 $marketplaceAccountHasProduct->productVariantId = $product->productVariantId;
                 $marketplaceAccountHasProduct->marketplaceAccountId = $campaign->marketplaceAccount->id;
                 $marketplaceAccountHasProduct->marketplaceId = $campaign->marketplaceAccount->marketplaceId;
-                $marketplaceAccountHasProduct->deleteProductFromMarketplaceAccount($marketplaceAccountHasProduct);
+                $marketplaceAccountHasProductRepo->deleteProductFromMarketplaceAccount($marketplaceAccountHasProduct);
+                $marketplaceAccountHasProduct->nCos = $nCos;
                 $marketplaceAccountHasProduct->cos = $cos;
+
                 if (!isset($reportArray[$marketplaceAccountHasProduct->marketplaceAccount->printId()]))
                     $reportArray[$marketplaceAccountHasProduct->marketplaceAccount->printId()] = [];
                 $reportArray[$marketplaceAccountHasProduct->marketplaceAccount->printId()][] = $marketplaceAccountHasProduct;
             }
-
-            $text = "";
-            $actualMarketplaceAccount = null;
-            foreach ($reportArray as $marketplaceAccountId => $marketplaceAccountHasProduct) {
-                if ($marketplaceAccountId != $actualMarketplaceAccount) {
-                    $text .= PHP_EOL . $marketplaceAccountHasProduct->marketplaceAccount->getCampaignName() . ': ' . PHP_EOL;
-                    $actualMarketplaceAccount = $marketplaceAccountId;
-                }
-                $text .= $marketplaceAccountHasProduct->product->printId() . ' - ' . $marketplaceAccountHasProduct->product->printCpf() . ' - cos: ' . $marketplaceAccountHasProduct->cos . PHP_EOL;
-            }
-            iwesMail('support@iwes.it',
-                'Report Depubblicazione produtti Marketplace',
-                "Sono stati depubblicati i seguenti prodotti: " . PHP_EOL . $text);
         }
+
+        $text = "";
+        $actualMarketplaceAccount = null;
+        foreach ($reportArray as $marketplaceAccountId => $marketplaceAccountHasProducts) {
+            $this->debug('Cycle Report', 'Signaling Delete',
+                ['key' => $marketplaceAccountId, 'val' => $marketplaceAccountHasProduct]);
+
+            if ($marketplaceAccountId != $actualMarketplaceAccount) {
+                $actualMarketplaceAccount = $marketplaceAccountId;
+                $marketplaceAccount = \Monkey::app()->repoFactory->create('MarketplaceAccount')->findOneByStringId($actualMarketplaceAccount);
+                $text .= (PHP_EOL . $marketplaceAccount->getCampaignName() . ': ' . PHP_EOL);
+            }
+            foreach ($marketplaceAccountHasProducts as $marketplaceAccountHasProduct) {
+                $text .= str_pad($marketplaceAccountHasProduct->product->printId(),15,' ',STR_PAD_RIGHT)
+                    . ' - ' . str_pad($marketplaceAccountHasProduct->product->printCpf(),35,' ',STR_PAD_RIGHT)
+                    . ' - nCos: ' . str_pad( $marketplaceAccountHasProduct->nCos,10,' ',STR_PAD_RIGHT)
+                    . ' - cos: ' . str_pad( $marketplaceAccountHasProduct->cos ,10,' ',STR_PAD_RIGHT). PHP_EOL;
+            }
+        }
+
+        iwesMail('support@iwes.it',
+            'Report Depubblicazione produtti Marketplace',
+            "Sono stati depubblicati i seguenti prodotti: " . PHP_EOL . $text);
 
     }
 }
