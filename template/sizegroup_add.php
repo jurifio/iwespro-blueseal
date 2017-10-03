b32658bcbfa6bf905e36a6a5a38ae42b683a090e
Fabrizio Marconi <fabrizio@bambooshoot.agency>
2017-10-03 10:20:56 +0200

    lavorando in sizegroup

--- template/sizegroup_add.php
+++ template/sizegroup_add.php
@@ -1,95 +1,109 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <?php include "parts/head.php"?>
-    <?php echo $app->getAssets(['ui','forms'], $page); ?>
+    <?php include "parts/head.php" ?>
+    <?php echo $app->getAssets(['ui', 'forms'], $page); ?>
     <title>BlueSeal - <?php echo $page->getTitle(); ?></title>
 </head>
 <body class="fixed-header">
 <?php include "parts/sidebar.php"; ?>
-    <div class="page-container">
-        <?php include "parts/header.php"?>
-        <?php include "parts/operations.php" ?>
-        <div class="page-content-wrapper">
-            <div class="content sm-gutter">
-                <div class="container-fluid container-fixed-lg bg-white">
-                    <div class="row">
-                        <div class="col-md-4 col-md-offset-4 alert-container closed">
+<div class="page-container">
+    <?php include "parts/header.php" ?>
+    <?php include "parts/operations.php" ?>
+    <div class="page-content-wrapper">
+        <div class="content sm-gutter">
+            <div class="container-fluid container-fixed-lg bg-white">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 alert-container closed">
 
-                        </div>
                     </div>
                 </div>
+            </div>
 
             <div class="container-fluid container-fixed-lg bg-white">
                 <div class="panel panel-transparent">
                     <div class="panel-body">
-                        <form id="form-project" role="form" action="" method="POST" autocomplete="off">
-                            <div class="row clearfix">
-                                <div class="col-md-12">
-                                    <div class="form-group form-group-default required">
-                                        <label>Nome MacroGruppo Taglie</label>
-                                        <input type="text" class="form-control" name="ProductSizeGroup_macroName" value="<?php echo isset($sizeEdit) ? $sizeEdit->getFirst()->macroName : "" ?>" required title="">
-                                    </div>
+                        <div class="row clearfix">
+                            <div class="col-md-12">
+                                <div class="form-group form-group-default required">
+                                    <label>Nome MacroGruppo Taglie</label>
+                                    <input type="text" class="form-control" name="ProductSizeGroup_macroName"
+                                           value="<?php echo isset($sizeEdit) ? $sizeEdit->getFirst()->macroName : "" ?>"
+                                           required title="">
                                 </div>
                             </div>
-                            <p>Taglie incluse</p>
-                            <div class="row clearfix">
-                            <?php for($k=0;$k<18;$k++): ?>
-                                <?php $actual = isset($sizeEdit) && $sizeEdit->valid() ? $sizeEdit->current() : false ; ?>
-                                <?php if($actual): ?>
-                                <input type="hidden" name="ProductSizeGroup_<?php echo $k?>_id" value="<?php echo $actual->id?>">
-                                <?php endif;?>
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <table class="table table-striped table-condensed">
-                                            <thead>
-                                                <tr class="center">
-                                                    <th><?php echo $k+1?></th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td scope="col"><label>Nome</label><input class="form-control" type="text" id="<?php echo "ProductSizeGroup_".($k)."_name" ?>" name="<?php echo "ProductSizeGroup_".($k)."_name" ?>" value="<?php echo $actual ? $actual->name : ""?>"></td>
-                                                </tr>
-                                                <tr>
-                                                    <td scope="col"><label>Locale</label><input class="form-control" type="text" id="<?php echo "ProductSizeGroup_".($k)."_locale" ?>" name="<?php echo "ProductSizeGroup_".($k)."_locale" ?>" value="<?php echo $actual ? $actual->locale : ""?>"></td>
-                                                </tr>
-                                                <?php for($i=0;$i<36;$i++):?>
-                                                    <?php if(isset($actual) && isset($actual->productSizeGroupHasProductSize)) {
-                                                        foreach ($actual->productSizeGroupHasProductSize as $key => $val) {
-                                                            if ($val->position == $i) {
-                                                                foreach ($actual->productSize as $val2) {
-                                                                    if ($val->productSizeId == $val2->id) {
-                                                                        $nomeTaglia = $val2->name;
-                                                                    }
-                                                                }
-                                                            }
-                                                        }
-                                                    }?>
-                                                    <tr>
-                                                        <td><label>Posizione <?php echo $i+1; ?></label><input class="form-control" type="text"  id="<?php echo "ProductSizeGroup_position_".$k."_".$i ?>" name="<?php echo "ProductSizeGroup_".($k)."_position_".$i ?>" value="<?php echo isset($nomeTaglia) ? $nomeTaglia : "" ?>" ></td>
-                                                    </tr>
-                                                <?php unset($nomeTaglia); endfor; ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                                <?php
-                                if(isset($sizeEdit) && $sizeEdit->valid()) $sizeEdit->next();
-                                endfor; ?>
+                        </div>
+                        <p>Taglie incluse</p>
+                        <div class="table-responsive">
+                            <div class="dataTables_wrapper no-footer">
+                                <table class="table table-hover table-condensed dataTable no-footer" id="condensedTable"
+                                       role="grid">
+                                    <thead>
+                                    <tr role="row">
+                                        <th style="width:30%" class="sorting_asc" tabindex="0"
+                                            aria-controls="condensedTable" rowspan="1" colspan="1" aria-sort="ascending"
+                                            aria-label="Title: activate to sort column descending">Title
+                                        </th>
+                                        <th style="width: 189px;" class="sorting" tabindex="0"
+                                            aria-controls="condensedTable" rowspan="1" colspan="1"
+                                            aria-label="Key: activate to sort column ascending">Key
+                                        </th>
+                                        <th style="width: 266px;" class="sorting" tabindex="0"
+                                            aria-controls="condensedTable" rowspan="1" colspan="1"
+                                            aria-label="Condensed: activate to sort column ascending">Condensed
+                                        </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+
+
+                                    <tr role="row" class="odd">
+                                        <td class="v-align-middle semi-bold sorting_1">Fifth tour</td>
+                                        <td class="v-align-middle">Simple but not simpler</td>
+                                        <td class="v-align-middle semi-bold">Wonders can be true. Believe in your
+                                            dreams!
+                                        </td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                        <td class="v-align-middle semi-bold sorting_1">First tour</td>
+                                        <td class="v-align-middle">Simple but not simpler</td>
+                                        <td class="v-align-middle semi-bold">Wonders can be true. Believe in your
+                                            dreams!
+                                        </td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <td class="v-align-middle semi-bold sorting_1">Fourth tour</td>
+                                        <td class="v-align-middle">Simple but not simpler</td>
+                                        <td class="v-align-middle semi-bold">Wonders can be true. Believe in your
+                                            dreams!
+                                        </td>
+                                    </tr>
+                                    <tr role="row" class="even">
+                                        <td class="v-align-middle semi-bold sorting_1">Second tour</td>
+                                        <td class="v-align-middle">Simple but not simpler</td>
+                                        <td class="v-align-middle semi-bold">Wonders can be true. Believe in your
+                                            dreams!
+                                        </td>
+                                    </tr>
+                                    <tr role="row" class="odd">
+                                        <td class="v-align-middle semi-bold sorting_1">Third tour</td>
+                                        <td class="v-align-middle">Simple but not simpler</td>
+                                        <td class="v-align-middle semi-bold">Wonders can be true. Believe in your
+                                            dreams!
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                            <?php if(isset($sizeEdit)):?>
-                            <button class="btn btn-success" type="submit">Modifica</button>
-                            <?php else: ?>
-                            <button class="btn btn-success" type="submit">Crea</button>
-                            <?php endif; ?>
-                            <button class="btn btn-default"><i class="pg-close"></i> Cancella</button>
-                        </form>
+                        </div>
+                        <table>
+
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-        <?php include "parts/footer.php"?>
+        <?php include "parts/footer.php" ?>
     </div>
 </div>
 <?php include "parts/bsmodal.php"; ?>
