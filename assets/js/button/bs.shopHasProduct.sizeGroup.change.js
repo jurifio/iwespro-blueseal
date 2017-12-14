94a7c3ec0aad980c69bf19c3a139e45c9046c1f4
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2017-12-14 19:25:11 +0100

    Mostro nome taglie su assegnazioni gruppi taglie private e pubbliche

--- assets/js/button/bs.shopHasProduct.sizeGroup.change.js
+++ assets/js/button/bs.shopHasProduct.sizeGroup.change.js
@@ -58,13 +58,13 @@ $(document).on('bs-manage-shop-sizeGroups', function () {
                     item: function (item, escape) {
                         return '<div>' +
                             '<span class="label">' + escape(item.locale+ ' '+ item.macroName) + '</span>' +
-                            ' - <span class="caption">' + escape(item.name) + '</span>' +
+                            ' - <span class="caption">' + escape(item.sizeNames) + '</span>' +
                             '</div>'
                     },
                     option: function (item, escape) {
                         return '<div>' +
                             '<span class="label">' + escape(item.locale+ ' '+ item.macroName) + '</span>' +
-                            ' - <span class="caption">' + escape(item.name) + '</span>' +
+                            ' - <span class="caption">' + escape(item.sizeNames) + '</span>' +
                             '</div>'
                     }
                 }
