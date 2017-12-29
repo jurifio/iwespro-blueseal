bb1ad142db67e26c68215d9d32169a1d88c303a8
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2017-12-29 19:36:09 +0100

    Aggiunto panello per forzare i prodotti in saldo su tabella ProductSku

---
+++ assets/js/sale_price_p_sku_list.js
@@ -0,0 +1,59 @@
+;(function () {
+
+    $(document).on('bs-sale-price-modify', function () {
+
+        let dataTable = $('.dataTable').DataTable();
+        let selectedRows = dataTable.rows('.selected').data();
+        let ids = selectedRows[0].id.split('-');
+        var productId = ids[0];
+        var productVariantId = ids[1];
+
+        if (selectedRows.length === 1) {
+
+            let bsModal = new $.bsModal('Aggiorna i prezzi in saldo', {
+                body: '<p>Inserisci il nuovo prezzo scontato</p>' +
+                '<div class="form-group form-group-default required">' +
+                '<label for="changeSalePrice">Metti il nuovo prezzo in saldo (Product SKU)</label>' +
+                '<input autocomplete="off" type="text" id="changeSalePrice" ' +
+                'placeholder="Nuovo prezzo in saldo" class="form-control" name="changeSalePrice" required="required">' +
+                '</div>'
+            });
+
+            bsModal.showCancelBtn();
+            bsModal.setOkEvent(function () {
+                const data = {
+                    productId:  productId,
+                    productVariantId:  productVariantId,
+                    newSalePrice: $('input#changeSalePrice').val()
+                };
+                $.ajax({
+                    method: 'put',
+                    url: '/blueseal/xhr/SalePriceProductSkuModify',
+                    data: data
+                }).done(function (res) {
+                    bsModal.writeBody(res);
+                }).fail(function (res) {
+                    bsModal.writeBody('Errore grave');
+                }).always(function (res) {
+                    bsModal.setOkEvent(function () {
+                        //refresha solo tabella e non intera pagina
+                        $.refreshDataTable();
+                        bsModal.hide();
+                    });
+                    bsModal.showOkBtn();
+                });
+            });
+        } else if(selectedRows.length < 1){
+            new Alert({
+                type: "warning",
+                message: "Non hai selelezionato nessuna riga"
+            }).open();
+        } else if (selectedRows.length > 1){
+            new Alert({
+                type: "warning",
+                message: "Seleziona una sola riga"
+            }).open();
+        }
+    });
+
+})();
