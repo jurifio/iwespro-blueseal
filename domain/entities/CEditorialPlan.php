a93534bc7fba71327821a68ea73ebce532fd4a83
juri fiorani <jurif@hotmail.com>
2018-04-06 17:23:50 +0200

    modifica event button newsletterevent-edit in bs.newslettergroup-edit per il file bs.lists.modify.newslettergroup.js

---
+++ domain/entities/CEditorialPlan.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace bamboo\domain\entities;
+
+use bamboo\core\db\pandaorm\entities\AEntity;
+
+/**
+ * Class CEditorialPlan
+ * @package bamboo\domain\entities
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 01/08/2016
+ * @since 1.0
+ */
+class CEditorialPlan extends AEntity
+{
+	protected $entityTable = 'EditorialPlan';
+	protected $primaryKeys = ['id'];
+}
