312e001736d4de22f21bc8395ecaba8edb14e372
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2018-04-20 10:31:31 +0200

    aggiunto il pannello "veloce" per accettazione prodotti SHOOTING

---
+++ controllers/back/ajax/CShootingAcceptProductListAjaxController.php
@@ -0,0 +1,117 @@
+<?php
+namespace bamboo\controllers\back\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\base\CObjectCollection;
+use bamboo\domain\entities\CProduct;
+use bamboo\domain\entities\CShooting;
+use bamboo\domain\entities\CShootingBooking;
+use bamboo\domain\entities\CShootingBookingHasProductType;
+use bamboo\domain\entities\CUser;
+use bamboo\domain\repositories\CDocumentRepo;
+use bamboo\domain\repositories\CShootingRepo;
+
+
+/**
+ * Class CSizeMacroGroupListAjaxController
+ * @package bamboo\controllers\back\ajax
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 23/03/2018
+ * @since 1.0
+ */
+class CShootingAcceptProductListAjaxController extends AAjaxController
+{
+    /**
+     * @return string
+     * @throws \Throwable
+     */
+    public function get()
+    {
+        $shopsIds = \Monkey::app()->repoFactory->create('Shop')->getAutorizedShopsIdForUser();
+
+        $user = \Monkey::app()->getUser();
+        $allShops = $user->hasPermission('allShops');
+
+        $sql = "SELECT
+                  `p`.`id`                                             AS `id`,
+                  `p`.`productVariantId`                               AS `productVariantId`,
+                  concat(`p`.`id`, '-', `p`.`productVariantId`)        AS `code`,
+                  `pb`.`name`                                          AS `brand`,
+                  concat(`p`.`itemno`, ' # ', `pv`.`name`)             AS `cpf`,
+                  shp.extId                                            AS `externalId`,
+                  `s`.`id`                                             AS `shopId`,
+                  `s`.`title`                                          AS `shop`,
+                  concat(phs.shootingId)                               AS shooting,
+                  concat(doc.number)                                   AS doc_number,
+                  `p`.`creationDate`                                   AS `creationDate`
+                FROM `Product` `p`
+                  JOIN `ProductVariant` `pv` ON `p`.`productVariantId` = `pv`.`id`
+                  JOIN `ProductBrand` `pb` ON `p`.`productBrandId` = `pb`.`id`
+                  JOIN `ShopHasProduct` `shp` ON (`p`.`id`, `p`.`productVariantId`) = (`shp`.`productId`, `shp`.`productVariantId`)
+                  JOIN `Shop` `s` ON `s`.`id` = `shp`.`shopId`
+                  LEFT JOIN (
+                    ProductHasShooting phs 
+                      JOIN Shooting shoot ON phs.shootingId = shoot.id
+                        LEFT JOIN Document doc ON shoot.friendDdt = doc.id) 
+                                ON p.productVariantId = phs.productVariantId AND p.id = phs.productId
+                GROUP BY p.id, p.productVariantId, s.id
+                ORDER BY `p`.`creationDate` DESC
+               ";
+
+        $datatable = new CDataTables($sql, ['id', 'productVariantId'], $_GET, true);
+        $datatable->addCondition('shopId', $shopsIds);
+        if (!$allShops) $datatable->addLikeCondition('status', 'Fuso', true);
+
+        $datatable->doAllTheThings();
+
+        /** @var CDocumentRepo $docRepo */
+        $docRepo = \Monkey::app()->repoFactory->create('Document');
+
+        $productRepo = \Monkey::app()->repoFactory->create('Product');
+        foreach ($datatable->getResponseSetData() as $key => $row) {
+
+            $val = $productRepo->findOneBy($row);
+            /** @var CProduct $val */
+            $row["DT_RowId"] = $val->printId();
+            $row["DT_RowClass"] = 'colore';
+            $row['code'] = $val->id . '-' . $val->productVariantId;
+
+            $row["row_shop"] = $val->getShops('|', true);
+            $row['shop'] = '<span>';
+            $row['shop'] .= $val->getShops('<br />');
+            $row['shop'] .= '</span>';
+
+
+            $row['externalId'] = '<span class="small">';
+            $row['externalId'] .= $val->getShopExtenalIds('<br />');
+            $row['externalId'] .= '</span>';
+
+            $row['cpf'] = '<span class="small">';
+            $row['cpf'] .= $val->itemno . ' # ' . $val->productVariant->name;
+            $row['cpf'] .= '</span>';
+
+            $row['brand'] = $val->productBrand->name;
+
+            $row['creationDate'] = $val->creationDate;
+            $sids = "";
+            $ddtNumbers = "";
+
+            /** @var CShooting $singleShooting */
+            foreach ($val->shooting as $singleShooting){
+                $sids .= '<br />'.$singleShooting->id;
+                $ddtNumbers .= '<br />'.$docRepo->findShootingFriendDdt($singleShooting);
+            }
+
+            $row["shooting"] = $sids;
+            $row["doc_number"] = $ddtNumbers;
+            $datatable->setResponseDataSetRow($key, $row);
+        }
+        return $datatable->responseOut();
+    }
+}
