243f1c49ecc6f793e50d6746d98449f69795ef3b
juri fiorani <jurif@hotmail.com>
2018-04-23 15:26:13 +0200

    aggiornamento file di invio test su newsletter creazione funzione clona record

---
+++ assets/js/button/bs.lists.clone.newsletter.js
@@ -0,0 +1,57 @@
+window.buttonSetup = {
+    tag: "a",
+    icon: "fa-clone",
+    permission: "/admin/product/delete&&allShops",
+    event: "bs-newsletter-clone",
+    class: "btn btn-default",
+    rel: "tooltip",
+    title: "Duplica la newsletter",
+    placement: "bottom",
+    toggle: "modal"
+};
+
+$(document).on('bs-newsletter-clone', function (e, element, button) {
+
+    let dataTable = $('.dataTable').DataTable();
+    let selectedRows = dataTable.rows('.selected').data();
+
+
+    if (selectedRows.length != 1) {
+        new Alert({
+            type: "warning",
+            message: "Devi selezionare una Newsletter per duplicarla"
+        }).open();
+        return false;
+    }
+
+    let newsletterId = selectedRows[0].id;
+    let bsModal = new $.bsModal('Duplicazione', {
+        body: '<p>Duplica la newsletter selezionata</p>'
+    });
+
+
+    bsModal.setOkEvent(function () {
+
+        let id = selectedRows[0].id;
+
+
+        $.ajax({
+            method: "put",
+            url: "/blueseal/xhr/NewsletterClone",
+            data: {
+                id: id
+            }
+        }).done(function (res) {
+            bsModal.writeBody(res);
+        }).fail(function (res) {
+            bsModal.writeBody(res);
+        }).always(function (res) {
+            bsModal.setOkEvent(function () {
+                window.location.reload();
+                bsModal.hide();
+                // window.location.reload();
+            });
+            bsModal.showOkBtn();
+        });
+    });
+});
---
+++ controllers/back/ajax/CNewsletterClone.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace bamboo\controllers\back\ajax;
+
+use bamboo\core\db\pandaorm\repositories\CRepo;
+use bamboo\core\exceptions\BambooException;
+use bamboo\domain\entities\CNewsletter;
+use bamboo\domain\repositories\CNewsletterUserRepo;
+
+
+/**
+ * Class CNewsletterClone
+ * @package bamboo\controllers\back\ajax
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date $date
+ * @since 1.0
+ */
+class CNewsletterClone extends AAjaxController
+{
+
+
+
+    /**
+     * @return mixed
+     * @throws BambooException
+     * @throws \bamboo\core\exceptions\BambooORMInvalidEntityException
+     * @throws \bamboo\core\exceptions\BambooORMReadOnlyException
+     */
+
+    public function put(){
+        $data  = $this->app->router->request()->getRequestData();
+        $id = $data["id"];
+        $value = $id;
+        $value = strstr($value, ">");
+        $value = strstr($value, "</a>", true);
+        $value =trim(str_replace(">","",$value));
+        /** @var CRepo $newsletterUserRepo */
+        $newsletterUserRepo = \Monkey::app()->repoFactory->create('Newsletter');
+
+        /** @var CNewsletter $newsletter */
+        $newsletter = $newsletterUserRepo->findOneBy(['id'=>$value]);
+        $newsletterCloneName=$newsletter->name;
+        $newsletterCloneFromEmailAddressId=$newsletter->fromEmailAddressId;
+        $newsletterCloneSendAddressdate='2100-00-00 00:00:00';
+        $newsletterCloneNewsletterEmailListId=$newsletter->newsletterEmailListId;
+        $newsletterCloneNewsletterTemplateId=$newsletter->newsletterTemplateId;
+        $newsletterCloneSubject=$newsletter->subject;
+        $newsletterCloneDataDescription=$newsletter->dataDescription;
+        $newsletterClonePreCompiledTemplate=$newsletter->preCompiledTemplate;
+        $newsletterCloneNewsletterCampaignId=$newsletter->newsletterCampaignId;
+        $newsletterCloneCampaignId=$newsletter->campaignId;
+        $newsletterCloneEventId=$newsletter->newsletterEventId;
+        $newsletterCloneName="Copia di ".$newsletterCloneName;
+        /** @var CNewsletterUser $newsletterUserInsert */
+        $newsletterUserInsert = \Monkey::app()->repoFactory->create('Newsletter')->getEmptyEntity();
+        //popolo la tabella
+
+        $newsletterUserInsert->name = $newsletterCloneName;
+        $newsletterUserInsert->fromEmailAddressId = $newsletterCloneFromEmailAddressId;
+        $newsletterUserInsert->sendAddressDate = $newsletterCloneSendAddressdate;
+        $newsletterUserInsert->newsletterEmailListId = $newsletterCloneNewsletterEmailListId;
+        $newsletterUserInsert->newsletterTemplateId = $newsletterCloneNewsletterTemplateId;
+        $newsletterUserInsert->subject = $newsletterCloneSubject;
+        $newsletterUserInsert->dataDescription = $newsletterCloneDataDescription;
+        $newsletterUserInsert->preCompiledTemplate = $newsletterClonePreCompiledTemplate;
+        $newsletterUserInsert->newsletterCampaignId = $newsletterCloneNewsletterCampaignId;
+        $newsletterUserInsert->CampaignId = $newsletterCloneCampaignId;
+        $newsletterUserInsert->newsletterEventId = $newsletterCloneEventId;
+        // eseguo la commit sulla tabella;
+
+        $newsletterUserInsert->smartInsert();
+
+
+
+
+        $res = " Newsletter Duplicata";
+        return $res;
+
+    }
+
+
+
+}
