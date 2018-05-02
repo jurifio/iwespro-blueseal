6a37a17b103ab40f9508b9975dc29998f1529605
juri fiorani <jurif@hotmail.com>
2018-05-02 16:19:50 +0200

    correzione ckeditor per newslettertemplate

--- assets/js/newslettertemplate_add.js
+++ assets/js/newslettertemplate_add.js
@@ -1,9 +1,72 @@
-(function ($) {
-    Pace.ignore(function () {
+var summer = $('textarea.summer');
+summer.summernote({
+    lang: "it-IT",
+    height: 300,
+    fontNames: [
+        'Arial',
+        'Arial Black',
+        'Comic Sans MS',
+        'Courier',
+        'Courier New',
+        'Helvetica',
+        'Impact',
+        'Lucida Grande',
+        'Raleway',
+        'Serif',
+        'Sans',
+        'Sacramento',
+        'Tahoma',
+        'Times New Roman',
+        'Verdana'
+    ],
+    onImageUpload: function() {},
+    fontNamesIgnoreCheck: ['Raleway']
+});
 
+summer.on('summernote.image.upload', function(we, files) {
+    we.preventDefault();
+    we.stopPropagation();
+    var data = new FormData();
+    data.append("file", files[0]);
+    $.ajaxForm({
+        url: '/blueseal/xhr/BlogPostPhotoUploadAjaxController',
+        type: 'POST',
+        formAutofill: false
+    },data).done(function (result) {
+        var i = new Image;
+        i.src = result;
+        summer.summernote('insertNode', i);
+    }).fail(function (result) {
+        console.log('fail');
+    });
+});
+
+$('[data-toggle="popover"]').popover();
+
+$('#cover').on('click',function() {
+    $('[data-json="PostTranslation.coverImage"]').click();
+});
+
+$('[data-json="PostTranslation.coverImage"]').on('change', function(){
+    if (this.files && this.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#cover').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(this.files[0]);
+    }
+});
+/*(function ($) {
+    Pace.ignore(function () {
+        CKEDITOR.replace( 'template', {
+            height: 260,
+            width:1024
+
+        } );
 
     });
-})(jQuery);
+})*/
+(jQuery);
 
 $(document).on('bs.newNewsletterTemplate.save', function () {
     let bsModal = new $.bsModal('Salva Template', {
@@ -13,11 +76,11 @@ $(document).on('bs.newNewsletterTemplate.save', function () {
 
     bsModal.showCancelBtn();
     bsModal.setOkEvent(function () {
-        var templateEditor =$('#template').val();
+
 
         const data = {
             name : $('#name').val(),
-            template: templateEditor
+            template: $('#template').val()
         };
         $.ajax({
             method: 'post',
