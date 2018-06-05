6203c84354ffe21ee6e4945e6ee8c99fa0c5624c
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2018-06-05 11:33:34 +0200

    Impostato limite minimo e massimo su Brand Description

--- assets/js/product_brand_edit.js
+++ assets/js/product_brand_edit.js
@@ -1,4 +1,20 @@
 $(document).on('bs.brand.edit', function() {
+
+    let descriptionLength = $('#ProductBrand_description')
+                                .val()
+                                    .length;
+
+    let minLength = 1300;
+    let maxLength = 2000;
+
+    if(descriptionLength < 1300 || descriptionLength > 2000){
+        new Alert({
+            type: "warning",
+            message: `La descrizione del brand deve essere compresa fra un numero di caratteri compreso tra ${minLength} e ${maxLength}`
+        }).open();
+        return false;
+    }
+
     $.ajax({
         type: "PUT",
         url:"#",
