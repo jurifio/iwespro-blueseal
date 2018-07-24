08e85cabe47aab1e249f876c2f2bba150dacaf23
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2018-07-24 09:31:35 +0200

    mail id view

--- remote/readextdbtable/AReadExtDbTable.php
+++ remote/readextdbtable/AReadExtDbTable.php
@@ -6,6 +6,7 @@ namespace bamboo\blueseal\remote\readextdbtable;
 use bamboo\core\base\CConfig;
 use bamboo\core\db\pandaorm\adapter\CMySQLStandAloneAdapter;
 use bamboo\core\exceptions\BambooConfigException;
+use bamboo\domain\entities\CNewsletterShop;
 
 
 /**
@@ -27,43 +28,65 @@ class AReadExtDbTable
 
     /**	@var CConfig */
     protected $config;
-
     protected $client;
-
+    protected $destination;
+    protected $dbName;
+    protected $ownersFields = [];
 
     /**
      * AReadExtDbTable constructor.
-     * @param $shop
+     * @param int $shop
      * @throws BambooConfigException
      * @throws \bamboo\core\exceptions\BambooDBALException
      */
-    public function __construct($shop)
+    public function __construct(int $shop)
     {
         $this->readConfig($shop);
-        $config = $this->config->fetchAll('destination');
+        $config = $this->config->fetchAll('origin');
+        $this->destination = \Monkey::app()->dbAdapter;
 
+        $this->dbName = $config["name"];
         $this->client = new CMySQLStandAloneAdapter(
             \Monkey::app(),
             $config['engine'],
-            $config['host'],$config['name'],
+            $config['host'],
+            $config['name'],
             $config['charset'],
             $config['user'],
             $config['pass']);
         $this->client->connect();
+
+
     }
 
     /**
+     * @param $shop
      * @throws BambooConfigException
      */
     public function readConfig($shop)
     {
+
+        /** @var CNewsletterShop $newsletterShop */
+        $newsletterShop = \Monkey::app()->repoFactory->create('NewsletterShop')->findOneBy(["id" => $shop]);
+        $shopName = $newsletterShop->name;
         $filePath = __DIR__ . '/config';
-        $filePath .= '/' . strtolower($shop) . '.json';
+        $filePath .= '/' . strtolower($shopName) . '.json';
 
         if (!file_exists($filePath)) throw new BambooConfigException('Configuration not found in: ' . $filePath);
 
         $this->config = new CConfig($filePath);
         $this->config->load();
+    }
+
+    /**
+     * @param $tableName
+     * @throws \bamboo\core\exceptions\BambooDBALException
+     */
+    protected function getTableFields($tableName){
+        $res = \Monkey::app()->dbAdapter->query("SHOW COLUMNS FROM ".$tableName, [])->fetchAll();
+        foreach($res as $v) {
+            $this->ownersFields[$tableName][] = $v["Field"];
+        }
     }
 
 }
--- remote/readextdbtable/CReadExtDbTable.php
+++ remote/readextdbtable/CReadExtDbTable.php
@@ -3,6 +3,9 @@
 
 namespace bamboo\blueseal\remote\readextdbtable;
 
+use bamboo\core\db\pandaorm\repositories\CRepo;
+use bamboo\core\exceptions\BambooException;
+
 
 /**
  * Class CReadExtDbTable
@@ -20,8 +23,78 @@ namespace bamboo\blueseal\remote\readextdbtable;
 class CReadExtDbTable extends AReadExtDbTable
 {
 
-    public function readTable($tablesName){
 
+    /**
+     * @param $tableName
+     * @param array $fields
+     * @param bool $full
+     * @return mixed
+     * @throws \bamboo\core\exceptions\BambooDBALException
+     */
+    public function readTables($tableName, array $fields, $full = false)
+    {
+        $select = $full ? "*" : implode(',', $fields);
+        $sql = "
+        SELECT  $select
+        FROM `$this->dbName`.`$tableName`
+         ";
+
+        $data = $this->client->query($sql, [])->fetchAll();
+
+        return $data;
     }
+
+    /**
+     * @param bool $isEqual
+     * @param string $remoteTable
+     * @param array $remoteFields
+     * @param array $remoteFieldsToSearch
+     * @param string $localTable
+     * @param array $localFieldsToCheck
+     * @throws BambooException
+     * @throws \bamboo\core\exceptions\BambooDBALException
+     */
+    public function insertData($isEqual = false,
+                               string $remoteTable,
+                               array $remoteFields,
+                               array $remoteFieldsToSearch,
+                               string $localTable,
+                               array $localFieldsToCheck)
+    {
+
+        //Prendo i dati dalla tabella remota
+        $data = $this->readTables($remoteTable, $remoteFields);
+
+        //Vedo tutti i campi nella tabella locale
+        $this->getTableFields($localTable);
+
+        /** @var CRepo $table */
+        $table = \Monkey::app()->repoFactory->create($localTable);
+
+        $countRemoteKeys = count($remoteFieldsToSearch);
+        $countLocalKeys = count($localFieldsToCheck);
+
+        if($countRemoteKeys != $countLocalKeys) throw new BambooException("The number of Local search keys in remote table must be identical to the number of Remote search keys");
+
+        foreach ($data as $v){
+
+            $keys = '';
+
+            for ($i = 0; $i < $countRemoteKeys; $i++) {
+                $keys .= '"'.$localFieldsToCheck[$i].'"'. ' => ' . '"'.$v[$remoteFieldsToSearch[$i]].'"'.','
+                ;
+            }
+
+            $a = json_decode($keys);
+
+            $extRow = $table->findOneBy([
+                $keys
+            ]);
+
+            $z = 3;
+        }
+
+
+      }
 
 }
