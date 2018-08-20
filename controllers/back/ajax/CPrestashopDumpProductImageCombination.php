203bc0ed360a716cbcaf4b1537de9684c174f2d2
juri fiorani <jurif@hotmail.com>
2018-08-20 22:23:02 +0200

    sviluppo api prestashop

--- controllers/back/ajax/CPrestashopDumpProductImageCombination.php
+++ controllers/back/ajax/CPrestashopDumpProductImageCombination.php
@@ -487,7 +487,7 @@ $productCombination =\Monkey::app()->repoFactory->create('ProductPublicSku')->fi
             $my_save_dir = '/media/sf_sites/PickyshopNew/tmp/';
 
         }else{
-            $my_save_dir = '/home/iwesshop/public_html/tmp/images/';
+            $my_save_dir = '/home/iwesshop/public_html/tmp/';
         }
         $filename = basename($url_to_image);
         $complete_save_loc = $my_save_dir . $filename;
@@ -499,7 +499,7 @@ $productCombination =\Monkey::app()->repoFactory->create('ProductPublicSku')->fi
             $image->save_to = '/media/sf_sites/PickyshopNew/tmp/';
 
         }else{
-            $image->save_to = '/home/iwesshop/public_html/tmp/images/';
+            $image->save_to = '/home/iwesshop/public_html/tmp/';
         }
 
 
@@ -516,9 +516,9 @@ $productCombination =\Monkey::app()->repoFactory->create('ProductPublicSku')->fi
 
 // change the local path where image has been downloaded "presta-api" is my local folder from where i run API script
         if(ENV=='prod') {
-            $img_path = '/home/iwesshop/public_html/tmp/presta-api/images/' . $image_name;
+            $img_path = '/home/iwesshop/public_html/tmp/' . $image_name;
         }else{
-            $img_path='/media/sf_sites/PickyshopNew/tmp/presta-api/images/'. $image_name;
+            $img_path='/media/sf_sites/PickyshopNew/tmp/'. $image_name;
         }
 
 
