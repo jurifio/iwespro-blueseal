45d7ee3db23b608f9c6819d409fe9e1807588cb9
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2018-08-27 10:01:07 +0200

    Sostituisci e trova

--- assets/js/product_model_manage_list.js
+++ assets/js/product_model_manage_list.js
@@ -1,48 +1,40 @@
 (function ($) {
 
 
-
-    $('#searchGender').on('input', function() {
+    $("#searchGender").keyup(function(event) {
         let searchTerm = $.trim(this.value);
         $('.sg').each(function() {
-            if (searchTerm.length < 1) {
-                $(this).show();
-            } else {
-                $(this).toggle($(this).filter('[data-searchgender*="' + searchTerm + '"]').length > 0);
+            if(event.keyCode === 13){
+                if(searchTerm.length < 1) { $(this).show() } else {$(this).toggle($(this).filter('[data-searchgender*="' + searchTerm + '"]').length > 0);}
             }
         });
     });
 
-    $('#searchMacroCategory').on('input', function() {
+    $("#searchMacroCategory").keyup(function(event) {
         let searchTerm = $.trim(this.value);
         $('.smcg').each(function() {
-            if (searchTerm.length < 1) {
-                $(this).show();
-            } else {
-                $(this).toggle($(this).filter('[data-searchmacrocategory*="' + searchTerm + '"]').length > 0);
+            if(event.keyCode === 13){
+                if(searchTerm.length < 1) { $(this).show() } else {$(this).toggle($(this).filter('[data-searchmacrocategory*="' + searchTerm + '"]').length > 0);}
             }
         });
     });
 
-    $('#searchCategory').on('input', function() {
+    $("#searchCategory").keyup(function(event) {
         let searchTerm = $.trim(this.value);
         $('.sc').each(function() {
-            if (searchTerm.length < 1) {
-                $(this).show();
-            } else {
-                $(this).toggle($(this).filter('[data-searchcategory*="' + searchTerm + '"]').length > 0);
+            if(event.keyCode === 13){
+                if(searchTerm.length < 1) { $(this).show() } else {$(this).toggle($(this).filter('[data-searchcategory*="' + searchTerm + '"]').length > 0);}
             }
         });
     });
 
-    $('#searchMaterial').on('input', function() {
+    $("#searchMaterial").keyup(function(event) {
         let searchTerm = $.trim(this.value);
         $('.sm').each(function() {
-            if (searchTerm.length < 1) {
-                $(this).show();
-            } else {
-                $(this).toggle($(this).filter('[data-searchmaterial*="' + searchTerm + '"]').length > 0);
+            if(event.keyCode === 13){
+                if(searchTerm.length < 1) { $(this).show() } else {$(this).toggle($(this).filter('[data-searchmaterial*="' + searchTerm + '"]').length > 0);}
             }
         });
-    })
+    });
+
 })(jQuery);
