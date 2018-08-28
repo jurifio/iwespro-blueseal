98d052cd4e0bc10f5d50c236e598610d6fabb6e4
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2018-08-28 18:21:54 +0200

    Trova dinamicamente

--- controllers/back/ajax/CDetailModelGetDetailsFason.php
+++ controllers/back/ajax/CDetailModelGetDetailsFason.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace bamboo\controllers\back\ajax;
+
 use bamboo\core\base\CObjectCollection;
 use bamboo\core\db\pandaorm\repositories\CRepo;
 use bamboo\domain\entities\CProductSheetModelPrototype;
@@ -27,130 +29,93 @@ class CDetailModelGetDetailsFason extends AAjaxController
         $step = \Monkey::app()->router->request()->getRequestData('step');
         /** @var CRepo $psmp */
         $psmp = \Monkey::app()->repoFactory->create('ProductSheetModelPrototype');
-        if($step == 1){
+        if ($step == 1) {
             $genderId = \Monkey::app()->router->request()->getRequestData('genderId');
 
-            /** @var CObjectCollection $psmpS1 */
-            $psmpS1 = \Monkey::app()->dbAdapter->query('SELECT categoryGroupId FROM ProductSheetModelPrototype WHERE genderId = ?',[$genderId])->fetchAll();
-
-            if(empty($psmpS1)){
-                return false;
-            } else {
-                $cats = [];
-
-                /** @var CProductSheetModelPrototype $val */
-                foreach ($psmpS1 as $val){
-                    $cats[] = $val["categoryGroupId"];
-                }
-
+            $psmpS1 = \Monkey::app()->cacheService->getCache("misc")->get("AllModelCategory-$genderId");
+            if (!$psmpS1) {
+                $psmpS1 = \Monkey::app()->dbAdapter->query('SELECT categoryGroupId FROM ProductSheetModelPrototype WHERE genderId = ?', [$genderId])->fetchAll();
+                if (empty($psmpS1)) return false;
+                $this->app->cacheService->getCache("misc")->set("AllModelCategory-$genderId", $psmpS1, 13000);
             }
+
+
+            $cats = [];
+
+            /** @var CProductSheetModelPrototype $val */
+            foreach ($psmpS1 as $val) {
+                $cats[] = $val["categoryGroupId"];
+            }
+
 
             $catsU = array_unique($cats);
 
             /** @var CRepo $catRepo */
-            $catRepo = \Monkey::app()->repoFactory->create('ProductSheetModelPrototypeCategoryGroup');
-            $i = 0;
+            $cats = implode(',', $catsU);
+            $query = "SELECT pmc.id, pmc.name, pmc.imageUrl AS img, pmc.description AS `desc` FROM ProductSheetModelPrototypeCategoryGroup pc
+                                                                  JOIN ProductSheetModelPrototypeMacroCategoryGroup pmc ON pc.macroCategoryGroupId = pmc.id
+                                                                  WHERE pc.id in ($cats)";
+            $macroCat = \Monkey::app()->dbAdapter->query($query, [])->fetchAll();
 
-            $macro = [];
-            foreach ($catsU as $cat){
-                /** @var CProductSheetModelPrototypeCategoryGroup $sCat */
-                //$sCat = \Monkey::app()->dbAdapter->query('SELECT FROM ProductSheetModelPrototypeMacroCategoryGroup');
-                $sCat = $catRepo->findOneBy(['id'=>$cat]);
-
-                //prendo la macro corrispondente
-
-                /** @var CProductSheetModelPrototypeMacroCategoryGroup $macroCat */
-                $macroCat = $sCat->productSheetModelPrototypeMacroCategoryGroup;
-                $macro[$i]['id'] = $macroCat->id;
-                $macro[$i]['name'] = $macroCat->name;
-                $macro[$i]['img'] = $macroCat->imageUrl;
-                $macro[$i]['desc'] = $macroCat->description;
-                $i++;
-            }
-            return json_encode(array_unique($macro, SORT_REGULAR));
-
+            return json_encode(array_unique($macroCat, SORT_REGULAR));
         }
 
 
-        if($step == 2){
+        if ($step == 2) {
 
             $genderId = \Monkey::app()->router->request()->getRequestData('genderId');
             $macroCategId = \Monkey::app()->router->request()->getRequestData('macroCategId');
 
-            /** @var CObjectCollection $psmpS2 */
-            $psmpS2 = $psmp->findBy(['genderId'=>$genderId]);
+            $psmpS2 = \Monkey::app()->cacheService->getCache("misc")->get("AllModelCategory-$genderId");
 
-            if($psmpS2->isEmpty()){
+            if (empty($psmpS2)) {
                 return false;
             } else {
                 $cats1 = [];
 
                 /** @var CProductSheetModelPrototype $val */
-                foreach ($psmpS2 as $val){
-                    $cats1[] = $val->categoryGroupId;
+                foreach ($psmpS2 as $val) {
+                    $cats1[] = $val['categoryGroupId'];
                 }
-
             }
 
             $catsU = array_unique($cats1);
 
             /** @var CRepo $catRepo */
-            $catRepo = \Monkey::app()->repoFactory->create('ProductSheetModelPrototypeCategoryGroup');
-            $catInfo1 = [];
-            $i = 0;
-            foreach ($catsU as $cat){
-                /** @var CProductSheetModelPrototypeCategoryGroup $sCat1 */
-                $sCat1 = $catRepo->findOneBySql('SELECT * FROM ProductSheetModelPrototypeCategoryGroup p WHERE p.id = ? AND p.macroCategoryGroupId = ?',
-                    [$cat, $macroCategId]);
+            $cats = implode(',', $catsU);
+            $query = "SELECT p.id, p.name, p.imageUrl AS img, p.description AS `desc` 
+                      FROM ProductSheetModelPrototypeCategoryGroup p WHERE p.id in ($cats) AND p.macroCategoryGroupId = $macroCategId";
+            $catInfo1 = \Monkey::app()->dbAdapter->query($query, [])->fetchAll();
 
-                if(!is_null($sCat1)) {
-                    $catInfo1[$i]['id'] = $sCat1->id;
-                    $catInfo1[$i]['name'] = $sCat1->name;
-                    $catInfo1[$i]['img'] = $sCat1->imageUrl;
-                    $catInfo1[$i]['desc'] = $sCat1->description;
-                    $i++;
-                }
-            }
             return json_encode($catInfo1);
-
-
 
         }
 
 
-
-        if($step == 3){
+        if ($step == 3) {
             $genderId = \Monkey::app()->router->request()->getRequestData('genderId');
             $categId = \Monkey::app()->router->request()->getRequestData('categId');
 
-            /** @var CObjectCollection $psmpS2 */
-            $psmpS2 = $psmp->findBy(['genderId'=>$genderId, 'categoryGroupId'=>$categId]);
+            $psmpS2 = \Monkey::app()->dbAdapter->query('SELECT materialId FROM ProductSheetModelPrototype WHERE genderId = ? AND categoryGroupId = ?', [$genderId, $categId])->fetchAll();
 
-            if($psmpS2->isEmpty()){
+            if (empty($psmpS2)) {
                 return false;
             } else {
                 $mats = [];
                 /** @var CProductSheetModelPrototype $valS2 */
-                foreach ($psmpS2 as $valS2){
-                    $mats[] = $valS2->materialId;
+                foreach ($psmpS2 as $valS2) {
+                    $mats[] = $valS2["materialId"];
                 }
             }
             // non ci possono essere due materiali uguali con lo stesso genere e gruppo categoria
             $matsU = array_unique($mats);
 
-            if($matsU === $mats){
-                $matInfo = [];
-                $y = 0;
-                /** @var CRepo $matRepo */
-                $matRepo = \Monkey::app()->repoFactory->create('ProductSheetModelPrototypeMaterial');
+            if ($matsU === $mats) {
 
-                foreach ($matsU as $mat){
-                    /** @var CProductSheetModelPrototypeMaterial $rMat */
-                    $rMat = $matRepo->findOneBy(['id'=>$mat]);
-                    $matInfo[$y]['id'] = $rMat->id;
-                    $matInfo[$y]['name'] = $rMat->name;
-                    $y++;
-                }
+                /** @var CRepo $catRepo */
+                $matsS = implode(',', $matsU);
+                $query = "SELECT p.id, p.name FROM ProductSheetModelPrototypeMaterial p WHERE p.id in ($matsS)";
+                $matInfo = \Monkey::app()->dbAdapter->query($query, [])->fetchAll();
 
                 return json_encode($matInfo);
 
@@ -160,16 +125,16 @@ class CDetailModelGetDetailsFason extends AAjaxController
         }
 
 
-        if($step == 4){
+        if ($step == 4) {
             $genderId = \Monkey::app()->router->request()->getRequestData('genderId');
             $categId = \Monkey::app()->router->request()->getRequestData('categId');
             $matId = \Monkey::app()->router->request()->getRequestData('matId');
 
             /** @var CObjectCollection $psmpS3 */
-            $psmpS3 = $psmp->findBy(['genderId'=>$genderId, 'categoryGroupId'=>$categId, 'materialId'=>$matId]);
+            $psmpS3 = $psmp->findBy(['genderId' => $genderId, 'categoryGroupId' => $categId, 'materialId' => $matId]);
 
             //non possono esistere due modelli con le stesse caratteristiche impostate da default per i fason
-            if($psmpS3->count() > 1 || $psmpS3->isEmpty()){
+            if ($psmpS3->count() > 1 || $psmpS3->isEmpty()) {
                 return false;
             } else {
                 /** @var CProductSheetModelPrototype $prodSFinal */
