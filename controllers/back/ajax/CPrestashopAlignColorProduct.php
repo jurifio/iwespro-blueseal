90b954761f010dab9d2cdc76bd54fe95c9b6f581
juri fiorani <jurif@hotmail.com>
2018-11-27 02:23:36 +0100

    aggiornamento Colori CprestahopAlignColorProduct settaggio a modifica dei prodotti in prestashop

--- controllers/back/ajax/CPrestashopAlignColorProduct.php
+++ controllers/back/ajax/CPrestashopAlignColorProduct.php
@@ -2,7 +2,7 @@
 
 namespace bamboo\controllers\back\ajax;
 
-
+use bamboo\core\exceptions\BambooException;
 use bamboo\blueseal\remote\readextdbtable\CReadExtDbTable;
 use PrestaShopWebservice;
 use PrestaShopWebserviceException;
@@ -209,12 +209,18 @@ ORDER BY `p`.`id`";
             $quantity_product = $value_product['quantity'];
             $reference =$value_product['productId']."-".$value_product['productVariantId'];
            $color=$value_product['color'];
+            try {
+                $stmtUpdateProduct = $db_con->prepare("INSERT INTO  psz6_feature_product (`id_feature`,`id_product`,`id_feature_value`) VALUES ('4657'," . $p .
+                    ",'(select pfv.id_feature_value from psz6_feature_value pfv inner join psz6_feature_value_lang pfvn on pfv.id_feature_value=pfvn.id_feature_value where
+                 pfvn.id_lang=2 and pfv.id_feature=4657 and pfvn.value ='" . $color . "')')");
 
-            $stmtUpdateProduct = $db_con->prepare("INSERT INTO  psz6_feature_product (`id_feature`,`id_product`,`id_feature_value`) VALUES ('4657',".$p.
-                ",(select pfv.id_feature_value from psz6_feature_value pfv inner join psz6_feature_value_lang pfvn on pfv.id_feature_value=pfvn.id_feature_value where
-                 pfvn.id_lang=2 and pfvn.value ='".$color."'))");
-            $stmtUpdateProduct->execute();
-
+                $stmtUpdateProduct->execute();
+            }catch(BambooException $e) {
+              $query="INSERT INTO  psz6_feature_product (`id_feature`,`id_product`,`id_feature_value`) VALUES ('4657'," . $p .
+                  ",'(select pfv.id_feature_value from psz6_feature_value pfv inner join psz6_feature_value_lang pfvn on pfv.id_feature_value=pfvn.id_feature_value where
+                 pfvn.id_lang=2 and pfv.id_feature=4657 and pfvn.value ='" . $color . "')')";
+                $this->error($query, $e->getMessage());
+            }
 
 
             }
