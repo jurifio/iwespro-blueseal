e2a93291ad01de65690f2dae9da005351bc28b1b
juri fiorani <jurif@hotmail.com>
2018-11-27 03:32:33 +0100

    aggiornamento Colori CprestahopAlignColorProduct settaggio a modifica dei prodotti in prestashop

--- controllers/back/ajax/CPrestashopAlignColorProduct.php
+++ controllers/back/ajax/CPrestashopAlignColorProduct.php
@@ -213,7 +213,12 @@ $res="";
                 $stmtUpdateProduct = $db_con->prepare("INSERT INTO  psz6_feature_product (`id_feature`,`id_product`,`id_feature_value`) VALUES ('4657'," . $p .
                     ",(select pfv.id_feature_value from psz6_feature_value pfv inner join psz6_feature_value_lang pfvn on pfv.id_feature_value=pfvn.id_feature_value where
                  pfvn.id_lang=2 and pfv.id_feature=4657 and pfvn.value ='".$color."'))");
-                $stmtUpdateProduct->execute();
+            if ($stmtUpdateProduct->execute()) {
+                // it worked
+            } else {
+                continue;
+            }
+
 
             }
 
