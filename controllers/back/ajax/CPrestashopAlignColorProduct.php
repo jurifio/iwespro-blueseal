50fdda9109b741354fe10122c39adf273830382c
juri fiorani <jurif@hotmail.com>
2018-11-27 03:09:17 +0100

    aggiornamento Colori CprestahopAlignColorProduct settaggio a modifica dei prodotti in prestashop

--- controllers/back/ajax/CPrestashopAlignColorProduct.php
+++ controllers/back/ajax/CPrestashopAlignColorProduct.php
@@ -213,7 +213,9 @@ ORDER BY `p`.`id`";
                 $stmtUpdateProduct = $db_con->prepare("INSERT INTO  psz6_feature_product (`id_feature`,`id_product`,`id_feature_value`) VALUES ('4657'," . $p .
                     ",(select pfv.id_feature_value from psz6_feature_value pfv inner join psz6_feature_value_lang pfvn on pfv.id_feature_value=pfvn.id_feature_value where
                  pfvn.id_lang=2 and pfv.id_feature=4657 and pfvn.value ='" . trim($color) . "'))");
-
+                $res="<br>"."INSERT INTO  psz6_feature_product (`id_feature`,`id_product`,`id_feature_value`) VALUES ('4657'," . $p .
+                    ",(select pfv.id_feature_value from psz6_feature_value pfv inner join psz6_feature_value_lang pfvn on pfv.id_feature_value=pfvn.id_feature_value where
+                 pfvn.id_lang=2 and pfv.id_feature=4657 and pfvn.value ='" . trim($color) . "'))"."<br>";
                 $stmtUpdateProduct->execute();
 
             }
