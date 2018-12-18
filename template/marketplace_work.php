0cdfe60e2570d454fd7099c36b666d8e5c4dc01e
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2018-12-18 18:47:02 +0100

    Fix agg

--- template/marketplace_work.php
+++ template/marketplace_work.php
@@ -58,7 +58,7 @@
                                                     <?php
                                                         if(!is_null($user->foison)) $statusB = $user->foison->foisonHasInterest->findOneByKey('workCategoryId',$pb->workCategoryId)->foisonStatusId;
                                                     ?>
-                                                    <button class="btn btn-info btn-xs acceptPB <?php echo $pb->id;?>" <?php if ($foisonRank > \bamboo\domain\entities\CFoison::MININUM_RANK || $statusB == 1 || $statusB == 4 || $statusB == 5 || $el === 0 || $hasOpenedProductBatch) echo 'disabled' ?> data-pbId="<?php echo $pb->id;?>">
+                                                    <button class="btn btn-info btn-xs acceptPB <?php echo $pb->id;?>" <?php if ($foisonRank < \bamboo\domain\entities\CFoison::MININUM_RANK || $statusB == 1 || $statusB == 4 || $statusB == 5 || $el === 0 || $hasOpenedProductBatch) echo 'disabled' ?> data-pbId="<?php echo $pb->id;?>">
                                                         Prenota
                                                     </button>
                                                 </div>
