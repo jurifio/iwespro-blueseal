2d9f04a41dd98abb31b52c4afa6f5a584a6e1dcd
juri fiorani <jurif@hotmail.com>
2019-02-01 16:10:36 +0100

    inserimento ean su prodotto padre

--- controllers/back/ajax/CInvoiceAjaxController.php
+++ controllers/back/ajax/CInvoiceAjaxController.php
@@ -359,7 +359,7 @@ class CInvoiceAjaxController extends AAjaxController
                                 'magazzino'=>$magazzino
                             ];
                         }
-                        $tot = number_format($tot,2)- number_format($scontotot,2);
+                        $tot = number_format($tot,2) - number_format($scontotot,2);
                         $today = new \DateTime();
                         $dateInvoice = $today->format('d/m/Y');
                         $insertJson.= json_encode($articoli).',
@@ -418,8 +418,12 @@ class CInvoiceAjaxController extends AAjaxController
                             $resultApi = "Errore=" . $result['error'] . " codice di errore:" . $result['error_code'];
                         }
                         \Monkey::app()->applicationLog('InvoiceAjaxController', 'Report', 'ResponseApi fatture in Cloud Numero'.$number.' data:'.$dateInvoice, 'Risposta FatturaincCloud',$resultApi);
-                        $fattureinCloudId=$result['new_id'];
-                        $fattureinCloudToken=$result['token'];
+                        if(array_key_exists('new_id',$result)) {
+                            $fattureinCloudId = $result['new_id'];
+                        }
+                        if(array_key_exists('token',$result)) {
+                            $fattureinCloudToken = $result['token'];
+                        }
                         $updateInvoice=\Monkey::app()->repoFactory->create('Invoice')->findOneBy(['orderId'=>$orderId]);
                         $updateInvoice->fattureInCloudId=$fattureinCloudId;
                         $updateInvoice->fattureInCloudToken=$fattureinCloudToken;
