4340e06d131d79311211da8088d9ac563b5db110
Ciro Lorenzetti <cirolorenzetti@gmail.com>
2019-02-07 16:57:03 +0100

    Prodotti catalogo

--- marketplace/prestashop/CPrestashopCategory.php
+++ marketplace/prestashop/CPrestashopCategory.php
@@ -2,9 +2,13 @@
 
 
 namespace bamboo\blueseal\marketplace\prestashop;
+use bamboo\controllers\api\Helper\DateTime;
 use bamboo\core\base\CObjectCollection;
+use bamboo\core\db\pandaorm\repositories\CRepo;
 use bamboo\core\theming\nestedCategory\CCategoryManager;
 use bamboo\domain\entities\CProductCategory;
+use bamboo\domain\entities\CProductCategoryHasPrestashopCategory;
+use bamboo\domain\entities\CProductCategoryTranslation;
 
 
 /**
@@ -22,6 +26,7 @@ use bamboo\domain\entities\CProductCategory;
  */
 class CPrestashopCategory extends APrestashopMarketplace
 {
+
     /**
      * @return \SimpleXMLElement
      * @throws \PrestaShopWebserviceException
@@ -34,6 +39,7 @@ class CPrestashopCategory extends APrestashopMarketplace
 
     /**
      * @param CObjectCollection $productCategories
+     * @return bool
      * @throws \PrestaShopWebserviceException
      */
     public function addAllCategories(CObjectCollection $productCategories)
@@ -94,16 +100,79 @@ class CPrestashopCategory extends APrestashopMarketplace
         return true;
     }
 
-    private function duplicateChildNodes($blankXml){
-        // Strip it out so it's not passed by reference
-        $newNode = new \SimpleXMLElement($blankXml->category->asXML());
+    /**
+     * @param CObjectCollection $productCategories
+     * @throws \PrestaShopWebserviceException
+     */
+    public function addNewCategories(CObjectCollection $productCategories){
 
-        // Create a dummy placeholder for it wherever you need it
-        $blankXml->addChild('replaceMe');
+        /** @var CRepo $pchpcR */
+        $pchpcR = \Monkey::app()->repoFactory->create('ProductCategoryHasPrestashopCategory');
 
-        // Do a string replace on the empty fake node
-        $blankXml = str_replace('<replaceMe/>',$newNode->asXML(),$blankXml->asXML());
-        $xml = str_replace('<?xml version="1.0"?>', '', $blankXml);
+        /** @var CCategoryManager $categoryManager */
+        $categoryManager = \Monkey::app()->categoryManager;
 
+        /** @var CProductCategory $productCategory */
+        foreach ($productCategories as $productCategory){
+
+            //search category in table
+            /** @var CProductCategoryHasPrestashopCategory $pchpc */
+            $pchpc = $pchpcR->findOneBy(['productCategoryId' => $productCategory->id]);
+
+            //if i found category is in prestashop
+            if(!is_null($pchpc)) continue;
+
+            $fatherId = $categoryManager->getCategoryParent($productCategory->id)['id'];
+
+            /** @var CProductCategoryHasPrestashopCategory $pchpcFather */
+            $pchpcFather = $pchpcR->findOneBy(['productCategoryId' => $fatherId]);
+            if(!is_null($pchpcFather)){
+                //if father is in table insert new cat
+                $this->insertPrestashopCategory($productCategory, $pchpcFather);
+            } else {
+
+            }
+
+        }
+    }
+
+    /**
+     * @param CProductCategory $productCategory
+     * @param CProductCategoryHasPrestashopCategory $pchpcFather
+     * @return bool
+     * @throws \PrestaShopWebserviceException
+     */
+    public function insertPrestashopCategory(CProductCategory $productCategory, CProductCategoryHasPrestashopCategory $pchpcFather){
+
+        /** @var \SimpleXMLElement $blankXml */
+        $blankXml = $this->getCategoryBlankSchema();
+
+        /** @var CProductCategoryTranslation $productCategoryTranslation */
+        $productCategoryTranslation = $productCategory->productCategoryTranslation->findOneByKey('langId', 1);
+
+        $date = date_format(new \DateTime(), 'Y-m-d H:i:s');
+        $categorySlug = $productCategoryTranslation->slug;
+        $categoryName = $productCategoryTranslation->name;
+        
+
+        $blankXml[0]->children()->id_parent = $pchpcFather->prestashopCategoryId;
+        $blankXml[0]->children()->active = 1;
+        $blankXml[0]->children()->id_shop_default = 1;
+        $blankXml[0]->children()->is_root_category = 0;
+        $blankXml[0]->children()->position = 0;
+        $blankXml[0]->children()->date_add = $date;
+        $blankXml[0]->children()->date_upd = $date;
+        $blankXml[0]->children()->name->language[0][0] = $categoryName;
+        $blankXml[0]->children()->link_rewrite->language[0][0] = $categorySlug;
+        $blankXml[0]->children()->description->language[0][0] = $categoryName;
+        $blankXml[0]->children()->meta_title->language[0][0] = $categoryName;
+        $blankXml[0]->children()->meta_description->language[0][0] = $categoryName;
+        $blankXml[0]->children()->meta_keywords->language[0][0] = $categoryName;
+
+        $opt = array('resource' => 'categories');
+        $opt['postXml'] = $blankXml->asXML();
+        $response = $this->ws->add($opt);
+
+        return true;
     }
 }
