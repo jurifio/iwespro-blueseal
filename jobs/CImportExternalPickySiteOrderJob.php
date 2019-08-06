523e261550260ae90d9c0c97abf9607b2045ff90
juri fiorani <jurif@hotmail.com>
2019-08-06 19:58:05 +0200

    allineamento ean

--- jobs/CImportExternalPickySiteOrderJob.php
+++ jobs/CImportExternalPickySiteOrderJob.php
@@ -67,7 +67,7 @@ class CImportExternalPickySiteOrderJob extends ACronJob
             $shop =$value->id;
             try {
 
-                $db_con = new PDO('mysql:host={$db_host};dbname={$db_name}', $db_user, $db_pass);
+                $db_con = new PDO("mysql:host={$db_host};dbname={$db_name}", $db_user, $db_pass);
                 $db_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $res .= " connessione ok <br>";
             } catch (PDOException $e) {
