6cbd7c2ee79c960c78214c34e4e2e6ac3872a21e
juri fiorani <jurif@hotmail.com>
2019-12-23 17:34:28 +0100

    creazione Vista Vendite

---
+++ jobs/CUpdateCouponValidityJob.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace bamboo\blueseal\jobs;
+
+use bamboo\domain\entities\CCartAbandonedEmailSend;
+use bamboo\domain\repositories\CCartAbandonedEmailSendRepo;
+use bamboo\domain\entities\COrder;
+use bamboo\domain\entities\CCart;
+use bamboo\domain\entities\CCartAbandonedEmailParam;
+use bamboo\domain\entities\CCouponType;
+use bamboo\domain\entities\CCoupon;
+use bamboo\domain\entities\CCartLine;
+use bamboo\core\base\CSerialNumber;
+use bamboo\core\db\pandaorm\repositories\ARepo;
+use bamboo\domain\repositories\CEmailRepo;
+use bamboo\blueseal\remote\readextdbtable\CReadExtDbTable;
+use PDO;
+use prepare;
+
+use bamboo\core\jobs\ACronJob;
+use bamboo\domain\entities\CProductPublicSku;
+use bamboo\domain\entities\CProduct;
+use bamboo\core\events\AEventListener;
+
+
+/**
+ * Class CUpdateCouponValidityJob
+ * @package bamboo\blueseal\jobs
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 23/12/2019
+ * @since 1.0
+ */
+class CUpdateCouponValidityJob extends ACronJob
+{
+    /**
+     * @param null $args
+     * @throws \bamboo\core\exceptions\BambooDBALException
+     */
+    public function run($args = null)
+    {
+        $dateNow = date_create('now')->format('Y-m-d H:i:s');
+        $coupons = \Monkey::app()->repoFactory->create('Coupon')->findAll();
+
+
+
+        foreach($coupons as  $coupon){
+            $validityDate=strtotime($coupon->validThru);
+            if($dateNow > $validityDate ){
+                $coupon->valid=0;
+                $coupon->update();
+                $this->report('CUpdateCouponValidityJob','set validity to 0',$coupon->code);
+            }
+        }
+
+    }
+
+
+}
