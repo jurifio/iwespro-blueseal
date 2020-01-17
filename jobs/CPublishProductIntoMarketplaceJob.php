89da38b90f01974e907f40df678a5d0824eed5ec
juri fiorani <jurif@hotmail.com>
2020-01-17 19:18:46 +0100

    creazione Vista Vendite

--- jobs/CPublishProductIntoMarketplaceJob.php
+++ jobs/CPublishProductIntoMarketplaceJob.php
@@ -11,6 +11,8 @@ use bamboo\domain\entities\CProductPublicSku;
 use bamboo\domain\entities\CProduct;
 use bamboo\core\events\AEventListener;
 use bamboo\domain\repositories\CMarketplaceAccountHasProductRepo;
+use bamboo\domain\repositories\CProductRepo;
+
 
 
 
@@ -55,6 +57,7 @@ class CPublishProductIntoMarketplaceJob extends ACronJob
         }
         $marketplaceAccountRepo = \Monkey::app()->repoFactory->create('MarketplaceAccount');
         $productBrandRepo = \Monkey::app()->repoFactory->create('ProductBrand');
+        /** @var CProductRepo $productRepo */
         $productRepo = \Monkey::app()->repoFactory->create('Product');
         $productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
         $marketplaceRepo = \Monkey::app()->repoFactory->create('Marketplace');
@@ -62,116 +65,134 @@ class CPublishProductIntoMarketplaceJob extends ACronJob
         $marketplaceAccounts = $marketplaceAccountRepo->findBy(['id' => 32,'marketplaceId' => 9]);
 
         foreach ($marketplaceAccounts as $marketplaceAccount) {
-            $markeplaceId = $marketplaceAccount->marketplaceId;
-            $marketplaceAccountId = $marketplaceAccount->id;
-            $activeAutomatic = isset($marketplaceAccount->config['activeAutomatic']) ? $marketplaceAccount->config['activeAutomatic'] : 0;
+            try {
+                $markeplaceId = $marketplaceAccount->marketplaceId;
+                $marketplaceAccountId = $marketplaceAccount->id;
+                $activeAutomatic = isset($marketplaceAccount->config['activeAutomatic']) ? $marketplaceAccount->config['activeAutomatic'] : 0;
 
-            $isActive = isset($marketplaceAccount->config['isActive']) ? $marketplaceAccount->config['isActive'] : 0;
-            if ($isActive == 0) {
-                continue;
-            }
-            $rows = [];
-            $countProduct = 0;
-            $bodyMail = 'Elenco Prodotti in Pubblicazione per aggregatore '.$marketplaceAccount->name.':<br>';
-            $marketplace = $marketplaceRepo->findOneBy(['id' => $marketplaceAccount->marketplaceId]);
-            $priceRange1=explode('-',$config['priceModifierRange1']);
-            $priceRange2=explode('-',$config['priceModifierRange2']);
-            $priceRange3=explode('-',$config['priceModifierRange3']);
-            $priceRange4=explode('-',$config['priceModifierRange4']);
-            $priceRange5=explode('-',$config['priceModifierRange5']);
+                $isActive = isset($marketplaceAccount->config['isActive']) ? $marketplaceAccount->config['isActive'] : 0;
+                if ($isActive == 0) {
+                    continue;
+                }
+                $rows = [];
+                $countProduct = 0;
+                $bodyMail = '<html><body>Elenco Prodotti in Pubblicazione per aggregatore ' . $marketplaceAccount->name . ':<br><table><thead><th>codice</th><th>brand</th><th>categoria</th><th>cpc Desktop</th><th>cpc Mobile</th></thead>';
+                $marketplace = $marketplaceRepo->findOneBy(['id' => $marketplaceAccount->marketplaceId]);
+                $priceRange1 = explode('-',$marketplaceAccount->config['priceModifierRange1']);
+                $priceRange2 = explode('-',$marketplaceAccount->config['priceModifierRange2']);
+                $priceRange3 = explode('-',$marketplaceAccount->config['priceModifierRange3']);
+                $priceRange4 = explode('-',$marketplaceAccount->config['priceModifierRange4']);
+                $priceRange5 = explode('-',$marketplaceAccount->config['priceModifierRange5']);
 
 
-            $filters = explode(',',json_encode($marketplaceAccount->config['ruleOption'],false));
-            foreach ($filters as $filter) {
-                $brandShop = explode('-',$filter);
-                $brand = $brandShop[0];
-                $shopD = $brandShop[1];
-                $productBrand = $productBrandRepo->findOneBy(['id' => $brand]);
-                $brandName = $productBrand->name;
-                $products = $productRepo->findBy(['productBrandId' => $brand,'productStatusId' => 6]);
-                foreach ($products as $product) {
-                    if ($product->qty >= 1) {
-                        $productSku = $productSkuRepo->findOneBy(['productId' => $product->id,'productVariantId' => $product->productVariantId,'shopId' => $shopD]);
-                        if ($productSku != null) {
-                            if ($marketplace->type == 'cpc') {
-                                if ($activeAutomatic == 0) {
-                                    $fee = $marketplaceAccount->config['defaultCpc'];
-                                    $feeMobile = $marketplaceAccount->config['defaultCpcM'];
-                                } else {
-                                    $price = $productSku -> price;
-                                    $salePrice = $productSku -> salePrice;
-                                    if ($isOnSale == 1) {
-                                        $activePrice = $salePrice;
+                $filters = explode(',',json_encode($marketplaceAccount->config['ruleOption'],false));
+                foreach ($filters as $filter) {
+                    $brandShop = explode('-',$filter);
+                    $brand = $brandShop[0];
+                    $shopD = $brandShop[1];
+                    $productBrand = $productBrandRepo->findOneBy(['id' => $brand]);
+                    $brandName = $productBrand->name;
+
+                    $products = $productRepo->findBy(['productBrandId' => $brand,'productStatusId' => 6]);
+
+                    foreach ($products as $product) {
+                        $isOnSale = $product->isOnSale;
+                        if ($product->qty >= 1) {
+                            $productSku = $productSkuRepo->findOneBy(['productId' => $product->id,'productVariantId' => $product->productVariantId,'shopId' => $shopD]);
+                            if ($productSku != null) {
+                                if ($marketplace->type == 'cpc') {
+                                    if ($activeAutomatic == 0) {
+                                        $fee = $marketplaceAccount->config['defaultCpc'];
+                                        $feeMobile = $marketplaceAccount->config['defaultCpcM'];
                                     } else {
-                                        $activePrice = $price;
+                                        $price = $productSku->price;
+                                        $salePrice = $productSku->salePrice;
+                                        if ($isOnSale == 1) {
+                                            $activePrice = $salePrice;
+                                        } else {
+                                            $activePrice = $price;
+                                        }
+
+
+                                        switch (true) {
+                                            case $activePrice >= $priceRange1[0] && $activePrice <= $priceRange1[1]:
+                                                $fee = $marketplaceAccount->config['range1Cpc'];
+                                                $feeMobile = $marketplaceAccount->config['range1CpcM'];
+                                                break;
+                                            case $activePrice >= $priceRange2[0] && $activePrice <= $priceRange2[1]:
+                                                $fee = $marketplaceAccount->config['range2Cpc'];
+                                                $feeMobile = $marketplaceAccount->config['range2CpcM'];
+                                                break;
+                                            case $activePrice >= $priceRange3[0] && $activePrice <= $priceRange3[1]:
+                                                $fee = $marketplaceAccount->config['range3Cpc'];
+                                                $feeMobile = $marketplaceAccount->config['range3CpcM'];
+                                                break;
+                                            case $activePrice >= $priceRange4[0] && $activePrice <= $priceRange4[1]:
+                                                $fee = $marketplaceAccount->config['range4Cpc'];
+                                                $feeMobile = $marketplaceAccount->config['range4CpcM'];
+
+                                                break;
+                                            case $activePrice >= $priceRange5[0] && $activePrice <= $priceRange5[1]:
+                                                $fee = $marketplaceAccount->config['range5Cpc'];
+                                                $feeMobile = $marketplaceAccount->config['range5CpcM'];
+                                                break;
+                                        }
                                     }
-
-                                    $priceRange1=explode('-',$marketplaceAccount->config['priceModifierRange1']);
-                                    $priceRange2=explode('-',$marketplaceAccount->config['priceModifierRange2']);
-                                    $priceRange3=explode('-',$marketplaceAccount->config['priceModifierRange3']);
-                                    $priceRange4=explode('-',$marketplaceAccount->config['priceModifierRange4']);
-                                    $priceRange5=explode('-',$marketplaceAccount->config['priceModifierRange5']);
-
-                                    switch(true){
-                                        case $activePrice>=$priceRange1[0] && $activePrice<=$priceRange1[1]:
-                                            $fee=$marketplaceAccount->config['range1Cpc'];
-                                            $feeMobile=$marketplaceAccount->config['range1CpcM'];
-                                            break;
-                                        case $activePrice>=$priceRange2[0] && $activePrice<=$priceRange2[1]:
-                                            $fee=$config['range2Cpc'];
-                                            $feeMobile=$marketplaceAccount->config['range2CpcM'];
-                                            break;
-                                        case $activePrice>=$priceRange3[0] && $activePrice<=$priceRange3[1]:
-                                            $fee=$marketplaceAccount->config['range3Cpc'];
-                                            $feeMobile=$marketplaceAccount->config['range3CpcM'];
-                                            break;
-                                        case $activePrice>=$priceRange4[0] && $activePrice<=$priceRange4[1]:
-                                            $fee=$marketplaceAccount->config['range4Cpc'];
-                                            $feeMobile=$marketplaceAccount->config['range4CpcM'];
-
-                                            break;
-                                        case $activePrice>=$priceRange5[0] && $activePrice<=$priceRange5[1]:
-                                            $fee=$marketplaceAccount->config['range5Cpc'];
-                                            $feeMobile=$marketplaceAccount->config['range5CpcM'];
-                                            break;
-                                    }
+                                } else {
+                                    $fee = 'nessun cpc ';
+                                    $feeMobile = 'nessun cpc Mobile';
                                 }
-                            }else{
-                                $fee='nessun cpc ';
-                                $feeMobile='nessun cpc Mobile';
+                                $countProduct++;
+                                array_push($rows,[$productSku->productId . '-' . $product->productVariantId]);
+                                $bodyMail .= " <tr><td>" . $productSku->productId . '-' . $productSku->productVariantId . ' </td><td>' . $brandName . '</td><td>' . $product->getLocalizedProductCategories('/','/') . ' </td><td>' . $fee . ' </td><td>' . $feeMobile . '</td></tr>';
                             }
-                            $countProduct++;
-                            array_push($rows,[$productSku->productId . '-' . $product->productVariantId]);
-                            $bodyMail .= "codice:" . $productSku->productId . '-' . $productSku->productVariantId . ' brand:' . $brandName . ' categoria:' . $val->getLocalizedProductCategories('','/'). 'cpc Desktop:'.$fee. 'cpc Mobile:'.$feeMobile.'</br>';
-                        }
 
-                    } else {
-                        continue;
+                        } else {
+                            continue;
+                        }
+                    }
+
+                }
+                /** @var CMarketplaceAccountHasProductRepo $marketplaceAccountHasProductRepo */
+                $marketplaceAccountHasProductRepo = \Monkey::app()->repoFactory->create('MarketplaceAccountHasProduct');
+                \Monkey::app()->repoFactory->beginTransaction();
+                foreach ($rows as $row) {
+                    try {
+                        $ids = [];
+                        set_time_limit(6);
+                        $product = $productRepo->findOneByStringId($row);
+                        $marketplaceAccountHasProduct = $marketplaceAccountHasProductRepo->addProductToMarketplaceAccountJob($product,$marketplaceAccount,$activeAutomatic);
+                        $i++;
+                        \Monkey::app()->repoFactory->commit();
+                    } catch
+                    (\Throwable $e) {
+                        \Monkey::app()->repoFactory->rollback();
+                        throw $e;
                     }
                 }
+                $bodyMail.='</table></body></html>';
+                $bodyMail .= 'numero Prodotti Totali in Coda di Pubblicazione ' . $countProduct;
+                /** @var CEmailRepo $mailRepo */
+                $mailRepo = \Monkey::app()->repoFactory->create('Email');
+                $mailRepo->newMail('it@iwes.it',["gianluca@iwes.it","juri@iwes.it"],[],[],"coda Pubblicazione su " . $marketplaceAccount->name,$bodyMail);
+                $aggregatorPublishLog=\Monkey::app()->repoFactory->create('AggregatorPublishLog')->getEmptyEntity();
+                $aggregatorPublishLog->marketplaceAccountId=$markeplaceAccountId;
+                $aggregatorPublishLog->marketplaceId=$markeplaceId;
+                $aggregatorPublishLog->subject="Pubblicazione su " . $marketplaceAccount->name;
+                $aggregatorPublishLog->result="success";
+                $aggregatorPublishLog->email="gianluca@iwes.it,juri@iwes.it";
 
+            }catch(\Throwable $e){
+                $bodyMail='<html><body>errore nella mail<br>'.$e.'</body></html>';
+                $mailRepo = \Monkey::app()->repoFactory->create('Email');
+                $mailRepo->newMail('it@iwes.it',["gianluca@iwes.it","juri@iwes.it"],[],[],"coda Pubblicazione su " . $marketplaceAccount->name,$bodyMail);
+                $aggregatorPublishLog=\Monkey::app()->repoFactory->create('AggregatorPublishLog')->getEmptyEntity();
+                $aggregatorPublishLog->marketplaceAccountId=$markeplaceAccountId;
+                $aggregatorPublishLog->marketplaceId=$markeplaceId;
+                $aggregatorPublishLog->subject="Pubblicazione su " . $marketplaceAccount->name;
+                $aggregatorPublishLog->result="error";
+                $aggregatorPublishLog->email="gianluca@iwes.it,juri@iwes.it";
             }
-            /** @var CMarketplaceAccountHasProductRepo $marketplaceAccountHasProductRepo */
-            $marketplaceAccountHasProductRepo = \Monkey::app()->repoFactory->create('MarketplaceAccountHasProduct');
-            \Monkey::app()->repoFactory->beginTransaction();
-            foreach ($rows as $row) {
-                try {
-                    $ids = [];
-                    set_time_limit(6);
-                    $product = $productRepo->findOneByStringId($row);
-                    $marketplaceAccountHasProduct = $marketplaceAccountHasProductRepo->addProductToMarketplaceAccountJob($product,$marketplaceAccount,$activeAutomatic);
-                    $i++;
-                    \Monkey::app()->repoFactory->commit();
-                } catch
-                (\Throwable $e) {
-                    \Monkey::app()->repoFactory->rollback();
-                    throw $e;
-                }
-            }
-            $bodyMail.='numero Prodotti Totali in Coda di Pubblicazione '.$countProduct;
-            /** @var CEmailRepo $mailRepo */
-            $mailRepo = \Monkey::app()->repoFactory->create('Email');
-            $mailRepo->newMail('it@iwes.it', ["gianluca@iwes.it","juri@iwes.it"],[],[],"coda Pubblicazione su ".$marketplaceAccount->name, $bodyMail);
         }
     }
 
