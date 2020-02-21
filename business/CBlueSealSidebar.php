7fb0fb6acbf2751cb084d11bb2089d2b92700c8e
juri fiorani <jurif@hotmail.com>
2020-02-21 15:50:59 +0100

    creazione Vista Vendite

--- business/CBlueSealSidebar.php
+++ business/CBlueSealSidebar.php
@@ -34,32 +34,57 @@ class CBlueSealSidebar
 
     public function build()
     {
-        $sidebar = $this->app->cacheService->getCache('misc')->get('BluesealSidebar:'.$this->app->getUser()->id);
-        if($sidebar == false) {
-            $groups = \Monkey::app()->repoFactory->create('SidebarGroup')->em()->findBySql("Select id 
-                                                                                    from SidebarGroup 
+        $sidebar = $this->app->cacheService->getCache('misc')->get('BluesealSidebar:' . $this->app->getUser()->id);
+        if ($sidebar == false) {
+
+            $sections = \Monkey::app()->repoFactory->create('SidebarSection')->em()->findBySql("Select id 
+                                                                                  from SidebarSection 
                                                                                     ORDER BY `order` asc");
             $sidebar = [];
-            foreach ($groups as $group) {
-                $sidebarPages = [];
-                foreach ($group->page as $page) {
-                    if($this->app->getUser()->hasPermissions($page->permission)) {
-                        $sidebarPages[$page->slug] = [
-                            'title' => $page->pageTranslation->getFirst()->title,
-                            'url' => $page->url,
-                            'icon' => $page->icon,
-                            'permission' => $page->permission,
-                            'postId' => $page->postId
-                        ];
+            $i=0;
+            foreach ($sections as $section) {
+
+                $sidebarGroups = [];
+                $sidebar[$i]['title'] = $section->sidebarSectionTranslation->getFirst()->title;
+                $sidebar[$i]['icon'] = $section->icon;
+                $groups = \Monkey::app()->repoFactory->create('SidebarGroup')->em()->findBySql("Select id
+                                                                                    from SidebarGroup where sidebarSectionId=" . $section->id . "
+                                                                               ORDER BY `order` asc");
+
+                foreach ($groups as $group) {
+
+                    $sidebarPages = [];
+                    foreach ($group->page as $page) {
+                        if ($this->app->getUser()->hasPermissions($page->permission)) {
+                            $sidebarPages[$page->slug] = [
+                                'title' => $page->pageTranslation->getFirst()->title,
+                                'url' => $page->url,
+                                'icon' => $page->icon,
+                                'permission' => $page->permission,
+                                'postId' => $page->postId
+                            ];
+                        }
+                        if (count($sidebarPages) > 0) {
+
+                            $sidebarGroups[$group->slug]['title'] = $group->sidebarGroupTranslation->getFirst()->title;
+                            $sidebarGroups[$group->slug]['icon'] = $group->icon;
+                            $sidebarGroups[$group->slug]['pages'] = $sidebarPages;
+
+                        }
+
+
                     }
+
+
                 }
-                if(count($sidebarPages) > 0) {
-                    $sidebar[$group->slug]['title'] = $group->sidebarGroupTranslation->getFirst()->title;
-                    $sidebar[$group->slug]['icon'] = $group->icon;
-                    $sidebar[$group->slug]['pages'] = $sidebarPages;
-                }
+
+
+                $sidebar[$i]['sidebarGroups'] = $sidebarGroups;
+
+        $i++;
             }
-            $this->app->cacheService->getCache('misc')->add('BluesealSidebar:'.$this->app->getUser()->id,$sidebar,10000);
+
+            $this->app->cacheService->getCache('misc')->add('BluesealSidebar:' . $this->app->getUser()->id,$sidebar,10000);
         }
         return $sidebar;
     }
--- template/parts/sidebar.php
+++ template/parts/sidebar.php
@@ -2,40 +2,58 @@
     <div id="appMenu" class="sidebar-overlay-slide from-top"></div>
     <div class="sidebar-header">
         <div class="sidebar-logo">
-            <img src="/assets/img/blueseal_logo_symbol.png" alt="logo" class="brand" data-src="/assets/img/blueseal_logo_symbol.png" data-src-retina="/assets/img/blueseal_logo_symbol.png" width="42">
-            <img class ="brand" width="120" src="/assets/img/logo_no_symbol_white.png" data-src-retina="/assets/img/logo_no_symbol_white.png" data-src="/assets/img/logo_no_symbol_white.png" />
+            <img src="/assets/img/blueseal_logo_symbol.png" alt="logo" class="brand"
+                 data-src="/assets/img/blueseal_logo_symbol.png" data-src-retina="/assets/img/blueseal_logo_symbol.png"
+                 width="42">
+            <img class="brand" width="120" src="/assets/img/logo_no_symbol_white.png"
+                 data-src-retina="/assets/img/logo_no_symbol_white.png"
+                 data-src="/assets/img/logo_no_symbol_white.png"/>
         </div>
     </div>
     <div class="sidebar-menu">
         <ul class="menu-items">
-            <?php $i=1; foreach($sidebar as $group => $content): ?>
-                <?php if(count($content['pages'])==1): ?>
-                    <?php foreach ($content['pages'] as $groupPage): ?>
-                        <li class="<?php echo ($i == 1) ? 'm-t-30' : ''; ?>">
-                            <a href="<?php echo $groupPage['url']; ?>" class="detailed">
-                                <span class="title"><?php echo $content['title']; ?></span>
-                            </a>
-                            <span class="icon-thumbnail "><i class="fa <?php echo $content['icon']; ?>"></i></span>
-                        </li>
-                    <?php endforeach; ?>
-                <?php else: ?>
-                    <li class="<?php echo (isset($content['pages'][$page->getSlug()])) ? "active open" : ""; ?> <?php echo ($i == 1) ? 'm-t-30' : ''; ?>">
-                        <a href="javascript:;">
-                            <span class="title"><?php echo $content['title']; ?></span>
-                            <span class="arrow <?php echo (isset($content['pages'][$page->getSlug()])) ? "open" : ""; ?>"></span>
-                        </a>
-                        <span class="icon-thumbnail"><i class="fa <?php echo $content['icon']; ?>"></i></span>
-                        <ul class="sub-menu">
-                            <?php foreach($content['pages'] as $slug => $groupPage): ?>
-                            <li class="<?php echo ($page->getSlug() == $slug) ? "active" : ""?>">
-                                <a href="<?php echo $groupPage['url']; ?>"><?php echo $groupPage['title']; ?></a>
-                                <span class="icon-thumbnail"><i class="fa <?php echo $groupPage['icon']; ?>"></i></span>
-                            </li>
+            <?php $c = 1;
+            foreach ($sidebar as $section => $groups): ?>
+                <li class="<?php echo (isset($groups['sidebarGroups'])) ? "active open" : ""; ?> <?php echo ($c == 1) ? 'm-t-30' : ''; ?>">
+                    <a href="javascript:;">
+                        <span class="title"><?php echo $groups['title']; ?></span>
+                        <span class="arrow <?php echo (isset($groups['sidebarGroups'])) ? "open" : ""; ?>"></span>
+                    </a>
+                    <span class="icon-thumbnail"><i class="fa <?php echo $groups['icon']; ?>"></i></span>
+                <ul class="sub-menu">
+                    <?php $i = 1;
+                    foreach ($groups['sidebarGroups'] as $group=>  $content): ?>
+                        <?php if (count($content['pages']) == 1): ?>
+                            <?php foreach ($content['pages'] as $groupPage): ?>
+                                <li class="<?php echo ($i == 1) ? 'm-t-30' : ''; ?>">
+                                    <a href="<?php echo $groupPage['url']; ?>" class="detailed">
+                                        <span class="title"><?php echo $content['title']; ?></span>
+                                    </a>
+                                    <span class="icon-thumbnail "><i
+                                                class="fa <?php echo $content['icon']; ?>"></i></span>
+                                </li>
                             <?php endforeach; ?>
-                        </ul>
-                    </li>
-                <?php endif; ?>
-            <?php $i++; endforeach; ?>
+                        <?php else: ?>
+                            <li class="<?php echo (isset($content['pages'][$page->getSlug()])) ? "active open" : ""; ?> <?php echo ($i == 1) ? 'm-t-30' : ''; ?>">
+                                <a href="javascript:;">
+                                    <span class="title"><?php echo $content['title']; ?></span>
+                                    <span class="arrow <?php echo (isset($content['pages'][$page->getSlug()])) ? "open" : ""; ?>"></span>
+                                </a>
+                                <span class="icon-thumbnail"><i class="fa <?php echo $content['icon']; ?>"></i></span>
+                                <ul class="sub-menu">
+                                    <?php foreach ($content['pages'] as $slug => $groupPage): ?>
+                                        <li class="<?php echo ($page->getSlug() == $slug) ? "active" : "" ?>">
+                                            <a href="<?php echo $groupPage['url']; ?>"><?php echo $groupPage['title']; ?></a>
+                                            <span class="icon-thumbnail"><i
+                                                        class="fa <?php echo $groupPage['icon']; ?>"></i></span>
+                                        </li>
+                                    <?php endforeach; ?>
+                                </ul>
+                            </li>
+                        <?php endif; ?>
+                        <?php $i++; endforeach; ?>
+                </ul>
+                <?php $c++; endforeach; ?>
         </ul>
         <div class="clearfix"></div>
     </div>
