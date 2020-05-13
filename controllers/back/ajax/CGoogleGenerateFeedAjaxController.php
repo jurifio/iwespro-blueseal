4c56ca94d6ab6952766845827375f8c4d6726607
juri fiorani <jurif@hotmail.com>
2020-05-13 21:39:31 +0200

    creazione Vista Vendite

--- controllers/back/ajax/CGoogleGenerateFeedAjaxController.php
+++ controllers/back/ajax/CGoogleGenerateFeedAjaxController.php
@@ -117,90 +117,92 @@ class CGoogleGenerateFeedAjaxController extends AAjaxController
         $writer = new \XMLWriter();
         $writer->openMemory();
         $writer->setIndent(!$this->minized);
-        $writer->startElement("item");
-        $writer->writeElement('g:id',$product->printId());
+        if ($product->qty>0) {
+            $writer->startElement("item");
+            $writer->writeElement('g:id',$product->printId());
 
-        $avai = 0;
-        $sizes = [];
-        $onSale = $product->isOnSale();
-        foreach ($product->productPublicSku as $sku) {
-            $sizes[] = $sku->productSize->name;
-            $avai++;
-        }
-
-        $writer->startElement('title');
-        $variant = ($product->productVariant) ? $product->productVariant->name : '';
-        $prodName = $product->getName();
-        $name = mb_strtoupper($product->productBrand->name) . ' ' . $variant . ' ' . $prodName;
-
-        if (count($sizes) < 3) $name .= " (" . implode('-',$sizes) . ")";
-        $writer->writeCdata($name);
-        $writer->endElement();
-
-
-        $writer->startElement('description');
-
-        $writer->writeCdata($product->getDescription());
-        $writer->endElement();
-
-
-        $product_type = [];
-        foreach ($product->productCategory as $category) {
-            $cats = $this->app->categoryManager->categories()->getPath($category->id);
-            $type = [];
-            foreach ($cats as $cat) {
-                if ($cat['id'] == 1) continue;
-                $type[] = \Monkey::app()->repoFactory->create('ProductCategory',$this->lang)->findOne([$cat['id']])->getLocalizedName();
+            $avai = 0;
+            $sizes = [];
+            $onSale = $product->isOnSale();
+            foreach ($product->productPublicSku as $sku) {
+                $sizes[] = $sku->productSize->name;
+                $avai++;
             }
-            $product_type[] = implode(', ',$type);
-        }
 
-        $writer->writeElement('g:product_type',implode('; ',$product_type));
+            $writer->startElement('title');
+            $variant = ($product->productVariant) ? $product->productVariant->name : '';
+            $prodName = $product->getName();
+            $name = mb_strtoupper($product->productBrand->name) . ' ' . $variant . ' ' . $prodName;
 
-        $categories = $product->getMarketplaceAccountCategoryIds($this->marketplaceAccount);
-        $writer->writeElement('g:google_product_category',$categories[0]);
+            if (count($sizes) < 3) $name .= " (" . implode('-',$sizes) . ")";
+            $writer->writeCdata($name);
+            $writer->endElement();
 
-        $baseUrlLang = $this->app->cfg()->fetch("paths","domain") . "/" . $this->lang->getLang();
-        $writer->writeElement('g:link','https://' . $product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
-        $writer->writeElement('g:mobile_link',$product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
-        $writer->writeElement('g:image_link',$this->helper->image($product->getPhoto(1,843),'amazon'));
-        for ($i = 2; $i < 8; $i++) {
-            $actual = $product->getPhoto($i,843);
-            if ($actual != false && !empty($actual)) {
-                $writer->writeElement('g:additional_image_link',$this->helper->image($actual,'amazon'));
+
+            $writer->startElement('description');
+
+            $writer->writeCdata($product->getDescription());
+            $writer->endElement();
+
+
+            $product_type = [];
+            foreach ($product->productCategory as $category) {
+                $cats = $this->app->categoryManager->categories()->getPath($category->id);
+                $type = [];
+                foreach ($cats as $cat) {
+                    if ($cat['id'] == 1) continue;
+                    $type[] = \Monkey::app()->repoFactory->create('ProductCategory',$this->lang)->findOne([$cat['id']])->getLocalizedName();
+                }
+                $product_type[] = implode(', ',$type);
             }
+
+            $writer->writeElement('g:product_type',implode('; ',$product_type));
+
+            $categories = $product->getMarketplaceAccountCategoryIds($this->marketplaceAccount);
+            $writer->writeElement('g:google_product_category',$categories[0]);
+
+            $baseUrlLang = $this->app->cfg()->fetch("paths","domain") . "/" . $this->lang->getLang();
+            $writer->writeElement('g:link','https://' . $product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
+            $writer->writeElement('g:mobile_link',$product->getProductUrl($baseUrlLang,$this->marketplaceAccount->getCampaignCode()));
+            $writer->writeElement('g:image_link',$this->helper->image($product->getPhoto(1,843),'amazon'));
+            for ($i = 2; $i < 8; $i++) {
+                $actual = $product->getPhoto($i,843);
+                if ($actual != false && !empty($actual)) {
+                    $writer->writeElement('g:additional_image_link',$this->helper->image($actual,'amazon'));
+                }
+            }
+            $writer->writeElement('g:condition','new');
+
+
+            $writer->writeElement('g:availability',$avai > 0 ? 'in stock' : 'out of stock');
+            $writer->writeElement('sizes',implode(';',$sizes));
+            $writer->writeElement('g:size',implode(';',$sizes));
+            $priceActive = \Monkey::app()->repoFactory->create('ProductSku')->findOneBy(['productId' => $product->id,'productVariantId' => $product->productVariantId]);
+
+            $price = number_format($priceActive->price,2,'.','');
+
+            $salePrice = number_format($priceActive->salePrice,2,'.','');
+
+            $writer->writeElement('g:price',$price . ' EUR');
+            if ($product->isOnSale == 1) {
+                $writer->writeElement('g:sale_price',$salePrice . ' EUR');
+            }
+            $writer->writeElement('g:mpn',$product->itemno . ' ' . $product->productVariant->name);
+            $writer->writeElement('g:brand',$product->productBrand->name);
+            if (!is_null($product->productColorGroup)) {
+                $writer->writeElement('g:color',$product->productColorGroup->productColorGroupTranslation->getFirst()->name);
+            }
+            $writer->startElement('g:shipping');
+            $writer->writeElement('g:service','Courier');
+            $writer->writeElement('g:price','10.00 EUR');
+            $writer->endElement();
+            $writer->startElement('g:shipping');
+            $writer->writeElement('g:country','IT');
+            $writer->writeElement('g:service','Courier');
+            $writer->writeElement('g:price','5.00 EUR');
+            $writer->endElement();
+            $writer->endElement();
         }
-        $writer->writeElement('g:condition','new');
-
-
-        $writer->writeElement('g:availability',$avai > 0 ? 'in stock' : 'out of stock');
-        $writer->writeElement('sizes',implode(';',$sizes));
-        $writer->writeElement('g:size',implode(';',$sizes));
-        $priceActive=\Monkey::app()->repoFactory->create('ProductSku')->findOneBy(['productId'=>$product->id,'productVariantId'=>$product->productVariantId]);
-
-        $price=number_format($priceActive->price,2,'.','');
-
-        $salePrice=number_format($priceActive->salePrice,2,'.','');
-
-        $writer->writeElement('g:price', $price . ' EUR');
-        if ($product->isOnSale==1) {
-            $writer->writeElement('g:sale_price', $salePrice . ' EUR');
-        }
-        $writer->writeElement('g:mpn',$product->itemno . ' ' . $product->productVariant->name);
-        $writer->writeElement('g:brand',$product->productBrand->name);
-        if (!is_null($product->productColorGroup)) {
-            $writer->writeElement('g:color',$product->productColorGroup->productColorGroupTranslation->getFirst()->name);
-        }
-        $writer->startElement('g:shipping');
-        $writer->writeElement('g:service','Courier');
-        $writer->writeElement('g:price','10.00 EUR');
-        $writer->endElement();
-        $writer->startElement('g:shipping');
-        $writer->writeElement('g:country','IT');
-        $writer->writeElement('g:service','Courier');
-        $writer->writeElement('g:price','5.00 EUR');
-        $writer->endElement();
-        $writer->endElement();
         return $writer->outputMemory();
     }
    public function fetchProductsCodeMinusDeleted() {
