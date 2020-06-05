adfbb4d926b29d0bc4c73f6daaf8599df08e426c
juri fiorani <jurif@hotmail.com>
2020-06-05 22:07:09 +0200

    creazione Vista Vendite

--- template/widgets/videos.php
+++ template/widgets/videos.php
@@ -1,10 +1,9 @@
-<?php foreach ($photos as $photo): ?>
-<div id="photo<?php echo $photo['order'] ?>" style="" class="draggable col-sm-2">
+
+<div id="photo1" style="" class="draggable col-sm-2">
     <div class="row">
         <video width="150" height="240"  controls autoplay>
-            <source src="https://cdn.iwes.it/test/VID_20200507_171039.mp4" type="video/mp4">
-            <source src="https://www.iwes.pro/test/VID_20200507_171039.mp4" type="video/ogg">
-
+            <source src="https://www.iwes.pro/test/VID_20200507_171039.mp4" type="video/mp4">
+        </video>
     </div>
     </div>
     <div class="row">
@@ -14,4 +13,3 @@
         <div class="pf-field"></div>
     </div>
 </div>
-<?php endforeach; ?>
