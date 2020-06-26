551b8b091fd87509e4e52a411be0ff3e96ce3ad9
juri fiorani <jurif@hotmail.com>
2020-06-26 18:49:35 +0200

    creazione Vista Vendite

--- controllers/back/ajax/CBillRegistryActivePaymentSlipListAjaxController.php
+++ controllers/back/ajax/CBillRegistryActivePaymentSlipListAjaxController.php
@@ -125,8 +125,11 @@ class CBillRegistryActivePaymentSlipListAjaxController extends AAjaxController
                 $row['impInizialePassive']= ($pb->amount!=null) ?
                     number_format($pb->amount,'2',',','.') . ' &euro;'
                     :'';
+                $sqlDiff='SELECT ((SELECT SUM(p.amount) FROM PaymentBill p WHERE p.id='.$paymentBill->paymentBillId.')-
+(SELECT SUM(p.amount) FROM BillRegistryActivePaymentSlip p WHERE p.paymentBillId='.$paymentBill->paymentBillId.' and  p.id='.$pb->id.')) AS difference';
+                $difference=\Monkey::app()->dbAdapter->query($sqlDiff, [])->fetchAll()[0]['difference'] + 1;
                 $row['impPassive']= ($pb->amount!=null) ?
-                    number_format($pb->amount,'2',',','.') . ' &euro;'
+                    number_format($difference,'2',',','.') . ' &euro;'
                :'';
                 $row['impSaldoPassive'] = ($pb->amount != null) ? number_format($pb->amount-$paymentBill->amount,'2',',','.') . ' &euro;' : '';
                 $row['negativeAmount'] = ($pb->amount != null) ? number_format($paymentBill->amount - $pb->amount,'2',',','.') . ' &euro;' : '';
