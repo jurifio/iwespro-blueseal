525477905388da4203217acfa1a67943f171855e
juri fiorani <jurif@hotmail.com>
2020-07-25 17:26:20 +0200

    creazione Vista Vendite

--- jobs/CImportExternalPickySiteOrderJob.php
+++ jobs/CImportExternalPickySiteOrderJob.php
@@ -445,7 +445,7 @@ class CImportExternalPickySiteOrderJob extends ACronJob
                                                c.creationDate as creationDate,
                                                c.isParallel as isParallel,
                                                c.isImport as isImport
-                                               from Cart c join User U on c.userId = U.id WHERE isParallel is null  AND c.isImport=0  order BY remoteCartSellerId ASC    ');
+                                               from Cart c left join `User` U on c.userId = U.id WHERE isParallel is null  AND c.isImport=0  order BY remoteCartSellerId ASC    ');
                 $stmtCart -> execute();
                 foreach ($stmtCart as $rowCart) {
                     //hile ($rowCart = $stmtCart->fetch(PDO::FETCH_ASSOC)) {
@@ -486,7 +486,7 @@ class CImportExternalPickySiteOrderJob extends ACronJob
             }
 
             /***** inserimento righe carrello *********/
-            try {
+       /*     try {
                 $stmtCartLine = $db_con -> prepare('SELECT
                                             cl.id as remoteCartLineSellerId,
                                             cl.cartId as remoteCartId,
@@ -527,7 +527,7 @@ class CImportExternalPickySiteOrderJob extends ACronJob
                 $stmtCartLineUpdate->execute();
             } catch (\throwable $e) {
                 $this -> report('CImportExternalPickySiteOrderJob', 'error', 'Errore CartLine ' . $e);
-            }
+            }*/
 
             try {
                 /***inserimento ordini */
