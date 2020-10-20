9a289b9d2df8cee7b546f7c8386c847c2dbefcd9
juri fiorani <jurif@hotmail.com>
2020-10-20 11:08:08 +0200

    creazione Vista Vendite

--- template/product_image_operation.php
+++ template/product_image_operation.php
@@ -38,10 +38,10 @@
                     <div class="row">
                         <div class="col-lg-12 h2">
                            Benvenuti nella sezione di riposizionamento foto<br><p>
-                                Per poter riposizionare le foto bisogna inserire nella path del nas <b>shootImport/resize</b><br>
+                                Per poter riposizionare le foto bisogna inserire nella path del nas <b>\\192.168.1.155\shootImport\resize</b><br>
                                 una volta posizionata la cartella cliccare sul tasto Remasterizzazinoe Foto <span class="fa fa-rocket"></span><br>
-                                attendere la fine dell'elaborazione e il risultato dell'elaborazione si troverà nella cartella <b>/shootImport/newage2/topublish</b><br>
-                                le  originali verrano inserite in <b>/shootImport/newage2/original</b><br>
+                                attendere la fine dell'elaborazione e il risultato dell'elaborazione si troverà nella cartella <b>\\192.168.1.155\shootImport\newage2\topublish</b><br>
+                                le  originali verrano inserite in <b>\\192.168.1.155\shootImport\newage2\original</b><br>
 
                         </div>
                     </div>
@@ -68,4 +68,4 @@
         </bs-toolbar-group>
     </bs-toolbar>
 </body>
-</html><?php
+</html>
