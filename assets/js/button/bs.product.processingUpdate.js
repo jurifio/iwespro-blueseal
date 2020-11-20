703feb367ec6a9571f6f92fa0f3efb9d67085a43
juri fiorani <jurif@hotmail.com>
2020-11-20 17:51:54 +0100

    creazione Vista Vendite

--- assets/js/button/bs.product.processingUpdate.js
+++ assets/js/button/bs.product.processingUpdate.js
@@ -1,7 +1,7 @@
 window.buttonSetup = {
     tag:"a",
     icon:"fa-check-square-o",
-    permission:"/admin/product/edit||/admin/product/edit||worker",
+    permission:"/admin/product/list||worker",
     event:"bs-product-processingUpdate",
     class:"btn btn-default",
     rel:"tooltip",
