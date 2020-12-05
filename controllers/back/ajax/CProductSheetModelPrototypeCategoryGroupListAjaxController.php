6a29a8d799438e90b489e7369f46204833a347cb
juri fiorani <jurif@hotmail.com>
2020-12-05 17:13:46 +0100

    creazione Vista Vendite

--- controllers/back/ajax/CProductSheetModelPrototypeCategoryGroupListAjaxController.php
+++ controllers/back/ajax/CProductSheetModelPrototypeCategoryGroupListAjaxController.php
@@ -49,7 +49,7 @@ class CProductSheetModelPrototypeCategoryGroupListAjaxController extends AAjaxCo
               catG.id,
               catG.name,
               catG.description,
-              if((count(pmp.id) = 0), 'v', count(pmp.id)) as models,
+              if((count(pmp.id) = 0), 'vuoto', count(pmp.id)) as models,
               if((isnull(catG.imageUrl)), 'no', 'sì') as image,
               catMacroG.id as macroId,
               catMacroG.name as macroName
@@ -74,7 +74,7 @@ class CProductSheetModelPrototypeCategoryGroupListAjaxController extends AAjaxCo
             $row['id'] = $cat->id;
             $row['name'] = $cat->name;
             $row['description'] = $cat->description;
-            $row['models'] = ($cat->productSheetModelPrototype->count()==0)? 'Vuoto': $cat->productSheetModelPrototype->count();
+            $row['models'] = ($cat->productSheetModelPrototype->count()==0)? 'vuoto': $cat->productSheetModelPrototype->count();
             $row['image'] = '<a href="#1" class="enlarge-your-img"><img width="50" src="' . $cat->imageUrl . '" /></a>';
             $row['macroName'] = (is_null($cat->macroCategoryGroupId) ? '-' : $cat->productSheetModelPrototypeMacroCategoryGroup->name);
 
