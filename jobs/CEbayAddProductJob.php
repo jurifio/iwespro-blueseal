3845af053fbb630ec9facb57e111a294b2cb42bd
juri fiorani <jurif@hotmail.com>
2020-12-30 17:42:48 +0100

    creazione Vista Vendite

--- jobs/CEbayAddProductJob.php
+++ jobs/CEbayAddProductJob.php
@@ -163,11 +163,12 @@ class CEbayAddProductJob extends ACronJob
 
                             $shopHasProduct=\Monkey::app()->repoFactory->create('ShopHasProduct')->findOneBy(['productId' => $reservedId['productId'],'productVariantId' => $reservedId['productVariantId']]);
                            if ($shopHasProduct->shopId!=1 || $shopHasProduct->shopId!=51 || $shopHasProduct->shopId!=58) {
-                               $findProductToWork = \Monkey::app()->repoFactory->create('PrestashopHasProductHasMarketplaceHasShop')->findOneBy(['productId' => $reservedId['productId'],'productVariantId' => $reservedId['productVariantId'],'marketplaceHasShopId' => $market['marketplaceId']]);
-                               if ($findProductToWork!=null) {
-                                   if ($findProductToWork->isPublished == 1) {
+                               try {
+                                   $findProductToWork = \Monkey::app()->repoFactory->create('PrestashopHasProductHasMarketplaceHasShop')->findOneBy(['productId' => $reservedId['productId'],'productVariantId' => $reservedId['productVariantId'],'marketplaceHasShopId' => $market['marketplaceId']]);
+                                   if ($findProductToWork != null) {
+                                       if ($findProductToWork->isPublished == 1) {
 
-                                       $request = '<?xml version="1.0" encoding="utf-8"?>
+                                           $request = '<?xml version="1.0" encoding="utf-8"?>
 <EndItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">
  <!-- Call-specific Input Fields -->
  <EndingReason>NotAvailable</EndingReason>
@@ -178,65 +179,68 @@ class CEbayAddProductJob extends ACronJob
  <Version>741</Version>
  <WarningLevel>High</WarningLevel>
 </EndItemRequest>';
-                                       $devID = '9c29584f-1f9e-4c60-94dc-84f786d8670e';
-                                       $appID = 'VendiloS-c310-4f4c-88a9-27362c05ea78';
-                                       $certID = '3050bb00-db24-4842-999c-b943deb09d1a';
-                                       $siteID = 101;
+                                           $devID = '9c29584f-1f9e-4c60-94dc-84f786d8670e';
+                                           $appID = 'VendiloS-c310-4f4c-88a9-27362c05ea78';
+                                           $certID = '3050bb00-db24-4842-999c-b943deb09d1a';
+                                           $siteID = 101;
 
-                                       $apiUrl = 'https://api.ebay.com/ws/api.dll';
-                                       $apiCall = 'EndItemRequest';
-                                       $compatibilityLevel = 741;
+                                           $apiUrl = 'https://api.ebay.com/ws/api.dll';
+                                           $apiCall = 'EndItemRequest';
+                                           $compatibilityLevel = 741;
 
-                                       $runame = 'Vendilo_SpA-VendiloS-c310-4-prlqnbrjv';
-                                       $loginURL = 'https://signin.ebay.it/ws/eBayISAPI.dll';
+                                           $runame = 'Vendilo_SpA-VendiloS-c310-4-prlqnbrjv';
+                                           $loginURL = 'https://signin.ebay.it/ws/eBayISAPI.dll';
 
-                                       $headers = array(
-                                           // Regulates versioning of the XML interface for the API
-                                           'X-EBAY-API-COMPATIBILITY-LEVEL: ' . $compatibilityLevel,
-                                           // Set the keys
-                                           'X-EBAY-API-DEV-NAME: ' . $devID,
-                                           'X-EBAY-API-APP-NAME: ' . $appID,
-                                           'X-EBAY-API-CERT-NAME: ' . $certID,
-                                           // The name of the call we are requesting
-                                           'X-EBAY-API-CALL-NAME: ' . $apiCall,
-                                           // SiteID must also be set in the Request's XML
-                                           // SiteID = 0 (US) - UK = 3, Canada = 2, Australia = 15, ....
-                                           // SiteID Indicates the eBay site to associate the call with
-                                           'X-EBAY-API-SITEID: ' . $siteID
-                                       );
-                                       $connection = curl_init();
-                                       curl_setopt($connection,CURLOPT_URL,$apiUrl);
+                                           $headers = array(
+                                               // Regulates versioning of the XML interface for the API
+                                               'X-EBAY-API-COMPATIBILITY-LEVEL: ' . $compatibilityLevel,
+                                               // Set the keys
+                                               'X-EBAY-API-DEV-NAME: ' . $devID,
+                                               'X-EBAY-API-APP-NAME: ' . $appID,
+                                               'X-EBAY-API-CERT-NAME: ' . $certID,
+                                               // The name of the call we are requesting
+                                               'X-EBAY-API-CALL-NAME: ' . $apiCall,
+                                               // SiteID must also be set in the Request's XML
+                                               // SiteID = 0 (US) - UK = 3, Canada = 2, Australia = 15, ....
+                                               // SiteID Indicates the eBay site to associate the call with
+                                               'X-EBAY-API-SITEID: ' . $siteID
+                                           );
+                                           $connection = curl_init();
+                                           curl_setopt($connection,CURLOPT_URL,$apiUrl);
 
-                                       curl_setopt($connection,CURLINFO_HEADER_OUT,true);
+                                           curl_setopt($connection,CURLINFO_HEADER_OUT,true);
 // Stop CURL from verifying the peer's certificate
-                                       curl_setopt($connection,CURLOPT_SSL_VERIFYPEER,0);
-                                       curl_setopt($connection,CURLOPT_SSL_VERIFYHOST,0);
+                                           curl_setopt($connection,CURLOPT_SSL_VERIFYPEER,0);
+                                           curl_setopt($connection,CURLOPT_SSL_VERIFYHOST,0);
 
 // Set the headers (Different headers depending on the api call !)
 
-                                       curl_setopt($connection,CURLOPT_HTTPHEADER,$headers);
+                                           curl_setopt($connection,CURLOPT_HTTPHEADER,$headers);
 
-                                       curl_setopt($connection,CURLOPT_POST,1);
+                                           curl_setopt($connection,CURLOPT_POST,1);
 
 // Set the XML body of the request
-                                       curl_setopt($connection,CURLOPT_POSTFIELDS,$request);
+                                           curl_setopt($connection,CURLOPT_POSTFIELDS,$request);
 
 // Set it to return the transfer as a string from curl_exec
-                                       curl_setopt($connection,CURLOPT_RETURNTRANSFER,1);
+                                           curl_setopt($connection,CURLOPT_RETURNTRANSFER,1);
 
 // Send the Request
-                                       $response = curl_exec($connection);
+                                           $response = curl_exec($connection);
 
 
-                                       $closeProduct = new \SimpleXMLElement($response);
-                                       $findProductToWork->isPublished = 0;
-                                       $findProductToWork->lasUpdate = $product->lastUpdate;
-                                       $findProductToWork->update();
-                                       $this->report('CEbayReviseProductJob','Report  Revise  Close Product' . $findProductToWork->productId . '-' . $findProductToWork->productVarinatId . '-Ref: ' . $findProductToWork->refMarketplaceId);
+                                           $closeProduct = new \SimpleXMLElement($response);
+                                           $findProductToWork->isPublished = 0;
+                                           $findProductToWork->lasUpdate = $product->lastUpdate;
+                                           $findProductToWork->update();
+                                           $this->report('CEbayReviseProductJob','Report  Revise  Close Product' . $findProductToWork->productId . '-' . $findProductToWork->productVarinatId . '-Ref: ' . $findProductToWork->refMarketplaceId);
+
+                                       }
+
 
                                    }
-
-
+                               }catch(\Throwable $e){
+                                   $this->report('CEbayReviseProductJob','Error' , $e);
                                }
                                continue;
                            }
