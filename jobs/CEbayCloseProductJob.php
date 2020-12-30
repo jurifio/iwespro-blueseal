f6814fe065405fc9ee4879bfc5c75cbafde3b7a0
juri fiorani <jurif@hotmail.com>
2020-12-30 21:12:46 +0100

    creazione Vista Vendite

--- jobs/CEbayCloseProductJob.php
+++ jobs/CEbayCloseProductJob.php
@@ -38,41 +38,28 @@ class CEbayCloseProductJob extends ACronJob
             $phphmhsRepo = \Monkey::app()->repoFactory->create('PrestashopHasProductHasMarketplaceHasShop');
             $marketplaceAccounts = \Monkey::app()->repoFactory->create('MarketplaceAccount')->findBy(['marketplaceId' => 3,'isActive' => 1]);
             foreach ($marketplaceAccounts as $marketplaceAccount) {
-                $phphmhs = $phphmhsRepo->findBy(['isPublished'=>1]);
+                $phphmhs = $phphmhsRepo->findBy(['isPublished' => 1]);
                 foreach ($phphmhs as $productInMarketplace) {
                     $product = $productRepo->findOneBy(['id' => $productInMarketplace->productId,'productVariantId' => $productInMarketplace->productVariantId,'marketplaceHasShopId' => $marketplaceAccount->config['marketplaceHasShopId']]);
                     if ($product->qty == 0) {
                         $productInMarketplace->isPublished = 0;
                         $productInMarketplace->update();
                         $request = '<?xml version="1.0" encoding="utf-8"?>';
-                        $request .='<EndItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
-  $request .='<RequesterCredentials>';
-    $request .='<eBayAuthToken>v^1.1#i^1#r^1#p^3#I^3#f^0#t^Ul4xMF8xOjk3MDVGODY4MkI3QUI4QkZGNzlGRTAwMjQwMjk4NkI4XzBfMSNFXjI2MA==</eBayAuthToken>';
-  $request .='</RequesterCredentials>';
-                        $request .='<ItemID>' . $productInMarketplace->refMarketplaceId . '</ItemID>';
-                        $request .='<EndingReason>NotAvailable</EndingReason>';
-                        $request .='<ErrorLanguage>it_IT</ErrorLanguage>';
-
-                        $request .='</EndItemRequest>';
-                        $request = preg_replace(
-                            '/[\x00-\x08\x10\x0B\x0C\x0E-\x19\x7F]'
-                            . '|[\x00-\x7F][\x80-\xBF]+'
-                            . '|([\xC0\xC1]|[\xF0-\xFF])[\x80-\xBF]*'
-                            . '|[\xC2-\xDF]((?![\x80-\xBF])|[\x80-\xBF]{2,})'
-                            . '|[\xE0-\xEF](([\x80-\xBF](?![\x80-\xBF]))|(?![\x80-\xBF]{2})|[\x80-\xBF]{3,})/S',
-                            '?',
-                            $request
-                        );
-
-                        $request = preg_replace('/\xE0[\x80-\x9F][\x80-\xBF]' . '|\xED[\xA0-\xBF][\x80-\xBF]/S','?',$request);
+                        $request .='<EndFixedPriceItemRequest xmlns="urn:ebay:apis:eBLBaseComponents">';
+                        $request .='<RequesterCredentials>';
+                        $request .='<eBayAuthToken>v^1.1#i^1#r^1#p^3#I^3#f^0#t^Ul4xMF8xOjk3MDVGODY4MkI3QUI4QkZGNzlGRTAwMjQwMjk4NkI4XzBfMSNFXjI2MA==</eBayAuthToken>';
+                        $request .='</RequesterCredentials><ErrorLanguage>it_IT</ErrorLanguage><WarningLevel>High</WarningLevel>';
+                        $request .= '<ItemID>' . $productInMarketplace->refMarketplaceId . '</ItemID>';
+                        $request .='<EndingReason>NotAvailable</EndingReason><WarningLevel>High</WarningLevel><Version>1149</Version>';
+                        $request .='</EndFixedPriceItemRequest>';
                         $devID = '9c29584f-1f9e-4c60-94dc-84f786d8670e';
                         $appID = 'VendiloS-c310-4f4c-88a9-27362c05ea78';
                         $certID = '3050bb00-db24-4842-999c-b943deb09d1a';
                         $siteID = 101;
 
                         $apiUrl = 'https://api.ebay.com/ws/api.dll';
-                        $apiCall = 'EndItemRequest';
-                        $compatibilityLevel = 741;
+                        $apiCall = 'EndFixedPriceItem';
+                        $compatibilityLevel = 1149;
 
                         $runame = 'Vendilo_SpA-VendiloS-c310-4-prlqnbrjv';
                         $loginURL = 'https://signin.ebay.it/ws/eBayISAPI.dll';
@@ -116,18 +103,18 @@ class CEbayCloseProductJob extends ACronJob
                             $response = curl_exec($connection);
                             $this->report('CEbayCloseProductJob','Report ',$response);
 
-                        }catch(\Throwable $e){
-                            $this->report('CEbayCloseProductJob','error call','Depublish '.$productInMarketplace->productId.'-'.$productInMarketplace->productVariantId.'-'.$productInMarketplace->refMarketplaceId);
+                        } catch (\Throwable $e) {
+                            $this->report('CEbayCloseProductJob','error call','Depublish ' . $productInMarketplace->productId . '-' . $productInMarketplace->productVariantId . '-' . $productInMarketplace->refMarketplaceId);
                         }
 
 
-                        $this->report('CEbayCloseProductJob','Success','Depublish '.$productInMarketplace->productId.'-'.$productInMarketplace->productVariantId.'-'.$productInMarketplace->refMarketplaceId);
+                        $this->report('CEbayCloseProductJob','Success','Depublish ' . $productInMarketplace->productId . '-' . $productInMarketplace->productVariantId . '-' . $productInMarketplace->refMarketplaceId);
                     }
                 }
             }
 
-        }catch(\Throwable $e) {
-            $this->report('CEbayCloseProductJob','ERROR',$e->getMessage().'-'.$e->getLine());
+        } catch (\Throwable $e) {
+            $this->report('CEbayCloseProductJob','ERROR',$e->getMessage() . '-' . $e->getLine());
 
         }
         $this->report('CEbayCloseProductJob','End','');
