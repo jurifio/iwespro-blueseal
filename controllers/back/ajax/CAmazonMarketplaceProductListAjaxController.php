d9ce3e73a159530b65773bf35584829974b7f94c
juri fiorani <jurif@hotmail.com>
2021-01-09 18:02:47 +0100

    creazione Vista Vendite

---
+++ controllers/back/ajax/CAmazonMarketplaceProductListAjaxController.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace bamboo\controllers\back\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\db\pandaorm\repositories\CRepo;
+use bamboo\domain\entities\CMarketplaceHasShop;
+use bamboo\domain\entities\CPrestashopHasProduct;
+use bamboo\domain\entities\CPrestashopHasProductHasMarketplaceHasShop;
+use bamboo\domain\repositories\CPrestashopHasProductRepo;
+
+/**
+ * Class CEbayMarketplaceProductListAjaxController
+ * @package bamboo\controllers\back\ajax
+ *
+ * @author Iwes Team <it@iwes.it>
+ *
+ * @copyright (c) Iwes  snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 14/03/2019
+ * @since 1.0
+ */
+class CEbayMarketplaceProductListAjaxController extends AAjaxController
+{
+    public function get()
+    {
+        $sql = "
+            SELECT
+              concat(php.productId, '-', php.productVariantId) AS productCode,
+              php.productId,
+              php.productVariantId,
+              pps.price,
+              pb.name  as `brand`, 
+              p.externalId AS externalId,
+                   
+
+              group_concat(concat(phphmhs.refMarketplaceId,' | ',s.name, ' | ', m.name, ' | Price: ', phphmhs.price,' | Sale price: ', phphmhs.salePrice,' | Sale: ', phphmhs.isOnSale, ' | Titolo modificato: ', phphmhs.titleModified,' | Aggiornamento: ',if(phphmhs.result=1, concat('Eseguito ',phphmhs.lastOperation),concat('Fallito ',phphmhs.lastOperation))  )) AS marketplaceAssociation,
+              p.isOnSale AS pickySale,
+              p.qty as totalQty,
+              PS.name as productStatus,     
+              group_concat(concat(s.name, ' | ', m.name, ' | Sale: ', phphmhs.isOnSale, ' | Titolo modificato: ', phphmhs.titleModified)) AS sale,
+              group_concat(concat(s.name, ' | ', m.name, ' | Sale price: ', phphmhs.salePrice)) AS salePrice,
+              php.status,
+              php.prestaId,
+              psm.`name` as productStatusMarketplaceId,     
+              phphmhs.refMarketplaceId as refMarketplaceId,
+              concat(s2.name, ' | ', m2.name) AS cronjobReservation,
+              concat('Type operation: ', php.modifyType, ' | Operation amount: ', php.variantValue) AS cronjobOperation,
+              if((isnull(p.dummyPicture) OR (p.dummyPicture = 'bs-dummy-16-9.png')), 'no', 'sì')            AS dummy,
+               concat(shop.id, '-', shop.name)                                                                     AS shop,
+               concat(pse.name, ' ', pse.year)                                                               AS season,
+               psiz.name                                                                                             AS stock
+            FROM PrestashopHasProduct php
+                join ProductStatusMarketplace psm on php.productStatusMarketplaceId=psm.id
+            JOIN ProductPublicSku pps ON pps.productId = php.productId AND pps.productVariantId = php.productVariantId
+            left JOIN Product p ON php.productId = p.id AND php.productVariantId = p.productVariantId    
+            LEFT JOIN ProductStatus PS on p.productStatusId=PS.id       
+            LEFT JOIN ProductBrand pb on p.productBrandId=pb.id
+            LEFT JOIN PrestashopHasProductHasMarketplaceHasShop phphmhs ON php.productId = phphmhs.productId AND php.productVariantId = phphmhs.productVariantId
+            LEFT JOIN MarketplaceHasShop mhs ON mhs.id = phphmhs.marketplaceHasShopId
+            LEFT JOIN Shop s ON mhs.shopId = s.id
+            LEFT JOIN Marketplace m ON mhs.marketplaceId = m.id
+            LEFT JOIN MarketplaceHasShop mhs2 ON php.marketplaceHasShopId = mhs2.id
+            LEFT JOIN Shop s2 ON mhs2.shopId = s2.id
+            LEFT JOIN Marketplace m2 ON mhs2.marketplaceId = m2.id
+            JOIN ShopHasProduct sp
+                    ON (p.id, p.productVariantId) = (sp.productId, sp.productVariantId)
+                  JOIN Shop shop ON shop.id = sp.shopId
+                   JOIN ProductSeason pse ON p.productSeasonId = pse.id
+                    
+                   LEFT JOIN (ProductSku psk
+                    JOIN ProductSize psiz ON psk.productSizeId = psiz.id)
+                    ON (p.id, p.productVariantId) = (psk.productId, psk.productVariantId)
+            where p.qty>0 and m.id=4
+            GROUP BY php.productId, php.productVariantId 
+        ";
+
+
+        $datatable = new CDataTables($sql, ['productId', 'productVariantId'], $_GET, true);
+
+        $datatable->doAllTheThings();
+
+        /** @var CPrestashopHasProductRepo $phpRepo */
+        $phpRepo = \Monkey::app()->repoFactory->create('PrestashopHasProduct');
+
+        /** @var CRepo $mhsRepo */
+        $mhsRepo = \Monkey::app()->repoFactory->create('MarketplaceHasShop');
+        $productStatusMarketplaceRepo=\Monkey::app()->repoFactory->create('ProductStatusMarketplace');
+        foreach ($datatable->getResponseSetData() as $key => $row) {
+
+            /** @var CPrestashopHasProduct $php */
+            $php = $phpRepo->findOneBy($row);
+
+            $row['productCode'] = $php->productId . '-' . $php->productVariantId;
+
+            $associations = '';
+            $onSale = '';
+            $salePrice = '';
+            $refMarketplaceId='';
+
+
+
+
+          /** @var CPrestashopHasProductHasMarketplaceHasShop $pHPHmHs */
+            foreach ($php->prestashopHasProductHasMarketplaceHasShop as $pHPHmHs) {
+                $associations .= $pHPHmHs->refMarketplaceId. ' | '.$pHPHmHs->marketplaceHasShop->shop->name . ' | ' . $pHPHmHs->marketplaceHasShop->marketplace->name . ' |<br> Price: ' . $pHPHmHs->price . ' ( ' . $pHPHmHs->salePrice . ' ) | Saldo: ' . ($pHPHmHs->isOnSale == 0 ? 'No' : 'Si') . ' |<br> Titolo modificato: ' . ($pHPHmHs->titleModified == 0 ? 'No' : 'Yes') . '<br>'. ' |<br> Aggiornamento: ' . ($pHPHmHs->result == 1 ? 'Eseguito '.$pHPHmHs->lastOperation : 'Fallito '.$pHPHmHs->lastOperation) . '<br><hr>';
+
+
+            }
+            $row['marketplaceAssociation'] = $associations;
+
+
+
+
+            switch ($php->status) {
+                case 1:
+                    $row['status'] = CPrestashopHasProduct::UPDATED;
+                    break;
+                case 2:
+                    $row['status'] = CPrestashopHasProduct::TOUPDATE;
+                    break;
+                default:
+                    $row['status'] = '';
+                    break;
+            }
+            $row['brand']=$php->product->productBrand->name;
+            $row['productStatus']=$php->product->productStatus->name;
+            $isOnSale=$php->product->isOnSale == 0 ? ' Saldo No' : ' Saldo Si';
+            $row['price'] = $php->product->getDisplayPrice() . ' (' . $php->product->getDisplaySalePrice() . ')<br>' .$isOnSale ;
+            $row['prestaId'] = $php->prestaId;
+            $productStatusMarketplace=$productStatusMarketplaceRepo->findOneBy(['id'=>$php->productStatusMarketplaceId]);
+            if($productStatusMarketplace) {
+                $row['productStatusMarketplaceId'] = $productStatusMarketplace->name;
+            }else{
+                $row['productStatusMarketplaceId'] = '';
+            }
+            $row['dummy'] = '<a href="#1" class="enlarge-your-img"><img width="50" src="' . $php->product->getDummyPictureUrl() . '" /></a>';
+            $row['shop'] = '<span class="small">' . $php->product->getShops('<br />', true) . '</span>';
+            $row['season'] = '<span class="small">' . $php->product->productSeason->name . " " . $php->product->productSeason->year .  '</span>';
+            $row['totalQty'] = '<span class="small">' .$php->product->qty.'</span>';
+            $row['stock'] = '<table class="nested-table inner-size-table" data-product-id="'.$php->product->printId().'"></table>';
+            $row['externalId']='<span class="small">' . $php->product->itemno .  '</span>';
+
+
+            /** @var CMarketplaceHasShop $mhsCron */
+            $mhsCron = $mhsRepo->findOneBy(['id' => $php->marketplaceHasShopId]);
+
+            $row['cronjobOperation'] = '';
+            $row['cronjobReservation'] = '';
+
+            if (!is_null($mhsCron)) {
+                $row['cronjobReservation'] = $mhsCron->shop->name . ' | ' . $mhsCron->marketplace->name;
+                $row['cronjobOperation'] = 'Type operation: ' . $php->modifyType . ' | Operation amount: ' . $php->variantValue;
+            }
+
+            $datatable->setResponseDataSetRow($key, $row);
+        }
+
+        return $datatable->responseOut();
+    }
+
+}
---
+++ template/amazon_marketplace_product.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include "parts/head.php" ?>
+    <?php echo $app->getAssets(['ui', 'forms', 'tables'], $page); ?>
+    <title>BlueSeal - <?php echo $page->getTitle(); ?></title>
+</head>
+<body class="fixed-header">
+<?php include "parts/sidebar.php"; ?>
+<div class="page-container">
+    <?php include "parts/header.php"; ?>
+    <?php include "parts/operations.php" ?>
+
+    <div class="page-content-wrapper">
+        <div class="content sm-gutter">
+
+            <div class="container-fluid container-fixed-lg bg-white">
+                <div class="panel panel-transparent">
+                    <div class="panel-body">
+                        <table class="table table-striped responsive" width="100%"
+                               data-datatable-name="amazon_marketplace_product"
+                               data-controller="AmazonMarketplaceProductListAjaxController"
+                               data-url="<?php echo $app->urlForBluesealXhr() ?>"
+                               data-inner-setup="true"
+                               data-length-menu-setup="25,100, 200, 500">
+                            <thead>
+                            <tr>
+                                <th data-slug="productCode"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Codice prodotto
+                                </th>
+                                <th data-slug="dummy"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Foto
+                                </th>
+                                <th data-slug="shop"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center">Shop
+                                </th>
+                                <th data-slug="brand"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">brand
+                                </th>
+                                <th data-slug="externalId"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Cpf
+                                </th>
+
+                                <th data-slug="price"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Prezzi Shop<br>pr.Pieno(pr.Saldo)-stato Saldo
+                                </th>
+                                <th data-slug="marketplaceAssociation"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Associazioni<br>Marketplace
+                                </th>
+                                <th data-slug="status"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Stato Pubblicazione<br>Marketplace
+                                </th>
+
+                                <th data-slug="productStatusMarketplaceId"
+                                    data-required="true"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center"
+                                    data-default-order="desc">Origine di<br>Pubblicazione
+                                </th>
+
+                                <th data-slug="season"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center">Stagione
+                                </th>
+                                <th data-slug="totalQty"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center">quantità Totali
+                                </th>
+                                <th data-slug="stock"
+                                    data-searchable="true"
+                                    data-orderable="true" class="center">Stock
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include "parts/footer.php" ?>
+    </div>
+</div>
+<?php include "parts/bsmodal.php"; ?>
+<?php include "parts/alert.php"; ?>
+<bs-toolbar class="toolbar-definition">
+    <bs-toolbar-group data-group-label="Gestisci Prodotto">
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-exchange"
+                data-permission="/admin/product/edit"
+                data-event="bs.adding.presta"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Pubblica Prodotti su marketPlace"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-space-shuttle"
+                data-permission="/admin/product/edit"
+                data-event="bs.add.presta.product.all"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Pubblica Tutti Prodotti su marketPlace con stato pubblicato"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-recycle"
+                data-permission="/admin/product/edit"
+                data-event="bs.update.presta.product.feature"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Aggiorna caratteristiche da PickyShop a Prestashop"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-close"
+                data-permission="/admin/product/edit"
+                data-event="bs.delete.product"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Elimina il prodotto dal marketplace"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-remote="bs.job.start"
+        ></bs-toolbar-button>
+    </bs-toolbar-group>
+    <bs-toolbar-group data-group-label="Gestisci saldo">
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-dollar"
+                data-permission="/admin/product/edit"
+                data-event="bs.marketplace.product.sale"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Metti in saldo i prodotti"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-window-close"
+                data-permission="/admin/product/edit"
+                data-event="bs.marketplace.remove.sale"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Togli i prodotti dal saldo sul Marketplace"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+    </bs-toolbar-group>
+    <bs-toolbar-group data-group-label="Operazione su Quantità">
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-check"
+                data-permission="/admin/product/edit"
+                data-event="bs.prestashop.align.quantity"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Aggiorna le quantità in prestashop"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-edge"
+                data-permission="/admin/product/edit"
+                data-event="bs.ebay.align.product"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Aggiorna i Prodotti su Ebay"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+    </bs-toolbar-group>
+    <bs-toolbar-group data-group-label="Emulatori Jobs">
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-flag"
+                data-permission="/admin/product/edit"
+                data-event="bs.marketplace.prepare.product"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Prepara prodotti per marketplace"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+        <bs-toolbar-button
+                data-tag="a"
+                data-icon="fa-flag-checkered"
+                data-permission="/admin/product/edit"
+                data-event="bs.marketplaceaccountrule.publish.product"
+                data-class="btn btn-default"
+                data-rel="tooltip"
+                data-title="Pubblica prodotti per Account asssociato a Marketplace"
+                data-placement="bottom"
+                data-toggle="modal"
+        ></bs-toolbar-button>
+
+    </bs-toolbar-group>
+</bs-toolbar>
+</body>
+</html>
