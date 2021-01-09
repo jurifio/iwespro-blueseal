7de0b571ca218e95b74c14fc3368d41820dbebe5
juri fiorani <jurif@hotmail.com>
2021-01-09 19:16:05 +0100

    creazione Vista Vendite

--- controllers/back/ajax/CAmazonMarketplaceProductListAjaxController.php
+++ controllers/back/ajax/CAmazonMarketplaceProductListAjaxController.php
@@ -58,7 +58,7 @@ class CEbayMarketplaceProductListAjaxController extends AAjaxController
             left JOIN Product p ON php.productId = p.id AND php.productVariantId = p.productVariantId    
             LEFT JOIN ProductStatus PS on p.productStatusId=PS.id       
             LEFT JOIN ProductBrand pb on p.productBrandId=pb.id
-           LEFT  JOIN PrestashopHasProductHasMarketplaceHasShop phphmhs ON php.productId = phphmhs.productId AND php.productVariantId = phphmhs.productVariantId and php.marketplaceHasShopId=phphmhs.marketplaceHasShopId
+            JOIN PrestashopHasProductHasMarketplaceHasShop phphmhs ON php.productId = phphmhs.productId AND php.productVariantId = phphmhs.productVariantId 
             LEFT JOIN MarketplaceHasShop mhs ON mhs.id = phphmhs.marketplaceHasShopId
             LEFT JOIN Shop s ON mhs.shopId = s.id
             LEFT JOIN Marketplace m ON mhs.marketplaceId = m.id
