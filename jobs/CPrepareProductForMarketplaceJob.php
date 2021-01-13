ddb2255c987a2d0fafecb003e57e7a86ca8812fe
juri fiorani <jurif@hotmail.com>
2021-01-13 19:29:17 +0100

    creazione Vista Vendite

--- jobs/CPrepareProductForMarketplaceJob.php
+++ jobs/CPrepareProductForMarketplaceJob.php
@@ -62,7 +62,7 @@ class CPrepareProductForMarketplaceJob extends ACronJob
                               if($phphmhs){
                                   $phphmhs->isPublished=3;
                                   $phphmhs->update();
-                                  $this->report('CPrepareProductForMarketplaceJob','Report','booking Depublish ' . $product['productId'] . '-' . $product['productVariantId'] . ' to marketplace' . $mhs->id);
+                                  $this->report('CPrepareProductForMarketplaceJob','Report','booking Depublish ' . $product['productId'] . '-' . $product['productVariantId'] . ' to aggregator' . $mhs->id);
                               }
                           }
                         }else{
@@ -71,7 +71,7 @@ class CPrepareProductForMarketplaceJob extends ACronJob
                             $phs->productVariantId=$product['productVariantId'];
                             $phs->marketplaceHasShopId=$mhs->id;
                             $phs->insert();
-                            $this->report('CPrepareProductForMarketplaceJob','Report','insert ' . $product['productId'] . '-' . $product['productVariantId'] . ' to marketplace' . $mhs->id);
+                            $this->report('CPrepareProductForMarketplaceJob','Report','insert ' . $product['productId'] . '-' . $product['productVariantId'] . ' to aggregator' . $mhs->id);
 
                         }
 
