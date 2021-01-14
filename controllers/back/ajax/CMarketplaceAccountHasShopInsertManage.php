87ba069e15b63b2d7b1873345edeec05e8bd6b3d
juri fiorani <jurif@hotmail.com>
2021-01-14 17:06:12 +0100

    creazione Vista Vendite

--- controllers/back/ajax/CMarketplaceAccountHasShopInsertManage.php
+++ controllers/back/ajax/CMarketplaceAccountHasShopInsertManage.php
@@ -462,7 +462,7 @@ class CMarketplaceAccountHasShopInsertManage extends AAjaxController
         $collectUpdate .= '"brands":"' . $brand . '"}';
         $collectUpdate = trim($collectUpdate," \t\n\r\0\x0B");
         $marketplaceAccount = \Monkey::app()->repoFactory->create('MarketplaceAccount')->findOneBy(['id' => $marketplaceAccountId]);
-        $marketplaceAccount->name = $marketplace_account_name;
+       // $marketplaceAccount->name = $marketplace_account_name;
         $marketplaceAccount->config = $collectUpdate;
         $marketplaceAccount->urlSite = $urlSite;
         $marketplaceAccount->isActive = $isActive;
