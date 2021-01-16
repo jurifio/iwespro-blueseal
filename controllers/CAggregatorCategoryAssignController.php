454e009315602a7940316d106eaa8e8a2cc58b7b
juri fiorani <jurif@hotmail.com>
2021-01-16 17:41:15 +0100

    creazione Vista Vendite

---
+++ controllers/CAggregatorCategoryAssignController.php
@@ -0,0 +1,68 @@
+<?php
+namespace bamboo\blueseal\controllers;
+
+use bamboo\core\theming\CRestrictedAccessWidgetHelper;
+use bamboo\ecommerce\views\VBase;
+
+
+/**
+ * Class CAggregatorCategoryAssignController
+ * @package bamboo\blueseal\controllers
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 12/07/2016
+ * @since 1.0
+ */
+class CAggregatorCategoryAssignController extends ARestrictedAccessRootController
+{
+    protected $fallBack = "blueseal";
+    protected $pageSlug = "marketplace_category_assign";
+
+    public function get()
+    {
+        $view = new VBase(array());
+        $view->setTemplatePath($this->app->rootPath().$this->app->cfg()->fetch('paths','blueseal').'/template/aggregator_category_assign.php');
+
+        return $view->render([
+            'app' => new CRestrictedAccessWidgetHelper($this->app),
+            'page'=>$this->page,
+            'sidebar' => $this->sidebar->build()
+        ]);
+    }
+
+	public function put()
+	{
+		$sample = \Monkey::app()->repoFactory->create('MarketplaceAccountCategory')->getEmptyEntity();
+		$sample->readId($this->app->router->request()->getRequestData("id"));
+		$one = \Monkey::app()->repoFactory->create('MarketplaceAccountCategory')->findOneBy($sample->getIds());
+		$this->app->dbAdapter->delete('ProductCategoryHasMarketplaceAccountCategory',
+			[   'marketplaceId'=>$one->marketplaceId,
+				'marketplaceAccountId'=>$one->marketplaceAccountId,
+				'marketplaceAccountCategoryId'=>$one->marketplaceCategoryId]);
+		foreach ($this->app->router->request()->getRequestData("value") as $value) {
+			$this->app->dbAdapter->insert('ProductCategoryHasMarketplaceAccountCategory',[   'marketplaceId'=>$one->marketplaceId,
+			                                  'marketplaceAccountId'=>$one->marketplaceAccountId,
+			                                  'marketplaceAccountCategoryId'=>$one->marketplaceCategoryId,'productCategoryId'=>$value]);
+		}
+	}
+
+	/**
+	 * @return int
+	 */
+	public function delete() {
+		$i = 0;
+		foreach($this->app->router->request()->getRequestData('ids') as $id) {
+			$sample = \Monkey::app()->repoFactory->create('MarketplaceAccountCategory')->getEmptyEntity();
+			$sample->readId($id);
+			$one = \Monkey::app()->repoFactory->create('MarketplaceAccountCategory')->findOneBy($sample->getIds());
+			$one->isRelevant = 0;
+			if($one->update()>0) $i++;
+		}
+		return $i;
+	}
+}
---
+++ controllers/back/ajax/CAggregatorCategoryAssignAjaxController.php
@@ -0,0 +1,87 @@
+<?php
+namespace bamboo\controllers\back\ajax;
+
+use bamboo\blueseal\business\CDataTables;
+use bamboo\core\intl\CLang;
+
+/**
+ * Class CAggregatorCategoryAssignAjaxController
+ * @package bamboo\blueseal\controllers\ajax
+ *
+ * @author Bambooshoot Team <emanuele@bambooshoot.agency>
+ *
+ * @copyright (c) Bambooshoot snc - All rights reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * @date 12/07/2016
+ * @since 1.0
+ */
+class CAggregatorCategoryAssignAjaxController extends AAjaxController
+{
+    public function get()
+    {
+	    $marketplaceAccountCategoryRepo =  \Monkey::app()->repoFactory->create('MarketplaceAccountCategory');
+
+        $sql = "select `m`.`id` AS `marketplaceId`,
+                       `ma`.`id` AS `marketplaceAccountId`,
+                       `mac`.`marketplaceCategoryId` AS `marketplaceCategoryId`,
+                       `m`.`name` AS `marketplace`,
+                       `ma`.`name` AS `marketplaceAccount`,
+                       `mac`.`name` AS `marketplaceAccountCategory`,
+                       `mac`.`path` AS `marketplaceAccountPath`,
+                       `mac`.`isRelevant` AS `isRelevant`,
+                       `pc`.`slug` AS `internalCategory`,
+                       pc.id as productCategoryId,
+                       `mac`.`name` AS `marketplaceCategory` 
+               from `Marketplace` `m` 
+                    join `MarketplaceAccount` `ma` on `m`.`id` = `ma`.`marketplaceId` 
+                    join `MarketplaceAccountCategory` `mac` on (`ma`.`marketplaceId`,`ma`.`id`) = (`mac`.`marketplaceId`,`mac`.`marketplaceAccountId`)
+                    left join (`ProductCategoryHasMarketplaceAccountCategory` `pchmac` 
+                                join `ProductCategory` `pc` on `pc`.`id` = `pchmac`.`productCategoryId`)
+                              on (`mac`.`marketplaceId`,`mac`.`marketplaceAccountId`, `mac`.`marketplaceCategoryId` ) = 
+                                  (`pchmac`.`marketplaceId`,`pchmac`.`marketplaceAccountId`, `pchmac`.`marketplaceAccountCategoryId`)
+                                  where m.type='cpc'";
+
+        $datatable = new CDataTables($sql,$marketplaceAccountCategoryRepo->getEmptyEntity()->getPrimaryKeys(),$_GET,true);
+		$datatable->addCondition('isRelevant',[1]);
+
+        $okManage = $this->app->getUser()->hasPermission('/admin/product/edit');
+
+        $datatable->doAllTheThings(true);
+
+        foreach($datatable->getResponseSetData() as $key => $row) {
+            $val = $marketplaceAccountCategoryRepo->findOne($row);
+            $row["DT_RowId"] = 'row__'.$val->printId();
+            $row['marketplace'] = $val->marketplaceAccount->marketplace->name;
+            $row['marketplaceAccount'] = $val->marketplaceAccount->name;
+            $row['marketplaceAccountCategory'] = $val->name;
+			try {
+				$appoggio = explode('_',$val->path);
+				unset($appoggio[0]);
+				$appoggio = array_reverse($appoggio);
+				$appoggio = implode('<br>',$appoggio);
+			} catch (\Throwable $e) {
+				$appoggio = $val->marketplaceCategoryPath;
+			}
+            $row['marketplaceAccountPath'] = $appoggio;
+	        if(!$okManage) {
+				$html = 'Non si può';
+	        } else {
+	        	$catIds = [];
+	        	foreach($val->productCategory as $productCategory) {
+					$catIds[] = $productCategory->id;
+		        }
+		        $html = '<select class="full-width selectpicker selectize-streachable" 
+		                         placeholder="Seleziona la categoria"
+		                         data-name="categorySelect"
+		                         data-selected="'.implode('__',$catIds).'"
+		                         data-id="' . $val->printId() . '" 
+		                         tabindex="-1" ></select>';
+	        }
+            $row['productCategoryId'] = $html;
+            $datatable->setResponseDataSetRow($key,$row);
+        }
+        return $datatable->responseOut();
+    }
+}
