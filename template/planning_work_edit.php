8ce049620f3dfbbb06a8d3045fb24a74e18267e8
juri fiorani <jurif@hotmail.com>
2021-01-26 11:41:43 +0100

    creazione Vista Vendite

--- template/planning_work_edit.php
+++ template/planning_work_edit.php
@@ -291,7 +291,6 @@
                                                             break;
 
                                                     } ?>
-
                                                 </select>
                                             </div>
                                         </div>
