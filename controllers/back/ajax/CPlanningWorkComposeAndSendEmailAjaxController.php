5598a218b8eaba6db02e6956e88764dc54ce5cda
juri fiorani <jurif@hotmail.com>
2021-02-01 14:51:19 +0100

    creazione Vista Vendite

--- controllers/back/ajax/CPlanningWorkComposeAndSendEmailAjaxController.php
+++ controllers/back/ajax/CPlanningWorkComposeAndSendEmailAjaxController.php
@@ -36,10 +36,10 @@ class CPlanningWorkComposeAndSendEmailAjaxController extends AAjaxController
         $planningWorkId = $this->app->router->request()->getRequestData('planningWorkId');
         $planningWorkStatusId = $this->app->router->request()->getRequestData('planningWorkStatusId');
         $planningWorkTypeId = $this->app->router->request()->getRequestData('planningWorkTypeId');
-        $billRegistryClientId=$this->app->router->request()->getRequestData('billRegistryClientId');
-        $bri=\Monkey::app()->repoFactory->create('BillRegistryClient')->findOneBy(['id'=>$billRegistryClientId]);
-        $companyName=$bri->companyName;
-        $emailAdmin=$bri->emailAdmin;
+        $billRegistryClientId = $this->app->router->request()->getRequestData('billRegistryClientId');
+        $bri = \Monkey::app()->repoFactory->create('BillRegistryClient')->findOneBy(['id' => $billRegistryClientId]);
+        $companyName = $bri->companyName;
+        $emailAdmin = $bri->emailAdmin;
         $title = $this->app->router->request()->getRequestData('title');
         $request = $this->app->router->request()->getRequestData('request');
         $startDateWork = $this->app->router->request()->getRequestData('start');
@@ -49,29 +49,31 @@ class CPlanningWorkComposeAndSendEmailAjaxController extends AAjaxController
         $hour = $this->app->router->request()->getRequestData('hour');
         $percentageStatus = $this->app->router->request()->getRequestData('percentageStatus');
         $notifyEmail = $this->app->router->request()->getRequestData('notifyEmail');
-        $planningWorkType=\Monkey::app()->repoFactory->create('PlanningWorkType')->findOneBy(['id'=>$planningWorkTypeId]);
-        $subject=str_replace('{planningWorkId}',$planningWorkId,$planningWorkType->subject);
-        $planningWorkEvent=\Monkey::app()->repoFactory->create('PlanningWorkEvent')->findOneBy(['planningWorkId'=>$planningWorkId,'planningWorkStatusId'=>$planningWorkStatusId,'planningWorkTypeId'=>$planningWorkTypeId,'isSent'=>'0']);
-if($planningWorkEvent) {
+        $planningWorkType = \Monkey::app()->repoFactory->create('PlanningWorkType')->findOneBy(['id' => $planningWorkTypeId]);
+        $subject = str_replace('{planningWorkId}',$planningWorkId,$planningWorkType->subject);
+        $planningWorkEvent = \Monkey::app()->repoFactory->create('PlanningWorkEvent')->findOneBy(['planningWorkId' => $planningWorkId,'planningWorkStatusId' => $planningWorkStatusId,'planningWorkTypeId' => $planningWorkTypeId,'isSent' => '0']);
+        if ($planningWorkEvent) {
 
 
-        $textEmail[] = ['result' => '1',
-            'toMail' => $emailAdmin,
-            'subject' => $subject,
-            'text' => $planningWorkEvent->mail
-        ];
+            $textEmail[] = ['result' => '1',
+                'toMail' => $emailAdmin,
+                'subject' => $subject,
+                'text' => $planningWorkEvent->mail
+            ];
 
-}else{
-    $textEmail[] = ['result' => '0',
-        'toMail' => 'Compila',
-        'subject' => 'Compila',
-        'text' => 'Compila'
-    ];
-}
+        } else {
+            $textEmail[] = ['result' => '0',
+                'toMail' => 'Compila',
+                'subject' => 'Compila',
+                'text' => 'Compila'
+            ];
+        }
 
         return json_encode($textEmail);
     }
-    public function post(){
+
+    public function post()
+    {
         try {
             $planningWorkId = $this->app->router->request()->getRequestData('planningWorkId');
             $planningWorkStatusId = $this->app->router->request()->getRequestData('planningWorkStatusId');
@@ -90,34 +92,34 @@ if($planningWorkEvent) {
             $hour = $this->app->router->request()->getRequestData('hour');
             $percentageStatus = $this->app->router->request()->getRequestData('percentageStatus');
             $notifyEmail = $this->app->router->request()->getRequestData('notifyEmail');
-            $toMail=$this->app->router->request()->getRequestData('toMail');
-            $mail=$this->app->router->request()->getRequestData('mail');
+            $toMail = $this->app->router->request()->getRequestData('toMail');
+            $mail = $this->app->router->request()->getRequestData('mail');
             $subject = $this->app->router->request()->getRequestData('subject');
             $planningWorkType = \Monkey::app()->repoFactory->create('PlanningWorkType')->findOneBy(['id' => $planningWorkTypeId]);
             $today = (new DateTime())->format('Y-m-d H:i:s');
-            $planningWorkEvent = \Monkey::app()->repoFactory->create('PlanningWorkEvent')->findOneBy(['planningWorkId' => $planningWorkId,'planningWorkStatusId' => $planningWorkStatusId,'planningWorkTypeId' => $planningWorkTypeId,'isSent'=>'0']);
-            $planningWorkEvent->mail=$mail;
+            $planningWorkEvent = \Monkey::app()->repoFactory->create('PlanningWorkEvent')->findOneBy(['planningWorkId' => $planningWorkId,'planningWorkStatusId' => $planningWorkStatusId,'planningWorkTypeId' => $planningWorkTypeId,'isSent' => '0']);
+            $planningWorkEvent->mail = $mail;
             if ($notifyEmail == "1") {
 
-                    $planningWorkEvent->isSent=1;
-                    $planningWorkEvent->dateSent=$today;
-                   if (ENV != 'dev') {
-                        /** @var \bamboo\domain\repositories\CEmailRepo $emailRepo */
-                        $emailRepo = \Monkey::app()->repoFactory->create('Email');
-                        $to[] = [$emailAdmin];
-                       $tocc=[];
-                       $toccb=[];
-                       $tocc[]=['gianluca@iwes.it'];
-                       $toccb[]=['juri@iwes.it'];
-                        $emailRepo->newMail('services@iwes.it',[$emailAdmin],[$tocc],[$toccb],$subject,$mail,null,null,null,'mailGun',false,null);
-                    }
+                $planningWorkEvent->isSent = 1;
+                $planningWorkEvent->dateSent = $today;
+                if (ENV != 'dev') {
+                    /** @var \bamboo\domain\repositories\CEmailRepo $emailRepo */
+                    $emailRepo = \Monkey::app()->repoFactory->create('Email');
+                    $to[] = [$emailAdmin];
+                    $tocc = [];
+                    $toccb = [];
+                    $tocc[] = ['gianluca@iwes.it'];
+                    $toccb[] = ['juri@iwes.it'];
+                    $emailRepo->newMail('services@iwes.it',[$emailAdmin],[$tocc],[$toccb],$subject,$mail,null,null,null,'mailGun',false,null);
+                }
 
             }
             $planningWorkEvent->update();
             return 'invio eseguito';
 
 
-        }catch(\Throwable $e){
+        } catch (\Throwable $e) {
             return $e;
 
         }
