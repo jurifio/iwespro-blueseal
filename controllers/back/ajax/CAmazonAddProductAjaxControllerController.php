408725a06d9ddef0cfa45a07ebb4c73811cbb02c
juri fiorani <jurif@hotmail.com>
2021-02-08 10:24:53 +0100

    creazione Vista Vendite

--- controllers/back/ajax/CAmazonAddProductAjaxControllerController.php
+++ controllers/back/ajax/CAmazonAddProductAjaxControllerController.php
@@ -43,6 +43,7 @@ class CAmazonAddProductAjaxControllerController extends AAjaxController
         $marketplaceAccounts = \Monkey::app()->repoFactory->create('MarketplaceAccount')->em()->findBySql($sql,[]);
 
         foreach ($marketplaceAccounts as $marketplaceAccount) {
+            $messageId=1;
             try {
                 $sql = "SELECT 	productId, 
 						productVariantId, 
@@ -52,42 +53,43 @@ class CAmazonAddProductAjaxControllerController extends AAjaxController
 						Marketplace m 
 				WHERE 	m.id = mahp.marketplaceId 
 					AND m.name = 'Amazon' and mahp.isToWork = 1 and mahp.marketplaceAccountId = 42";
-                $res = \Monkey::app()->repoFactory->create('MarketplaceAccountHasProduct')->em()->findBySql($sql,[$marketplaceAccount->id]);
+                $res = \Monkey::app()->repoFactory->create('MarketplaceAccountHasProduct')->em()->findBySql($sql,[]);
 
                 foreach ($res as $re) {
                     $this->prepareSkus($re);
                 }
-
+                $productFeed="productFeed";
                 $product = new CAmazonProductFeedBuilder($this->app);
-                $this->prepareAndSend($marketplaceAccount,$product,$res);
-
+                $this->prepareAndSend($marketplaceAccount,$product,$res,$productFeed,$messageId);
+                $inventoryFeed="inventoryFeed";
                 $inventary = new CAmazonInventoryFeedBuilder($this->app);
-                $this->prepareAndSend($marketplaceAccount,$inventary,$res);
-
+                $this->prepareAndSend($marketplaceAccount,$inventary,$res,$inventoryFeed,$messageId);
+                $pricingFeed = "pricingFeed";
                 $pricing = new CAmazonPricingFeedBuilder($this->app);
-                $this->prepareAndSend($marketplaceAccount,$pricing,$res);
-
-                $relationship = new CAmazonRelationshipFeedBuilder($this->app);
-                $this->prepareAndSend($marketplaceAccount,$relationship,$res);
-
+                $this->prepareAndSend($marketplaceAccount,$pricing,$res,$pricingFeed,$messageId);
+                $imageFeed="imageFeed";
                 $image = new CAmazonImageFeedBuilder($this->app);
-                $this->prepareAndSend($marketplaceAccount,$image,$res);
-
+                $this->prepareAndSend($marketplaceAccount,$image,$res,$imageFeed,$messageId);
+                $relationshipFeed="relationshipFeed";
+                $relationship = new CAmazonRelationshipFeedBuilder($this->app);
+                $this->prepareAndSend($marketplaceAccount,$relationship,$res,$relationshipFeed,$messageId);
+                $messageId++;
             } catch
             (\Throwable $e) {
                 \Monkey::app()->applicationLog('CAmazonAddProductJob','ERROR',$e->getLine(),$e->getMessage(),$e->getFile());
             }
-            return $count;
+
+            return $messageId;
         }
     }
-    protected function prepareAndSend($marketplaceAccount, AAmazonFeedBuilder $builder,$products) {
+    protected function prepareAndSend($marketplaceAccount, AAmazonFeedBuilder $builder,$products,$typeFeed,$messageId) {
         \Monkey::app()->vendorLibraries->load('amazonMWS');
-        $service = new \MarketplaceWebService_Client(
+      /*  $service = new \MarketplaceWebService_Client(
             $marketplaceAccount->config['awsAccessKeyId'],
             $marketplaceAccount->config['awsSecretAccessKey'],
             ["ServiceURL"=>$marketplaceAccount->config['serviceUrl'],],
             "BlueSeal",
-            "1.01");
+            "1.01");*/
 
         $content = $builder->prepare($products,false)->getRawBody();
 
@@ -96,8 +98,8 @@ class CAmazonAddProductAjaxControllerController extends AAjaxController
         $x->setIndent(false);
         $x->startDocument();
         $x->startElement('AmazonEnvelope');
-        $x->writeAttribute("xsi:noNamespaceSchemaLocation","amzn-envelope.xsd");
         $x->writeAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance");
+        $x->writeAttribute("xsi:noNamespaceSchemaLocation","amzn-envelope.xsd");
         $x->startElement('Header');
         $x->writeElement('DocumentVersion','1.01');
         $x->writeElement('MerchantIdentifier',$marketplaceAccount->config['merchantIdentifier']);
@@ -109,10 +111,15 @@ class CAmazonAddProductAjaxControllerController extends AAjaxController
         echo $content;
         $feedHandle = @fopen('php://temp', 'rw+');
         fwrite($feedHandle, $content);
+        $dateNow=(new \DateTime())->format('Y-m-d_His');
+        $myfile = fopen("/media/sf_sites/iwespro/temp/".$typeFeed."_".$messageId."_".$dateNow.".xml", "w");
+        fwrite($myfile, $content);
+        fclose($myfile);
         rewind($feedHandle);
+        //$marketplaceIdList=explode(',',$marketplaceAccount->config['marketplaceIdList']);
         $parameters = array (
             'Merchant' => $marketplaceAccount->config['merchantIdentifier'],
-            'MarketplaceIdList' => ["Id" => $marketplaceAccount->config['marketplaceIdList']],
+            'MarketplaceIdList' => ["Id" => 'APJ6JRA9NG5V4'],
             'FeedType' => $builder->getFeedTypeName(),
             'FeedContent' => $feedHandle,
             'PurgeAndReplace' => false,
@@ -122,7 +129,7 @@ class CAmazonAddProductAjaxControllerController extends AAjaxController
             $parameters['MWSAuthToken'] = $marketplaceAccount->config['MWSAuthToken']; // Optional]
         }
         rewind($feedHandle);
-        $request = new \MarketplaceWebService_Model_SubmitFeedRequest($parameters);
+      /*  $request = new \MarketplaceWebService_Model_SubmitFeedRequest($parameters);
 
         try {
             $response = $service->submitFeed($request);
@@ -190,7 +197,7 @@ class CAmazonAddProductAjaxControllerController extends AAjaxController
             echo("ResponseHeaderMetadata: " . $ex->getResponseHeaderMetadata() . "\n");
             fclose($feedHandle);
             return false;
-        }
+        }*/
         fclose($feedHandle);
         return true;
     }
