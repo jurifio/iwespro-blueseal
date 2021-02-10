a73233382bc32b54b727c529de447f46a1bd276f
juri fiorani <jurif@hotmail.com>
2021-02-10 19:47:35 +0100

    creazione Vista Vendite

--- assets/js/newdashboard.js
+++ assets/js/newdashboard.js
@@ -1,25 +1,30 @@
 (function ($) {
 
-var Name='gennaio';
-    var Name2='Febbraio';
-    var
 
 
-    let ctxSent = document.getElementById("ChartQtyOrder").getContext('2d');
-    let ChartQtyOrder = new Chart(ctxSent, {
+
+    let ctxQtyOrder = document.getElementById("ChartQtyOrder").getContext('2d');
+    let ChartQtyOrder = new Chart(ctxQtyOrder, {
         type: 'line',
         data: {
-            labels: ['Gennaio', 'Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
+            labels: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
             datasets: [{
             label: 'Numero Ordini',
-                data: ['150', '160','15','100','10','150','180','15','10','150', '160','15'],
+                data: [150.00, 160.00,15.00,100.00,10.00,150.00,180.00,15.00,10.00,150.00, 160.00,15.00],
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
                     'rgba(75, 192, 192, 0.2)',
                     'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(186, 159, 64, 0.2)',
+                    'rgba(160, 159, 64, 0.2)',
+                    'rgba(50, 159, 64, 0.2)',
+                    'rgba(70, 159, 64, 0.2)',
+                    'rgba(80, 159, 64, 0.2)',
+                    'rgba(10, 159, 64, 0.2)'
+
                 ],
                 borderColor: [
                     'rgba(255,99,132,1)',
@@ -27,7 +32,63 @@ var Name='gennaio';
                     'rgba(255, 206, 86, 1)',
                     'rgba(75, 192, 192, 1)',
                     'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(186, 159, 64, 1)',
+                    'rgba(160, 159, 64, 1)',
+                    'rgba(50, 159, 64, 1)',
+                    'rgba(70, 159, 64, 1)',
+                    'rgba(80, 159, 64, 1)',
+                    'rgba(10, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+    let ctxValueOrder = document.getElementById("ChartValueOrder").getContext('2d');
+    let ChartValueOrder = new Chart(ctxValueOrder, {
+        type: 'bar',
+        data: {
+            labels: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'],
+            datasets: [{
+                label: 'Numero Ordini',
+                data: [150.00, 160.00,15.00,100.00,10.00,150.00,180.00,15.00,10.00,150.00, 160.00,15.00],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                    'rgba(186, 159, 64, 0.2)',
+                    'rgba(160, 159, 64, 0.2)',
+                    'rgba(50, 159, 64, 0.2)',
+                    'rgba(70, 159, 64, 0.2)',
+                    'rgba(80, 159, 64, 0.2)',
+                    'rgba(10, 159, 64, 0.2)'
+
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                    'rgba(186, 159, 64, 1)',
+                    'rgba(160, 159, 64, 1)',
+                    'rgba(50, 159, 64, 1)',
+                    'rgba(70, 159, 64, 1)',
+                    'rgba(80, 159, 64, 1)',
+                    'rgba(10, 159, 64, 1)'
                 ],
                 borderWidth: 1
             }]
