2a296e0355d76fe62074df08036b9f75be2b5966
juri fiorani <jurif@hotmail.com>
2021-02-11 18:37:28 +0100

    creazione Vista Vendite

--- controllers/CBlueSealNewDashboardController.php
+++ controllers/CBlueSealNewDashboardController.php
@@ -315,7 +315,7 @@ WHERE c.creationDate between \''.$timeStartMaskCompare.'\' and \''.$timeEndMaskC
             'data' => $this->request->getUrlPath(),
             'page' => $this->page,
             'stats' => $stats,
-            'arrayOrder'=>substr($arrayOrder,-1),
+            'arrayOrder'=>substr($arrayOrder,0,-1),
             'arrayLabelOrder'=>substr($arrayLabelOrder,0,-1),
             'arrayOrderReturn'=>substr($arrayOrderReturn,0,-1),
             'arrayLabelOrderReturn'=>substr($arrayLabelOrderReturn,0,-1),
