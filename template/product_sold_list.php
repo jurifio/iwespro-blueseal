ee07677fa20b8632009dcf2c22244dc9d337ad4e
juri fiorani <jurif@hotmail.com>
2021-02-18 09:19:18 +0100

    creazione Vista Vendite

--- template/product_sold_list.php
+++ template/product_sold_list.php
@@ -314,5 +314,12 @@
 </div>
 <?php include "parts/bsmodal.php"; ?>
 <?php include "parts/alert.php"; ?>
+<bs-toolbar class="toolbar-definition">
+    <bs-toolbar-group data-group-label="Gestione prodotti">
+        <bs-toolbar-button
+                data-remote="bs.lists.generate.csv"
+        ></bs-toolbar-button>
+    </bs-toolbar-group>
+</bs-toolbar>
 </body>
 </html>
