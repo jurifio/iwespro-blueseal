6e81ddbe5d632d2ca44fc37c05a6635868907544
juri fiorani <jurif@hotmail.com>
2021-05-04 10:59:22 +0200

    creazione Vista Vendite

--- jobs/CGenerateDummyJob.php
+++ jobs/CGenerateDummyJob.php
@@ -32,51 +32,66 @@ class CGenerateDummyJob extends ACronJob
         try {
             /** @var  $productRepo CProductRepo */
             $productRepo = \Monkey::app()->repoFactory->create('Product');
-            $query = "SELECT
-              `p`.`id`                                                         AS `productId`,
-              `p`.`productVariantId`                                           AS `productVariantId`,
-              concat(`p`.`id`, '-', `p`.`productVariantId`)                    AS `productCode`,
-              concat(`p`.`itemno`, ' # ', `pv`.`name`)                         AS `code`,
-              `s`.`name`                                                       AS `shop`,
-              `s`.`id`                                                         AS `shopId`,
-              `pb`.`name`                                                      AS `brand`,
-              `p`.`externalId`                                                 AS `externalId`,
-              `ps`.`name`                                                      AS `status`,
-              concat_ws('-',`psg`.`locale`, `psmg`.`name`)                     AS `sizeGroup`,
-              `p`.`creationDate`                                               AS `creationDate`,
-              `pb`.slug as productBrandSlug,  
-              group_concat(`ds`.`size` ORDER BY `ds`.`size` ASC SEPARATOR '-') AS `problems`,
-          if((p.id, p.productVariantId) IN (SELECT
-                                                              ProductHasProductPhoto.productId,
-                                                              ProductHasProductPhoto.productVariantId
-                                                            FROM ProductHasProductPhoto), 'sì', 'no')                 AS hasPhotos,
-              productCategoryId AS categoryId
-            FROM `Product` `p`
-              JOIN `ProductVariant` `pv` ON `pv`.`id` = `p`.`productVariantId`
-              JOIN `ProductBrand` `pb` ON `p`.`productBrandId` = `pb`.`id`
-              JOIN `ProductStatus` `ps` ON `p`.`productStatusId` = `ps`.`id`
-              JOIN `DirtyProduct` `dp` ON (`p`.`id` = `dp`.`productId`) AND (`p`.`productVariantId` = `dp`.`productVariantId`)
-              JOIN `DirtySku` `ds` ON `dp`.`id` = `ds`.`dirtyProductId`
-              JOIN `ShopHasProduct` `sp` ON (`dp`.`productId` = `sp`.`productId`)
-                                              AND (`dp`.`productVariantId` = `sp`.`productVariantId`)
-                                              AND (`dp`.`shopId` = `sp`.`shopId`)
-              JOIN `ProductSizeGroup` `psg` ON `sp`.`productSizeGroupId` = `psg`.`id`
-              JOIN `Shop` `s` ON `sp`.`shopId` = `s`.`id`
-              LEFT JOIN ProductSizeMacroGroup psmg ON psg.productSizeMacroGroupId = psmg.id
+            /* $query = "SELECT
+               `p`.`id`                                                         AS `productId`,
+               `p`.`productVariantId`                                           AS `productVariantId`,
+               concat(`p`.`id`, '-', `p`.`productVariantId`)                    AS `productCode`,
+               concat(`p`.`itemno`, ' # ', `pv`.`name`)                         AS `code`,
+               `s`.`name`                                                       AS `shop`,
+               `s`.`id`                                                         AS `shopId`,
+               `pb`.`name`                                                      AS `brand`,
+               `p`.`externalId`                                                 AS `externalId`,
+               `ps`.`name`                                                      AS `status`,
+               concat_ws('-',`psg`.`locale`, `psmg`.`name`)                     AS `sizeGroup`,
+               `p`.`creationDate`                                               AS `creationDate`,
+               `pb`.slug as productBrandSlug,
+               group_concat(`ds`.`size` ORDER BY `ds`.`size` ASC SEPARATOR '-') AS `problems`,
+           if((p.id, p.productVariantId) IN (SELECT
+                                                               ProductHasProductPhoto.productId,
+                                                               ProductHasProductPhoto.productVariantId
+                                                             FROM ProductHasProductPhoto), 'sì', 'no')                 AS hasPhotos,
+               productCategoryId AS categoryId
+             FROM `Product` `p`
+               JOIN `ProductVariant` `pv` ON `pv`.`id` = `p`.`productVariantId`
+               JOIN `ProductBrand` `pb` ON `p`.`productBrandId` = `pb`.`id`
+               JOIN `ProductStatus` `ps` ON `p`.`productStatusId` = `ps`.`id`
+               JOIN `DirtyProduct` `dp` ON (`p`.`id` = `dp`.`productId`) AND (`p`.`productVariantId` = `dp`.`productVariantId`)
+               JOIN `DirtySku` `ds` ON `dp`.`id` = `ds`.`dirtyProductId`
+               JOIN `ShopHasProduct` `sp` ON (`dp`.`productId` = `sp`.`productId`)
+                                               AND (`dp`.`productVariantId` = `sp`.`productVariantId`)
+                                               AND (`dp`.`shopId` = `sp`.`shopId`)
+               JOIN `ProductSizeGroup` `psg` ON `sp`.`productSizeGroupId` = `psg`.`id`
+               JOIN `Shop` `s` ON `sp`.`shopId` = `s`.`id`
+               LEFT JOIN ProductSizeMacroGroup psmg ON psg.productSizeMacroGroupId = psmg.id
 
-              LEFT JOIN ProductHasProductCategory phpc ON p.id = phpc.productId AND p.productVariantId = phpc.productVariantId
-            WHERE
-                  if((p.id, p.productVariantId) IN (SELECT
-                                                              ProductHasProductPhoto.productId,
-                                                              ProductHasProductPhoto.productVariantId
-                                                            FROM ProductHasProductPhoto), 'sì', 'no') ='sì' and
-                   p.dummyPicture LIKE '%bs-dummy-16-9.png%' or p.dummyPicture like '%//assets//%' 
-           
-            GROUP BY `dp`.`productId`, `dp`.`productVariantId`, `dp`.`shopId`, phpc.productCategoryId";
+               LEFT JOIN ProductHasProductCategory phpc ON p.id = phpc.productId AND p.productVariantId = phpc.productVariantId
+             WHERE
+                   if((p.id, p.productVariantId) IN (SELECT
+                                                               ProductHasProductPhoto.productId,
+                                                               ProductHasProductPhoto.productVariantId
+                                                             FROM ProductHasProductPhoto), 'sì', 'no') ='sì' and
+                    p.dummyPicture LIKE '%bs-dummy-16-9.png%' or p.dummyPicture like '%//assets//%'
+
+             GROUP BY `dp`.`productId`, `dp`.`productVariantId`, `dp`.`shopId`, phpc.productCategoryId";*/
+            $query ='select id as productId, productVariantId from Product ';
             $res = $this->app->dbAdapter->query($query,[])->fetchAll();
             foreach ($res as $result) {
                 $product = $productRepo->findOneBy(['id' => $result['productId'],'productVariantId' => $result['productVariantId']]);
-                $url = 'https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_BIG);
+
+
+                if (@getimagesize('https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_THUMB))) {
+                    $url='https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_THUMB);
+                }elseif(@getimagesize('https://cdn.iwes.it/'.$product->getPhoto(2, \bamboo\domain\entities\CProductPhoto::SIZE_THUMB))){
+                    $url='https://cdn.iwes.it/'.$product->getPhoto(2, \bamboo\domain\entities\CProductPhoto::SIZE_THUMB);
+                }elseif(@getimagesize('https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_MEDIUM))){
+                    $url='https://cdn.iwes.it/'.$product->getPhoto(2, \bamboo\domain\entities\CProductPhoto::SIZE_MEDIUM);
+                }elseif(@getimagesize('https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_BIG))){
+                    $url='https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_BIG);
+                }elseif(@getimagesize('https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_MEDIUM))){
+                    $url='https://cdn.iwes.it/'.$product->getPhoto(1, \bamboo\domain\entities\CProductPhoto::SIZE_MEDIUM);
+                }else{
+                    $url='bs-dummy-16-9.png';
+                }
                 $product->dummyPicture = $url;
                 $product->update();
             }
