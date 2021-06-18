9390fe3b40ee62a4a3feb3384738f398668be9fc
juri fiorani <jurif@hotmail.com>
2021-06-18 16:09:11 +0200

    creazione Vista Vendite

--- controllers/back/ajax/CProductPhotoAjaxManage.php
+++ controllers/back/ajax/CProductPhotoAjaxManage.php
@@ -53,11 +53,22 @@ if($product) {
     public function post()
     {
 
-        $product = \Monkey::app()->repoFactory->create('Product')->findOne([$this->app->router->request()->getRequestData('id'),
-            $this->app->router->request()->getRequestData('productVariantId')]);
+        $product = \Monkey::app()->repoFactory->create('Product')->findOne([$this->app->router->request()->getRequestData('id'),$this->app->router->request()->getRequestData('productVariantId')]);
+        $files = $this->app->router->request()->getFiles();
         $this->app->vendorLibraries->load("amazon2723");
         $config = $this->app->cfg()->fetch('miscellaneous', 'amazonConfiguration');
         $tempFolder = $this->app->rootPath().$this->app->cfg()->fetch('paths', 'tempFolder')."/";
+        $files = $this->app->router->request()->getFiles();
+        $name = md5(rand(100,200));
+        $ext = explode('.',$files['file']['name'][0]);
+        $filename = $name . '.' . $ext[1];
+        $destination = $tempFolder . $filename; //change this directory
+        $location = $files["file"]["tmp_name"][0];
+        move_uploaded_file($location,$destination);
+        $newname =  $files["file"]["name"][0];
+        rename($destination,$tempFolder . $newname);
+        $newdestination = $tempFolder . $newname;
+        $image = new S3Manager($config['credential']);
 
         $image = new ImageManager(new S3Manager($config['credential']), $this->app, $tempFolder);
 
