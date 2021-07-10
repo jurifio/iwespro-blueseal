f0bfb7129e6aef22c454b924fdf881ef1a80323f
juri fiorani <jurif@hotmail.com>
2021-07-10 18:37:38 +0200

    creazione Vista Vendite

--- jobs/CEbayNewAddProductJob.php
+++ jobs/CEbayNewAddProductJob.php
@@ -62,7 +62,6 @@ class CEbayNewAddProductJob extends ACronJob
             $shop = $shopRepo->findOneBy(['id' => $marketplaceAccount->config['shopId']]);
             $addressBook = $addressBookRepo->findOneBy(['id' => $shop->billingAddressBookId]);
             foreach ($goods as $good) {
-                if($good->refMarketplaceId=='') {
                     /**  @var CProduct $product * */
                     $product = \Monkey::app()->repoFactory->create('Product')->findOneBy(['id' => $good->productId,'productVariantId' => $good->productVariantId]);
                     if ($product->qty == 0) {
@@ -390,7 +389,7 @@ class CEbayNewAddProductJob extends ACronJob
                     $good->refMarketplaceId = $refMarketplaceId;
                     $good->result = $resultCall;
                     $good->update();
-                }
+
             }
 
 
