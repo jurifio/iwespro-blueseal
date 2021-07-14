15395ee1456d3c0daed6745b880cfdd360539e90
juri fiorani <jurif@hotmail.com>
2021-07-14 11:29:15 +0200

    creazione Vista Vendite

--- template/widgets/orderLine.php
+++ template/widgets/orderLine.php
@@ -42,22 +42,24 @@ $sku = \bamboo\domain\entities\CProductSku::defrost($line->frozenProduct);
 <td class="center">
     <?php $shipmentPrint='';
     $orderLineHasShipment=\Monkey::app()->repoFactory->create('OrderLineHasShipment')->findOneBy(['orderLineId'=>$line->id,'orderId'=>$line->orderId]);
-    if(count($orderLineHasShipment)>0){
-        $shipment=\Monkey::app()->repoFactory->create('Shipment')->findOneBy(['id'=>$orderLineHasShipment->shipmentId]);
-        $findCarrier = \Monkey::app()->repoFactory->create('Carrier')->findOneBy(['id' => $shipment->carrierId]);
-        $btnclass='btn btn-success';
-        if ($shipment->deliveryDate != null && $shipment->shipmentDate != null) {
+    if(count($orderLineHasShipment)>0) {
+        $shipment = \Monkey::app()->repoFactory->create('Shipment')->findOneBy(['id' => $orderLineHasShipment->shipmentId]);
+        if ($shipment) {
+            $findCarrier = \Monkey::app()->repoFactory->create('Carrier')->findOneBy(['id' => $shipment->carrierId]);
             $btnclass = 'btn btn-success';
-        } else if ($shipment->deliveryDate == null && $shipment->shipmentDate != null) {
-            $btnclass = 'btn btn-warning';
-        } else {
-            $btnclass = 'btn btn-light';
-        }
-        if ($shipment->carrierId == 2) {
-            $shipmentPrint .='<button style="width: 200px ; height:32px;"  onclick="openTrackGlsDelivery(\'' . $shipment->trackingNumber . '\');" class=' . $btnclass . '> <i class="fa fa-truck" aria-hidden="true"></i>' . $findCarrier->name .'-'.$shipment->trackingNumber.'</button><br>';
-        } else {
-            $shipmentPrint .='<button style="width: 200px ; height:32px;"  onclick="openTrackDelivery(\'' . $shipment->trackingNumber . '\');" class=' . $btnclass . '> <i class="fa fa-truck" aria-hidden="true"></i>' . $findCarrier->name .'-'.$shipment->trackingNumber.'</button><br>';
-            //  $shipmentCollect.= '<button onclick="openTrackDelivery(\'1Z463V1V6897807419\');" class="btn btn-light" role="button"><i class="fa fa-truck" aria-hidden="true"></i>1Z463V1V6897807419</button>';
+            if ($shipment->deliveryDate != null && $shipment->shipmentDate != null) {
+                $btnclass = 'btn btn-success';
+            } else if ($shipment->deliveryDate == null && $shipment->shipmentDate != null) {
+                $btnclass = 'btn btn-warning';
+            } else {
+                $btnclass = 'btn btn-light';
+            }
+            if ($shipment->carrierId == 2) {
+                $shipmentPrint .= '<button style="width: 200px ; height:32px;"  onclick="openTrackGlsDelivery(\'' . $shipment->trackingNumber . '\');" class=' . $btnclass . '> <i class="fa fa-truck" aria-hidden="true"></i>' . $findCarrier->name . '-' . $shipment->trackingNumber . '</button><br>';
+            } else {
+                $shipmentPrint .= '<button style="width: 200px ; height:32px;"  onclick="openTrackDelivery(\'' . $shipment->trackingNumber . '\');" class=' . $btnclass . '> <i class="fa fa-truck" aria-hidden="true"></i>' . $findCarrier->name . '-' . $shipment->trackingNumber . '</button><br>';
+                //  $shipmentCollect.= '<button onclick="openTrackDelivery(\'1Z463V1V6897807419\');" class="btn btn-light" role="button"><i class="fa fa-truck" aria-hidden="true"></i>1Z463V1V6897807419</button>';
+            }
         }
     }
     $shipmentPrint .='<br><button style="width: 200px ; height:32px;"  onclick="createDelivery('.$line->orderId.','.$line->id.')" class="btn btn-light"><i class="fa fa-plus" aria-hidden="true"></i>Crea Spedizione</button>';
