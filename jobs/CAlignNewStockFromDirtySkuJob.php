8728ae50904962e7a21a5aa4904e264481c4dad7
juri fiorani <jurif@hotmail.com>
2021-08-04 15:21:27 +0200

    creazione Vista Vendite

--- jobs/CAlignNewStockFromDirtySkuJob.php
+++ jobs/CAlignNewStockFromDirtySkuJob.php
@@ -50,7 +50,7 @@ class CAlignNewStockFromDirtySkuJob extends ACronJob
             $productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
             $sql='select p.id as productId, p.productVariantId as productVariantId, ds.qty as qty, ds.productSizeId as productSizeId,
                                 shp.shopId as shopId from Product p join DirtyProduct shp on p.id=shp.productId
- and p.productVariantId=shp.productVariantId join DirtySku ds on shp.id=ds.dirtyProductId where shp.shopId IN (1,51,58,61) and ds.productSizeId is not null';
+ and p.productVariantId=shp.productVariantId join DirtySku ds on shp.id=ds.dirtyProductId where  ds.productSizeId is not null';
             $res=\Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
             foreach($res as $result){
                                 $sku = $productSkuRepo->findOneBy(['productId' => $result['productId'],'productVariantId' => $result['productVariantId'],'productSizeId' => $result['productSizeId'],'shopId' => $result['shopId']]);
