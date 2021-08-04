f8b29b5f84bb64fb4b23207f9310e5fbf04e4444
juri fiorani <jurif@hotmail.com>
2021-08-04 14:26:50 +0200

    creazione Vista Vendite

--- jobs/CAlignNewStockFromDirtySkuJob.php
+++ jobs/CAlignNewStockFromDirtySkuJob.php
@@ -55,24 +55,17 @@ class CAlignNewStockFromDirtySkuJob extends ACronJob
     {
         $res = "";
         try {
-            $productRepo = \Monkey::app()->repoFactory->create('Product');
-            $dirtyProductRepo = \Monkey::app()->repoFactory->create('DirtyProduct');
-            $dirtySkuRepo = \Monkey::app()->repoFactory->create('DirtySku');
+
             $productSkuRepo = \Monkey::app()->repoFactory->create('ProductSku');
             $sql='select p.id as productId, p.productVariantId as productVariantId,p.qty as qty,ds.productSizeId as productSizeId,
                                 shp.shopId as shopId from Product p join DirtyProduct shp on p.id=shp.productId
  and p.productVariantId=shp.productVariantId join DirtySku ds on shp.id=ds.dirtyProductId where shp.shopId IN (1,51,58,61) and ds.productSizeId is not null';
             $res=\Monkey::app()->dbAdapter->query($sql,[]);
             foreach($res as $result){
-
                                 $sku = $productSkuRepo->findOneBy(['productId' => $result['productId'],'productVariantId' => $result['productVariantId'],'productSizeId' => $result['productSizeId'],'shopId' => $result['shopId']]);
                                 $sku->stockQty = $dirtySku->qty;
                                 $sku->update();
                             }
-
-
-
-
 
         }catch(\Throwable $e){
             \Monkey::app()->applicationLog('CAlignNewStockFromDirtySkuJob','Error','Error Align Quantity',$e->getMessage(),$e->getLine());
