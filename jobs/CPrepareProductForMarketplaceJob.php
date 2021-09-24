c6f788d38d93619ed049cfd8585383ad56d1fdf0
juri fiorani <jurif@hotmail.com>
2021-09-24 21:25:57 +0200

    creazione Vista Vendite

--- jobs/CPrepareProductForMarketplaceJob.php
+++ jobs/CPrepareProductForMarketplaceJob.php
@@ -50,10 +50,10 @@ class CPrepareProductForMarketplaceJob extends ACronJob
                     $this->report('CPrepareProductForMarketplaceJob','start Preparing','');
                     $sql = '(select p.id as productId, p.productVariantId as productVariantId,p.qty as qty,
                                 shp.shopId as shopId from Product p join ShopHasProduct shp on p.id=shp.productId
- and p.productVariantId=shp.productVariantId where p.qty > 0 and p.productStatusId in (6) and p.productSeasonId >35 and  shp.shopId ='.$shop->id.') UNION
+ and p.productVariantId=shp.productVariantId where p.qty > 0 and p.productStatusId in (6) and p.productSeasonId > 35 and  shp.shopId ='.$shop->id.') UNION
 (select p2.id as productId, p2.productVariantId as productVariantId, p2.qty as qty, shp2.shopIdDestination as shopId from
  Product p2 join ProductHasShopDestination shp2 on p2.id=shp2.productId
- and p2.productVariantId=shp2.productVariantId where p2.qty > 0 and p2.productStatusId in (6) and p.productSeasonId >35 and  shp2.shopIdDestination ='.$shop->id.')';
+ and p2.productVariantId=shp2.productVariantId where p2.qty > 0 and p2.productStatusId in (6) and p2.productSeasonId >35 and  shp2.shopIdDestination ='.$shop->id.')';
                     $products = \Monkey::app()->dbAdapter->query($sql,[])->fetchAll();
                     foreach ($products as $product) {
                         $phs=$phsRepo->findOneBy(['productId'=>$product['productId'],'productVariantId'=>$product['productVariantId'],'marketplaceHasShopId'=>$mhs->id]);
