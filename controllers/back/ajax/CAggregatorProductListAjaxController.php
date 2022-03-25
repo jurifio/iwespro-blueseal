6ccdf545696718eceae627f22a53c1e17731c6b7
juri fiorani <jurif@hotmail.com>
2022-03-25 18:19:19 +0100

    creazione Vista Vendite

--- controllers/back/ajax/CAggregatorProductListAjaxController.php
+++ controllers/back/ajax/CAggregatorProductListAjaxController.php
@@ -38,7 +38,7 @@ class CAggregatorProductListAjaxController extends AAjaxController
               pps.price,
               pb.name  as `brand`, 
               p.externalId AS externalId,
-                   
+              '' as marketplaceAssociation,     
 
               # group_concat(concat(mahp.marketplaceProductId,' | ',`s`.`name`, ' | ', `m`.`name`, ' | Fee: ', mahp.fee,' | Fee Mobile: ', mahp.feeMobile, ' | Fee Customer: ', mahp.feeCustomer,' | Fee  Customer Mobile: ', mahp.feeCustomerMobile,' | Price Modifier: ', mahp.priceModifier, ' | Titolo modificato: ', mahp.titleModified,' | Operazione: ',if(mahp.insertionDate=null,'da inserire ','inserito '),if(mahp.lastResponse=null, concat('Eseguito ',mahp.lastResponse),concat('Fallito ',mahp.lastResponse))  )) AS marketplaceAssociation,
               p.isOnSale AS pickySale,
