24f2975e1f9674d4ed059603ff6858719d90ee72
juri fiorani <jurif@hotmail.com>
2022-05-26 09:56:14 +0200

    creazione Vista Vendite

--- assets/js/button/bs.product.photo.manage.js
+++ assets/js/button/bs.product.photo.manage.js
@@ -1,7 +1,7 @@
 window.buttonSetup = {
     tag:"a",
     icon:"fa-camera-retro",
-    permission:"/admin/product/edit&&allShops",
+    permission:"/admin/product/edit",
     event:"bs-manage-photo",
     class:"btn btn-default",
     rel:"tooltip",
