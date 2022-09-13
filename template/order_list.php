95d3ebb6daa59d44b0ca1cabfa023b6c615a779f
juri fiorani <jurif@hotmail.com>
2022-09-13 19:37:14 +0200

    creazione Vista Vendite

--- template/order_list.php
+++ template/order_list.php
@@ -66,8 +66,8 @@
                                    data-controller="OrderListAjaxController"
                                    data-url="<?php echo $app->urlForBluesealXhr() ?>" id="orderTable"
                                    data-inner-setup="true"
-                                   data-length-menu-setup="50, 100, 200, 500,1000,2000"
-                                   data-display-length="50">
+                                   data-length-menu-setup="10, 20, 50, 100, 200, 500,1000,2000"
+                                   data-display-length="20">
                                 <thead>
                                 <tr>
                                     <th data-slug="orderDate"
