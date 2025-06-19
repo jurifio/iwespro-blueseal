bae1bae4d1b097f6556606bc7daad60bc7e6fa16
jurif <jurif@hotmail.com>
2025-06-19 20:02:12 +0200

    aggiornamento  CAuthmanager

--- assets/js/dictionary_groupsize_edit.js
+++ assets/js/dictionary_groupsize_edit.js
@@ -4,7 +4,7 @@ $(document).on('change', $('name[productSizeGroupId]').val, function(event) {
         type: "PUT",
         url: changed.data('action'),
         data: {
-            sizeId: encodeURIComponent(changed.val()),
+            productSizeGroupId: encodeURIComponent(changed.val()),
             id: encodeURIComponent(changed.data('pid'))
         }
     });
